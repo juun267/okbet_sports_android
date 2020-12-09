e25352bf2654c34a67aa7a65a617a6b9b303c57a
edward_android <koyo98765@gmail.com>
2020-12-09 15:01:37 +0800

    [Add] moshi annotation to login response

--- app/src/main/java/org/cxct/sportlottery/network/LoginResponse.kt
+++ app/src/main/java/org/cxct/sportlottery/network/LoginResponse.kt
@@ -1,26 +1,50 @@
 package org.cxct.sportlottery.network
 
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Json
+
+@JsonClass(generateAdapter = true)
 data class LoginResponse(
+    @Json(name = "code")
     val code: Int,
+    @Json(name = "msg")
     val msg: String,
+    @Json(name = "success")
     val success: Boolean,
-    val t: T
+    @Json(name = "t")
+    val t: LoginData
 )
 
-data class T(
+@JsonClass(generateAdapter = true)
+data class LoginData(
+    @Json(name = "fullName")
     val fullName: String,
+    @Json(name = "iconUrl")
     val iconUrl: String?,
+    @Json(name = "lastLoginDate")
     val lastLoginDate: Long,
+    @Json(name = "lastLoginIp")
     val lastLoginIp: String,
+    @Json(name = "loginDate")
     val loginDate: Long,
+    @Json(name = "loginIp")
     val loginIp: String,
+    @Json(name = "nickName")
     val nickName: String?,
+    @Json(name = "platformId")
     val platformId: Int,
+    @Json(name = "rechLevel")
     val rechLevel: String?,
+    @Json(name = "testFlag")
     val testFlag: Int,
+    @Json(name = "token")
     val token: String,
+    @Json(name = "uid")
     val uid: Int,
+    @Json(name = "userId")
     val userId: Int,
+    @Json(name = "userName")
     val userName: String,
+    @Json(name = "userType")
     val userType: String
 )
