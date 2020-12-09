11cb7e95c7dacd3b367dc6e516727fc1b3e99370
edward_android <koyo98765@gmail.com>
2020-12-09 15:26:17 +0800

    [Refactor] rename SportApi to OneBoSportApi

--- app/src/main/java/org/cxct/sportlottery/network/OneBoSportApi.kt
+++ app/src/main/java/org/cxct/sportlottery/network/OneBoSportApi.kt
@@ -16,7 +16,7 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
-object SportApi {
+object OneBoSportApi {
     val indexService: IndexService by lazy {
         retrofit.create(IndexService::class.java)
     }
