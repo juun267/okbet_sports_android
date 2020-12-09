83a6f9851aed5f1658b49b317522004dde2e7777
Sky <simon@cxteam.io>
2020-12-09 16:06:56 +0800

    [新增] 添加 navigation component

---
+++ app/src/main/java/org/cxct/sportlottery/ui/game/BetListDialog.kt
@@ -0,0 +1,23 @@
+package org.cxct.sportlottery.ui.game
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cxct.sportlottery.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BetListDialog : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.dialog_bet_list, container, false)
+    }
+
+}
