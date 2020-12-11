a0398543cdc6d28af222d85a059bea2c0f4b3d41
edward_android <koyo98765@gmail.com>
2020-12-11 13:51:56 +0800

    [Add] common param for each request used

---
+++ app/src/main/java/org/cxct/sportlottery/common/IdParams.kt
@@ -0,0 +1,6 @@
+package org.cxct.sportlottery.common
+
+data class IdParams(
+    val userId: Int?,
+    val platformId: Int?
+)
---
+++ app/src/main/java/org/cxct/sportlottery/common/TimeRangeParams.kt
@@ -0,0 +1,6 @@
+package org.cxct.sportlottery.common
+
+data class TimeRangeParams(
+    val startTime: String? = null,
+    val endTime: String? = null
+)
