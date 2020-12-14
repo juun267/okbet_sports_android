fd7ef2c589935738909765578fc4e1f4b0026b92
Cheryl <a222025115@gmail.com>
2020-12-14 17:48:53 +0800

    [add] bet record activity, vm and xml. add menu item on click event.

---
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/BetRecordActivity.kt
@@ -0,0 +1,13 @@
+package org.cxct.sportlottery.ui.bet_record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.base.BaseActivity
+
+class BetRecordActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bet_record)
+    }
+}
