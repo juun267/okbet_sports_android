b8c1a78dc4dcc79d7273bb507d49d96f86a186f8
Cheryl <a222025115@gmail.com>
2020-12-15 18:38:47 +0800

    [Add] bet record views and dependencies.

---
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/search/result/BetRecordResultFragment.kt
@@ -0,0 +1,18 @@
+package org.cxct.sportlottery.ui.bet_record.search.result
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cxct.sportlottery.R
+
+class BetRecordResultFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bet_record_result, container, false)
+        
+    }
+
+}
