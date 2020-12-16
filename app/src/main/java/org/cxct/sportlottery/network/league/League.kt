627a181daf327a059f149c8c25dbd4194646eaf5
edward_android <koyo98765@gmail.com>
2020-12-16 11:17:07 +0800

    [Add] get league list api to network object

---
+++ app/src/main/java/org/cxct/sportlottery/network/league/League.kt
@@ -0,0 +1,15 @@
+package org.cxct.sportlottery.network.league
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class League(
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "num")
+    val num: Int
+)
