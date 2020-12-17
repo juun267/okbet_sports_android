a278c38c38806c44879d5651ca2b4a8c7f7b7d05
edward_android <koyo98765@gmail.com>
2020-12-17 12:22:29 +0800

    [Add] login result in error utils

--- app/src/main/java/org/cxct/sportlottery/network/error/ErrorUtils.kt
+++ app/src/main/java/org/cxct/sportlottery/network/error/ErrorUtils.kt
@@ -2,6 +2,8 @@ package org.cxct.sportlottery.network.error
 
 import okhttp3.ResponseBody
 import org.cxct.sportlottery.network.OneBoSportApi
+import org.cxct.sportlottery.network.index.INDEX_LOGIN
+import org.cxct.sportlottery.network.index.LoginResult
 import retrofit2.Converter
 import retrofit2.Response
 import java.io.IOException
@@ -9,7 +11,7 @@ import java.io.IOException
 
 object ErrorUtils {
 
-    fun <T> parseError(response: Response<T>): APIError? {
+    fun <T> parseError(response: Response<T>): T? {
         val converter: Converter<ResponseBody, APIError> = OneBoSportApi.retrofit
             .responseBodyConverter(APIError::class.java, arrayOfNulls<Annotation>(0))
 
@@ -19,9 +21,22 @@ object ErrorUtils {
             try {
                 error = converter.convert(it)
             } catch (e: IOException) {
-                return APIError()
+                e.printStackTrace()
             }
         }
-        return error
+
+        error?.let {
+            if (it.success != null && it.code != null && it.msg != null) {
+                val url = response.raw().request.url.toString()
+                when {
+                    (url.contains(INDEX_LOGIN)) -> {
+                        @Suppress("UNCHECKED_CAST")
+                        return LoginResult(it.code, it.msg, it.success, null) as T
+                    }
+                }
+            }
+        }
+
+        return null
     }
 }
