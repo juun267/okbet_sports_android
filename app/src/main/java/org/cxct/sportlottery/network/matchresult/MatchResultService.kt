a278bf4aeb3e37f7ec841295afaa0b0089f90297
edward_android <koyo98765@gmail.com>
2020-12-18 10:06:18 +0800

    [Add] match result play result list service to one sport api

--- app/src/main/java/org/cxct/sportlottery/network/matchresult/MatchResultService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/matchresult/MatchResultService.kt
@@ -1,12 +1,11 @@
 package org.cxct.sportlottery.network.matchresult
 
+import org.cxct.sportlottery.network.matchresult.playlist.MatchResultPlayListResult
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 const val MATCH_RESULT_LIST = "/api/front/match/result/list"
+const val MATCH_RESULT_PLAY_LIST = "/api/front/match/result/play/list"
 
 interface MatchResultService {
 
@@ -16,4 +15,11 @@ interface MatchResultService {
         @Header("x-session-token") token: String,
         @Body matchResultListRequest: MatchResultListRequest
     ): Response<MatchResultListResult>
+
+    @Headers("x-session-platform-code:plat1")
+    @GET(MATCH_RESULT_PLAY_LIST)
+    suspend fun getMatchResultPlayList(
+        @Header("x-session-token") token: String,
+        @Query("matchId") matchId: String
+    ): Response<MatchResultPlayListResult>
 }
