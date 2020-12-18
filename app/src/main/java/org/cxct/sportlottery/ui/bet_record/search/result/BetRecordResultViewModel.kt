a62bdd7e6dd3861e91dad41c41370da4fa4a2d05
Cheryl <a222025115@gmail.com>
2020-12-18 17:16:22 +0800

    [Fix] rebase conflict.

---
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/search/result/BetRecordResultViewModel.kt
@@ -0,0 +1,18 @@
+package org.cxct.sportlottery.ui.bet_record.search.result
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.network.OneBoSportApi
+import org.cxct.sportlottery.network.index.LoginRequest
+import org.cxct.sportlottery.network.index.LoginResult
+import org.cxct.sportlottery.util.MD5Util
+import retrofit2.Response
+
+
+class BetRecordResultViewModel() : ViewModel() {
+}
