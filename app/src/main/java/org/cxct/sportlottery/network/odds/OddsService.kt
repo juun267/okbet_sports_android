de9e97bbbe4ea0faf3d25d48a6f8780d87ea3ee5
edward_android <koyo98765@gmail.com>
2020-12-21 11:12:58 +0800

    [Refactor] move OddsService constant to Constants

--- app/src/main/java/org/cxct/sportlottery/network/odds/OddsService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/odds/OddsService.kt
@@ -1,11 +1,10 @@
 package org.cxct.sportlottery.network.odds
 
+import org.cxct.sportlottery.network.Constants.MATCH_ODDS_Detail
 import org.cxct.sportlottery.network.Constants.MATCH_ODDS_LIST
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-
-const val MATCH_ODDS_Detail = "/api/front/match/odds/detail"
 
 
 interface OddsService {
