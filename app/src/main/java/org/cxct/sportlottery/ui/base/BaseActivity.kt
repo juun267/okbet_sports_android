a4d0a5f9fff35d7b7e795320aaf1b076392fd019
edward_android <koyo98765@gmail.com>
2020-12-22 11:06:07 +0800

    [Add] BaseResult for BaseActivity to deal common error

--- app/src/main/java/org/cxct/sportlottery/ui/base/BaseActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/base/BaseActivity.kt
@@ -1,12 +1,14 @@
 package org.cxct.sportlottery.ui.base
 
 import android.app.Activity
+import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.layout_loading.view.*
 import org.cxct.sportlottery.R
 
@@ -18,6 +20,28 @@ abstract class BaseActivity<VM : BaseViewModel> : AppCompatActivity() {
     protected abstract val viewModel: VM
 
     private var loadingView: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        onTokenStateObserved()
+    }
+
+    private fun onTokenStateObserved() {
+        viewModel.baseResult.observe(this, Observer {
+            when (it.code) {
+                2014 -> {
+                    //TODO deal response code 2014
+                }
+                2015 -> {
+                    //TODO deal response code 2015
+                }
+                2018 -> {
+                    //TODO deal response code 2018
+                }
+            }
+        })
+    }
 
     /*弹出加载界面*/
     open fun loading() {
