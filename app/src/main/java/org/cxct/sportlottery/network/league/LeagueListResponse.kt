71888006af15efc5e37b1c107be0c27b6a08960d
edward_android <koyo98765@gmail.com>
2020-12-22 11:13:06 +0800

    [Add] all network result extends BaseResult

--- app/src/main/java/org/cxct/sportlottery/network/league/LeagueListResponse.kt
+++ app/src/main/java/org/cxct/sportlottery/network/league/LeagueListResponse.kt
@@ -3,17 +3,19 @@ package org.cxct.sportlottery.network.league
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import org.cxct.sportlottery.network.common.BaseResult
+
 
 @JsonClass(generateAdapter = true)
 data class LeagueListResponse(
     @Json(name = "code")
-    val code: Int,
+    override val code: Int,
     @Json(name = "msg")
-    val msg: String,
+    override val msg: String,
     @Json(name = "rows")
     val rows: List<Row>?,
     @Json(name = "success")
-    val success: Boolean,
+    override val success: Boolean,
     @Json(name = "total")
     val total: Int?
-)
+) : BaseResult()
--- app/src/main/java/org/cxct/sportlottery/network/odds/OddsListResult.kt
+++ app/src/main/java/org/cxct/sportlottery/network/odds/OddsListResult.kt
@@ -3,15 +3,16 @@ package org.cxct.sportlottery.network.odds
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import org.cxct.sportlottery.network.common.BaseResult
 
 @JsonClass(generateAdapter = true)
 data class OddsListResult(
     @Json(name = "code")
-    val code: Int,
+    override val code: Int,
     @Json(name = "msg")
-    val msg: String,
+    override val msg: String,
     @Json(name = "success")
-    val success: Boolean,
+    override val success: Boolean,
     @Json(name = "t")
     val oddsListData: OddsListData?
-)
+) : BaseResult()
