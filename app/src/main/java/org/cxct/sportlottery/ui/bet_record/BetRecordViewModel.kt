dd4fa936c7884f3a28886ec7353b47bad21443cc
Cheryl <a222025115@gmail.com>
2020-12-22 20:46:31 +0800

    [Add] finish calendar date pick, bet status listview, and api calling.

--- app/src/main/java/org/cxct/sportlottery/ui/bet_record/BetRecordViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/BetRecordViewModel.kt
@@ -21,30 +21,20 @@ class BetRecordViewModel(private val loginRepository: LoginRepository) : ViewMod
     val betListRequest: LiveData<BetListRequest>
         get() = _betListRequest
 
-    val betRecordResult: LiveData<BetListResult?>
+    val betRecordResult: LiveData<BetListResult>
         get() = _betRecordResult
 
     private val _betListRequest = MutableLiveData<BetListRequest>()
-    private val _betRecordResult = MutableLiveData<BetListResult?>()
+    private val _betRecordResult = MutableLiveData<BetListResult>()
 
-    val statusNameMap = mapOf(
-        0 to "未确认",
-        1 to "未结算",
-        2 to "全赢",
-        3 to "赢半",
-        4 to "全输",
-        5 to "输半",
-        6 to "和",
-        7 to "已取消"
-    )
+
+    val statusNameMap =
+        mapOf(0 to "未确认", 1 to "未结算", 2 to "全赢", 3 to "赢半", 4 to "全输", 5 to "输半", 6 to "和", 7 to "已取消")
 
     fun chooseBetStatus() {
         Log.e(">>>", "chooseBetStatus")
     }
-
-    fun chooseDate() {
-
-    }
+/*
 
     private fun updateStartDate(timeStamp: Long?) {
 
@@ -55,20 +45,23 @@ class BetRecordViewModel(private val loginRepository: LoginRepository) : ViewMod
             it.timeRangeParams?.startTime = timeStamp
         }
 
-        _betListRequest.value = BetListRequest(timeRangeParams = TimeRangeParams(startTime = timeStamp))
+        _betListRequest.value =
+            BetListRequest(timeRangeParams = TimeRangeParams(startTime = timeStamp))
     }
 
 
     private fun updateEndDate(timeStamp: Long?) {
-        _betListRequest.value = BetListRequest(timeRangeParams = TimeRangeParams(endTime = timeStamp))
+        _betListRequest.value =
+            BetListRequest(timeRangeParams = TimeRangeParams(endTime = timeStamp))
     }
 
     fun getStartDate(): String {
-        return timeStampToDate(_betListRequest.value?.timeRangeParams?.startTime)?:""
+        return timeStampToDate(_betListRequest.value?.timeRangeParams?.startTime) ?: ""
     }
+*/
 
     private fun timeStampToDate(time: Long?): String {
-        if (time==null) return ""
+        if (time == null) return ""
         val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         return formatter.format(time)
     }
@@ -77,35 +70,44 @@ class BetRecordViewModel(private val loginRepository: LoginRepository) : ViewMod
         return "asdf"
     }
 
-    fun setToday() {
-        //get today
-        updateDate()
-    }
-    fun setYesterday() {
-        updateDate(1)
-    }
+    /*
 
-    fun setPast30Days() {
-        updateDate(18)
-    }
+        fun setToday() {
+            //get today
+            updateDate()
+        }
+        fun setYesterday() {
+            updateDate(1)
+        }
 
-    private fun updateDate(minusDays: Int ?= null) {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DATE)
+        fun setPast30Days() {
+            updateDate(18)
+        }
 
-        //set format
+        private fun updateDate(minusDays: Int ?= null) {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH) + 1
+            val day = calendar.get(Calendar.DATE)
+
+            //set format
+            val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val endTimeStamp = formatter.parse("$year-$month-$day 23:59:59")?.time
+            var startTimeStamp = formatter.parse("$year-$month-$day 00:00:00")?.time
+            if (minusDays != null)
+                startTimeStamp = endTimeStamp?.minus((1000 * 60 * 60 * 24 * minusDays))
+
+            //update data
+            updateStartDate(startTimeStamp)
+            updateEndDate(endTimeStamp)
+        }
+    */
+    fun dateToTimeStamp(date: String, isStartTime: Boolean = true): Long? {
         val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        val endTimeStamp = formatter.parse("$year-$month-$day 23:59:59")?.time
-        var startTimeStamp = formatter.parse("$year-$month-$day 00:00:00")?.time
-        if (minusDays != null)
-            startTimeStamp = endTimeStamp?.minus((1000 * 60 * 60 * 24 * minusDays))
+        val startTimeStamp = formatter.parse("$date 00:00:00")?.time
+        val endTimeStamp = formatter.parse("$date 23:59:59")?.time
 
-        //update data
-        updateStartDate(startTimeStamp)
-        updateEndDate(endTimeStamp)
-
+        return if (isStartTime) startTimeStamp else endTimeStamp
     }
 
     fun searchBetRecordHistory(betListRequest: BetListRequest) {
@@ -118,11 +120,11 @@ class BetRecordViewModel(private val loginRepository: LoginRepository) : ViewMod
         val betListResponse =
             OneBoSportApi.betService.getBetList(loginRepository.token.value, betListRequest)
 
-//        val statusList: List<Int>, //状态 0：未确认，1：未结算，2：全赢，3：赢半，4：全输，5：输半，6：和，7：已取消
-//        val gameType: String? = null,
-//        val pagingParams: PagingParams? = null,
-//        val timeRangeParams: TimeRangeParams? = null,
-//        val idParams: IdParams? = null
+        //        val statusList: List<Int>, //状态 0：未确认，1：未结算，2：全赢，3：赢半，4：全输，5：输半，6：和，7：已取消
+        //        val gameType: String? = null,
+        //        val pagingParams: PagingParams? = null,
+        //        val timeRangeParams: TimeRangeParams? = null,
+        //        val idParams: IdParams? = null
 
         if (betListResponse.isSuccessful) {
             _betRecordResult.postValue(betListResponse.body())
--- app/src/main/java/org/cxct/sportlottery/ui/bet_record/search/result/BetRecordResultFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/search/result/BetRecordResultFragment.kt
@@ -5,13 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_bet_record_result.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.databinding.FragmentBetRecordSearchBinding
+import org.cxct.sportlottery.ui.bet_record.BetRecordViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BetRecordResultFragment : Fragment() {
 
+    private val betRecordViewModel: BetRecordViewModel by viewModel()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bet_record_result, container, false)
+        val binding: FragmentBetRecordSearchBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_bet_record_result, container, false)
+        binding.apply {
+            betRecordViewModel = this@BetRecordResultFragment.betRecordViewModel
+            lifecycleOwner = this@BetRecordResultFragment
+        }
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRv()
+    }
+
+    private fun initRv() {
+        rv_bet_record
+    }
 }
