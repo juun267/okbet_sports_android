37cd4be9b8ab1ab4ba2bc3ef4feadc942c46d7d8
Bill <h853047455@gmail.com>
2020-12-22 11:29:28 +0800

    [Add] build InfoCenter UI

---
+++ app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterActivity.kt
@@ -0,0 +1,12 @@
+package org.cxct.sportlottery.ui.infoCenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cxct.sportlottery.R
+
+class InfoCenterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_center)
+    }
+}
