422495207325df7c633ae5848f86ea9572960440
Cheryl <a222025115@gmail.com>
2020-12-22 20:46:31 +0800

    [Add] finish calendar date pick, bet status listview, and api calling.

--- app/src/main/java/org/cxct/sportlottery/network/bet/list/BetListRequest.kt
+++ app/src/main/java/org/cxct/sportlottery/network/bet/list/BetListRequest.kt
@@ -3,6 +3,8 @@ package org.cxct.sportlottery.network.bet.list
 import org.cxct.sportlottery.network.common.IdParams
 import org.cxct.sportlottery.network.common.PagingParams
 import org.cxct.sportlottery.network.common.TimeRangeParams
+/*
+
 data class BetListRequest(
     val statusList: List<Int>? = null, //状态 0：未确认，1：未结算，2：全赢，3：赢半，4：全输，5：输半，6：和，7：已取消
     val gameType: String? = null,
@@ -10,15 +12,15 @@ data class BetListRequest(
     val timeRangeParams: TimeRangeParams? = null,
     val idParams: IdParams? = null
 )
-/*
+*/
+
 data class BetListRequest(
     val page: Int? = null,
     val pageSize: Int? = null,
     val startTime: String? = null,
     val endTime: String? = null,
-    val statusList: List<Int>, //状态 0：未确认，1：未结算，2：全赢，3：赢半，4：全输，5：输半，6：和，7：已取消
+    val statusList: List<Int>? = null, //状态 0：未确认，1：未结算，2：全赢，3：赢半，4：全输，5：输半，6：和，7：已取消
     val gameType: String? = null,
-    val userId: Int?,
-    val platformId: Int?
+    val userId: Int? = null,
+    val platformId: Int? = null
 )
-*/
