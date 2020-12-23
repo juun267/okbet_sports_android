97b61bcced6477e41e4581e252de24e97535902b
Cheryl <a222025115@gmail.com>
2020-12-23 11:00:27 +0800

    [Add] betRecordResultFragment ui. / fix dateToTimeStamp fun.

---
+++ app/src/main/java/org/cxct/sportlottery/ui/bet_record/search/result/BetRecordAdapter.kt
@@ -0,0 +1,20 @@
+package org.cxct.sportlottery.ui.bet_record.search.result
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+/*
+
+class BetRecordAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+}
+*/
