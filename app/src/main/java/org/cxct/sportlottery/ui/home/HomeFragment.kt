79fa1d6b719e39984c1dd8b38c5182d4797257af
Sky <simon@cxteam.io>
2020-12-24 15:30:42 +0800

    [Refactor] HomeGameDrawer 表單資料串接 API

--- app/src/main/java/org/cxct/sportlottery/ui/home/HomeFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/home/HomeFragment.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.databinding.FragmentHomeBinding
+import org.cxct.sportlottery.interfaces.OnSelectItemListener
+import org.cxct.sportlottery.network.match.Match
 import org.cxct.sportlottery.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
@@ -42,14 +44,32 @@ class HomeFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
     private fun refreshView() {
         viewModel.earlyGameResult.observe(viewLifecycleOwner, Observer {
             drawer_early.setCount(it.matchPreloadData?.num.toString())
+            drawer_early.setRvGameData(it.matchPreloadData)
+            drawer_early.setOnSelectItemListener(object : OnSelectItemListener<Match> {
+                override fun onClick(select: Match) {
+                    //TODO simon test review 設定點擊事件跳轉畫面
+                }
+            })
         })
 
         viewModel.inPlayGameResult.observe(viewLifecycleOwner, Observer {
             drawer_in_play.setCount(it.matchPreloadData?.num.toString())
+            drawer_in_play.setRvGameData(it.matchPreloadData)
+            drawer_in_play.setOnSelectItemListener(object : OnSelectItemListener<Match> {
+                override fun onClick(select: Match) {
+                    //TODO simon test review 設定點擊事件跳轉畫面
+                }
+            })
         })
 
         viewModel.todayGameResult.observe(viewLifecycleOwner, Observer {
             drawer_today.setCount(it.matchPreloadData?.num.toString())
+            drawer_today.setRvGameData(it.matchPreloadData)
+            drawer_today.setOnSelectItemListener(object : OnSelectItemListener<Match> {
+                override fun onClick(select: Match) {
+                    //TODO simon test review 設定點擊事件跳轉畫面
+                }
+            })
         })
     }
 }
--- app/src/main/java/org/cxct/sportlottery/util/TimeUtil.kt
+++ app/src/main/java/org/cxct/sportlottery/util/TimeUtil.kt
@@ -1,11 +1,13 @@
 package org.cxct.sportlottery.util
 
 import android.annotation.SuppressLint
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
 @SuppressLint("SimpleDateFormat")
 object TimeUtil {
+    private const val TAG = "TimeUtil"
 
     @JvmStatic
     fun stampToDate(time: Long): String {
@@ -18,4 +20,17 @@ object TimeUtil {
         return SimpleDateFormat("yyyy-MM-dd HH:mm").parse(date).time
     }
 
+    @JvmStatic
+    fun timeFormat(time: Long?, format: String, timeZone: TimeZone = TimeZone.getDefault()): String {
+        var formattedTime = ""
+        try {
+            val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+            dateFormat.timeZone = timeZone
+            formattedTime = dateFormat.format(Date(time!!))
+        } catch (e: Exception) {
+            Log.e(TAG, "解析日期失敗!!!")
+            e.printStackTrace()
+        }
+        return formattedTime
+    }
 }
