3d2d3fa7f5dc3e46dc6c3cee4d73d04ba51270ba
edward_android <koyo98765@gmail.com>
2020-12-25 16:15:00 +0800

    [Fix] change IdParams type from data class to interface

--- app/src/main/java/org/cxct/sportlottery/network/bet/list/BetListRequest.kt
+++ app/src/main/java/org/cxct/sportlottery/network/bet/list/BetListRequest.kt
@@ -7,9 +7,10 @@ import org.cxct.sportlottery.network.common.TimeRangeParams
 data class BetListRequest(
     val statusList: List<Int>,
     val gameType: String? = null,
-    val idParams: IdParams? = null,
     override val startTime: String? = null,
     override val endTime: String? = null,
     override val page: Int? = null,
     override val pageSize: Int? = null,
-) : TimeRangeParams, PagingParams
+    override val userId: Int? = null,
+    override val platformId: Int? = null
+) : TimeRangeParams, PagingParams, IdParams
