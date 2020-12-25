18548050aaaebfcfb16711e65a9cd990d009fa97
edward_android <koyo98765@gmail.com>
2020-12-25 16:00:32 +0800

    [Add] outright result list error handling return null data result to ErrorUtils

--- app/src/main/java/org/cxct/sportlottery/network/outright/OutrightResultListResult.kt
+++ app/src/main/java/org/cxct/sportlottery/network/outright/OutrightResultListResult.kt
@@ -12,9 +12,9 @@ data class OutrightResultListResult(
     @Json(name = "msg")
     override val msg: String,
     @Json(name = "rows")
-    val rows: List<Row>,
+    val rows: List<Row>?,
     @Json(name = "success")
     override val success: Boolean,
     @Json(name = "total")
-    val total: Int
+    val total: Int?
 ) : BaseResult()
