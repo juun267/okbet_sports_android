ee4077f65e92227837e4edfa7532c156f3c49617
Bill <h853047455@gmail.com>
2020-12-29 16:35:32 +0800

    [Refactor] InfoCenterData

--- app/src/main/java/org/cxct/sportlottery/network/InfoCenter/InfoCenterData.kt
+++ app/src/main/java/org/cxct/sportlottery/network/InfoCenter/InfoCenterData.kt
@@ -7,24 +7,24 @@ import com.squareup.moshi.JsonClass
 data class InfoCenterData(
     @Json(name = "id")
     val id: Int,
+    @Json(name = "userId")
+    val userId: Int?,
+    @Json(name = "userName")
+    val userName: String,
+    @Json(name = "addDate")
+    val addDate: String,
     @Json(name = "title")
-    val title: String?,
+    val title: String,
     @Json(name = "content")
     val content: String,
-    @Json(name = "messageType")//消息类型 1:系统公告 2:赛事公告
-    val messageType: Int,
-    @Json(name = "matchId")//关联赛事id
-    val matchId: String,
-    @Json(name = "url")
-    val url: String,//TODO BIll 文件寫是Int要再確認
-    @Json(name = "platformId")//平台id,platformId = 0时，代表全部平台
+    @Json(name = "isRead")//是否已读 0未读 1已读
+    val isRead: Int,
+    @Json(name = "noticeType")//消息类型 0消息通知、1app推送
+    val noticeType: Int,
+    @Json(name = "msgShowType")//消息类型 1发送至用户消息中心、2右下角弹出提示、3页面中央弹出提示、4app推送
+    val msgShowType: Int,
+    @Json(name = "platformId")//平台id
     val platformId: Int,
-    @Json(name = "addTime")//添加时间
-    val addTime: String,
-    @Json(name = "updateTime")//更新时间
-    val updateTime: String,
-    @Json(name = "startTime")
-    val startTime: String,
-    @Json(name = "endTime")
-    val endTime: String
+    @Json(name = "operatorName")//发件人
+    val operatorName: String
 )
