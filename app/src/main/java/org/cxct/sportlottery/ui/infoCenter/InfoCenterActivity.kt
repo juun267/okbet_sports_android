cae56a9e05d076c96ef9cbcf24e1338670505eff
Bill <h853047455@gmail.com>
2020-12-29 15:06:10 +0800

    [Add] add InfoCenterViewModel

--- app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterActivity.kt
@@ -1,10 +1,15 @@
 package org.cxct.sportlottery.ui.infoCenter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.base.BaseActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class InfoCenterActivity : AppCompatActivity() {
+
+class InfoCenterActivity : BaseActivity<InfoCenterViewModel>(InfoCenterViewModel::class) {
+
+    private val InfoCenterViewModel: InfoCenterViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_center)
---
+++ app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterViewModel.kt
@@ -0,0 +1,10 @@
+package org.cxct.sportlottery.ui.infoCenter
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.cxct.sportlottery.ui.base.BaseViewModel
+
+class InfoCenterViewModel: BaseViewModel()  {
+
+
+}
