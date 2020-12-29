885962e6b976a9cff9cf069438e04502bd93391b
edward_android <koyo98765@gmail.com>
2020-12-29 18:13:42 +0800

    [Add] user service money api

---
+++ app/src/main/java/org/cxct/sportlottery/network/user/UserMoneyResult.kt
@@ -0,0 +1,17 @@
+package org.cxct.sportlottery.network.user
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class UserMoneyResult(
+    @Json(name = "code")
+    val code: Int,
+    @Json(name = "msg")
+    val msg: String,
+    @Json(name = "success")
+    val success: Boolean,
+    @Json(name = "t")
+    val money: Double
+)
