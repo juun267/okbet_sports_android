241c9a41e5c2628f6c27fa5c4381f95e3dbe6c69
Bill <h853047455@gmail.com>
2020-12-30 13:29:06 +0800

    [Add] InfoCenterService

---
+++ app/src/main/java/org/cxct/sportlottery/network/InfoCenter/InfoCenterService.kt
@@ -0,0 +1,22 @@
+package org.cxct.sportlottery.network.InfoCenter
+
+import org.cxct.sportlottery.network.Constants.USER_NOTICE_LIST
+import org.cxct.sportlottery.network.Constants.USER_NOTICE_READED
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface InfoCenterService {
+
+    @POST(USER_NOTICE_LIST)
+    suspend fun getInfoList(
+        @Body infoCenterRequest: InfoCenterRequest
+    ): Response<InfoCenterResult>
+
+    @POST(USER_NOTICE_READED)
+    suspend fun setMsgReaded(
+        @Path("id") msgId:String,
+        @Body infoCenterRequest: InfoCenterRequest
+    ): Response<InfoCenterResult>
+}
