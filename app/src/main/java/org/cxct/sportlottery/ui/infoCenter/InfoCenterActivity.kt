b15eeacdf2ee9e4a935432ca56b7146659115af5
Bill <h853047455@gmail.com>
2021-01-06 15:24:03 +0800

    [Add] Add ObservableField in InfoCenter

--- app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterActivity.kt
@@ -2,36 +2,45 @@ package org.cxct.sportlottery.ui.infoCenter
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_info_center.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.databinding.ActivityInfoCenterBinding
 import org.cxct.sportlottery.ui.base.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class InfoCenterActivity : BaseActivity<InfoCenterViewModel>(InfoCenterViewModel::class) {
 
-    private val infoCenterViewModel: InfoCenterViewModel by viewModel()
     var adapter = InfoCenterAdapter()
+    private lateinit var infoCenterBinding: ActivityInfoCenterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info_center)
-
+        initDataBiding()
         initLiveData()
         initRecyclerView()
         initData()
     }
 
+    private fun initDataBiding() {
+        infoCenterBinding = DataBindingUtil.setContentView(this, R.layout.activity_info_center)
+        infoCenterBinding.apply {
+            infoCenterViewModel = this@InfoCenterActivity.viewModel
+            lifecycleOwner = this@InfoCenterActivity
+        }
+    }
+
     private fun initData() {
-        infoCenterViewModel.getUserMsgList(true, 0)
+        viewModel.getUserMsgList(true, 0)
     }
 
     private fun initLiveData() {
         //獲取訊息清單
-        infoCenterViewModel.userMsgList.observe(this@InfoCenterActivity, Observer {
+        viewModel.userMsgList.observe(this@InfoCenterActivity, Observer {
             val userMsgList = it ?: return@Observer
             if (adapter.data.isNotEmpty()) {
                 adapter.addData(userMsgList)//上拉加載
@@ -52,7 +61,7 @@ class InfoCenterActivity : BaseActivity<InfoCenterViewModel>(InfoCenterViewModel
         rv_data.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 if (!recyclerView.canScrollVertically(1))
-                    infoCenterViewModel.getUserMsgList(true, 0)
+                    viewModel.getUserMsgList(true, 0)
             }
         })
     }
