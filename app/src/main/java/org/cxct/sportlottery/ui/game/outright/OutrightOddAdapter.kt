f3ea82160c77ac9018432ddb522fae76e3521abb
edward_android <koyo98765@gmail.com>
2021-01-08 17:40:13 +0800

    [Add] outright odd list expand content

--- app/src/main/java/org/cxct/sportlottery/ui/game/outright/OutrightOddAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/outright/OutrightOddAdapter.kt
@@ -3,6 +3,7 @@ package org.cxct.sportlottery.ui.game.outright
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.itemview_league_odd.view.*
 import org.cxct.sportlottery.R
@@ -29,10 +30,42 @@ class OutrightOddAdapter : RecyclerView.Adapter<OutrightOddAdapter.ViewHolder>()
     override fun getItemCount(): Int = data.size
 
     class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val matchOddAdapter by lazy {
+            MatchOddAdapter()
+        }
 
         fun bind(item: LeagueOdd) {
             itemView.league_odd_name.text = item.league.name
             itemView.league_odd_count.text = item.matchOdds.size.toString()
+
+            setupMatchOddList(item)
+            setupMatchOddExpand(item)
+        }
+
+        private fun setupMatchOddList(item: LeagueOdd) {
+            itemView.league_odd_sub_list.apply {
+                this.layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                this.adapter = matchOddAdapter
+            }
+
+            matchOddAdapter.data = item.matchOdds
+        }
+
+        private fun setupMatchOddExpand(item: LeagueOdd) {
+            itemView.league_odd_sub_expand.setExpanded(item.isExpand, false)
+            itemView.setOnClickListener {
+                item.isExpand = !item.isExpand
+                itemView.league_odd_sub_expand.setExpanded(item.isExpand, true)
+                updateArrowExpand()
+            }
+        }
+
+        private fun updateArrowExpand() {
+            when (itemView.league_odd_sub_expand.isExpanded) {
+                true -> itemView.league_odd_arrow.setImageResource(R.drawable.ic_arrow_dark)
+                false -> itemView.league_odd_arrow.setImageResource(R.drawable.ic_arrow_down_dark)
+            }
         }
 
         companion object {
