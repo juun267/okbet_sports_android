1583ca6b3d1262fadfc6aa2bc2a3f372b022582f
Bill <h853047455@gmail.com>
2021-01-11 17:44:27 +0800

    [Add] button event

--- app/src/main/java/org/cxct/sportlottery/ui/base/BaseToolBarActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/base/BaseToolBarActivity.kt
@@ -1,12 +1,17 @@
 package org.cxct.sportlottery.ui.base
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import kotlinx.android.synthetic.main.activity_base_tool_bar.*
+import kotlinx.android.synthetic.main.view_tool_bar_drawer_layout.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.menu.MenuFragment
 import org.cxct.sportlottery.ui.odds.OddsDetailViewModel
+import org.cxct.sportlottery.util.MetricsUtil
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlin.reflect.KClass
 
@@ -24,6 +29,7 @@ abstract class BaseToolBarActivity<T : BaseViewModel>(claazz: KClass<T>) : BaseA
 
         frame_layout.addView(layoutInflater.inflate(resources.getLayout(setContentView()), null))
         tv_toolbar_title.text = setToolBarName()
+        initMenu()
     }
     /**
      * 回傳 Layout
@@ -34,4 +40,28 @@ abstract class BaseToolBarActivity<T : BaseViewModel>(claazz: KClass<T>) : BaseA
      * 回傳 標題名稱
      * */
     abstract fun setToolBarName():String
+
+    private fun initMenu() {
+        try {
+            //選單選擇結束要收起選單
+            val menuFrag = supportFragmentManager.findFragmentById(R.id.fragment_menu) as MenuFragment
+            menuFrag.setDownMenuListener { drawer_layout.closeDrawers() }
+
+            nav_right.layoutParams.width = MetricsUtil.getMenuWidth() //動態調整側邊欄寬
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        btn_toolbar_menu.setOnClickListener {
+            if (drawer_layout.isDrawerOpen(nav_right)) drawer_layout.closeDrawers()
+            else {
+                drawer_layout.openDrawer(nav_right)
+            }
+        }
+
+        btn_toolbar_back.setOnClickListener {
+            super.onBackPressed()
+        }
+
+    }
 }
