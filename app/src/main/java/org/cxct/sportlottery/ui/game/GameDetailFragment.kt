4f3e5932d0d95cdf8d4eda0b8bc2d42f6b07cd40
edward_android <koyo98765@gmail.com>
2021-01-12 16:09:18 +0800

    [Refactor] game detail fragment

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameDetailFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameDetailFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.SimpleItemAnimator
-import kotlinx.android.synthetic.main.fragment_game2.*
+import kotlinx.android.synthetic.main.fragment_game_detail.*
 import kotlinx.android.synthetic.main.itemview_league_odd.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.common.PlayType
@@ -23,10 +23,10 @@ import org.cxct.sportlottery.ui.home.MainViewModel
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Game2Fragment.newInstance] factory method to
+ * Use the [GameDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Game2Fragment : BaseFragment<MainViewModel>(MainViewModel::class) {
+class GameDetailFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
 
     private val playType: PlayType by lazy { PlayType.OU_HDP }
 
@@ -42,63 +42,25 @@ class Game2Fragment : BaseFragment<MainViewModel>(MainViewModel::class) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game2, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        (league_odd_sub_list.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        viewModel.oddsListResult.observe(this.viewLifecycleOwner, Observer {
-            if (it.success) {
-                it.oddsListData?.leagueOdds?.getOrNull(0)?.let { leagueOdd ->
-                    this.view?.let { view ->
-                        setupMatchOddList(view, leagueOdd)
-                        setupLeagueLayout(view, it.oddsListData.leagueOdds[0])
-                        setupEvent(view)
-                    }
-                }
-            }
-        })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        requireView().isFocusableInTouchMode = true
-        requireView().requestFocus()
-        requireView().setOnKeyListener(View.OnKeyListener { _, i, keyEvent ->
-            if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_BACK) {
-                backEvent()
-                return@OnKeyListener true
-            }
-            false
-        })
-    }
-
-    private fun setupMatchOddList(view: View, item: LeagueOdd) {
-        view.league_odd_sub_list.apply {
-            this.layoutManager =
-                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            this.adapter = matchOddAdapter
-        }
-        matchOddAdapter.data = item.matchOdds.apply { this[0].isExpand = true }
-        matchOddAdapter.playType = playType
-    }
-
-    private fun setupLeagueLayout(view: View, item: LeagueOdd) {
-        view.apply {
-            league_odd_name.text = item.league.name
-            league_odd_count.text = item.matchOdds.size.toString()
+        return inflater.inflate(R.layout.fragment_game_detail, container, false).apply {
+            setupEvent(this)
+            setupMatchOddList(this)
         }
     }
 
     private fun setupEvent(view: View) {
         view.league_odd_arrow.setOnClickListener {
             backEvent()
+        }
+    }
+
+    private fun setupMatchOddList(view: View) {
+        view.league_odd_sub_list.apply {
+            this.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            this.adapter = matchOddAdapter.apply {
+                this.playType = this@GameDetailFragment.playType
+            }
         }
     }
 
@@ -119,5 +81,50 @@ class Game2Fragment : BaseFragment<MainViewModel>(MainViewModel::class) {
             }
         })
         this.view?.startAnimation(animation)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        (league_odd_sub_list.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.oddsListResult.observe(this.viewLifecycleOwner, Observer {
+            if (it.success) {
+                it.oddsListData?.leagueOdds?.getOrNull(0)?.let { leagueOdd ->
+                    this.view?.let { view ->
+                        setupMatchOddList(leagueOdd)
+                        setupLeagueLayout(view, it.oddsListData.leagueOdds[0])
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setupLeagueLayout(view: View, item: LeagueOdd) {
+        view.apply {
+            league_odd_name.text = item.league.name
+            league_odd_count.text = item.matchOdds.size.toString()
+        }
+    }
+
+    private fun setupMatchOddList(item: LeagueOdd) {
+        matchOddAdapter.data = item.matchOdds.apply { this[0].isExpand = true }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireView().isFocusableInTouchMode = true
+        requireView().requestFocus()
+        requireView().setOnKeyListener(View.OnKeyListener { _, i, keyEvent ->
+            if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_BACK) {
+                backEvent()
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }
