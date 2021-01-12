309128078222ee9fe47b18cd2b759c2526047c68
Bill <h853047455@gmail.com>
2021-01-12 17:07:20 +0800

    [Add] Money recharge Main View

---
+++ app/src/main/java/org/cxct/sportlottery/ui/money/recharge/MoneyRechargeActivity.kt
@@ -0,0 +1,15 @@
+package org.cxct.sportlottery.ui.money.recharge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cxct.sportlottery.R
+
+class MoneyRechargeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_money_recharge)
+    }
+
+
+
+}
