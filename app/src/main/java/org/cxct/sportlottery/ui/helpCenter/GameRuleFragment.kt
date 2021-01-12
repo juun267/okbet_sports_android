3416f682d21fe0eb7e03f3ac73f1d73ebb64f707
Dean <b062020315@gmail.com>
2021-01-12 18:38:43 +0800

    [Add] HelpCenter back pressed event

--- app/src/main/java/org/cxct/sportlottery/ui/helpCenter/GameRuleFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/helpCenter/GameRuleFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
 
-class GameRuleFragment: BaseFragment<HelpCenterViewModel>(HelpCenterViewModel::class) {
+class GameRuleFragment : BaseFragment<HelpCenterViewModel>(HelpCenterViewModel::class) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_game_rule, container, false)
     }
--- app/src/main/java/org/cxct/sportlottery/ui/helpCenter/HelpCenterActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/helpCenter/HelpCenterActivity.kt
@@ -1,13 +1,20 @@
 package org.cxct.sportlottery.ui.helpCenter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_base_tool_bar.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseToolBarActivity
 
 class HelpCenterActivity : BaseToolBarActivity<HelpCenterViewModel>(HelpCenterViewModel::class) {
+
+    private val mNavController by lazy {
+        findNavController(R.id.help_center_container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupEvent()
     }
 
     override fun setContentView(): Int {
@@ -16,5 +23,29 @@ class HelpCenterActivity : BaseToolBarActivity<HelpCenterViewModel>(HelpCenterVi
 
     override fun setToolBarName(): String {
         return getString(R.string.help_center)
+    }
+
+
+    override fun onBackPressed() {
+        backEvent()
+    }
+
+    private fun setupEvent() {
+        btn_toolbar_back.setOnClickListener {
+            backEvent()
+        }
+    }
+
+    private fun backEvent() {
+        when (mNavController.currentDestination?.id) {
+            R.id.helpCenterFragment -> {
+                drawer_layout.closeDrawers()
+                super.onBackPressed()
+            }
+            R.id.gameRuleFragment -> {
+                val action = GameRuleFragmentDirections.actionGameRuleFragmentToHelpCenterFragment()
+                mNavController.navigate(action)
+            }
+        }
     }
 }
