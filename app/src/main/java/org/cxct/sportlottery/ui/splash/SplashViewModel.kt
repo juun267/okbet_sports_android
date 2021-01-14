5b2f900ee142bc3a95d543dd9fbf04ef3fd18cc2
edward_android <koyo98765@gmail.com>
2021-01-14 16:47:48 +0800

    [Refactor] remove try-catch which never called

--- app/src/main/java/org/cxct/sportlottery/ui/splash/SplashViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/splash/SplashViewModel.kt
@@ -16,16 +16,12 @@ class SplashViewModel : BaseViewModel() {
     private val _configResult = MutableLiveData<ConfigResult?>()
 
     fun getConfig() {
-        try {
-            viewModelScope.launch {
-                val result = doNetwork {
-                    OneBoSportApi.indexService.getConfig()
-                }
-                sConfigData = result?.configData
-                _configResult.postValue(result)
+        viewModelScope.launch {
+            val result = doNetwork {
+                OneBoSportApi.indexService.getConfig()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
+            sConfigData = result?.configData
+            _configResult.postValue(result)
         }
     }
 
