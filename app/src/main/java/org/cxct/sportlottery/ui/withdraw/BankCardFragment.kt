f6f7f44ac91c1a68010c773e4ea9e57d08289f02
Dean <b062020315@gmail.com>
2021-01-14 15:26:45 +0800

    [Refactor] rename BindBankCardFragment to BankCardFragment

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BindBankCardFragment.newInstance] factory method to
+ * Use the [BankCardFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BindBankCardFragment : Fragment() {
+class BankCardFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class BindBankCardFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BindBankCardFragment().apply {
+            BankCardFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
