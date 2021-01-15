68e7a13016e02861e08c9c9e2dcb2e8492142f45
Dean <b062020315@gmail.com>
2021-01-15 14:12:24 +0800

    [Add] withdraw page back pressed event

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawActivity.kt
@@ -1,15 +1,13 @@
 package org.cxct.sportlottery.ui.withdraw
 
-import android.content.Context
 import android.os.Bundle
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseToolBarActivity
 
 class WithdrawActivity : BaseToolBarActivity<WithdrawViewModel>(WithdrawViewModel::class) {
 
-    private  val  mNavController by lazy {
+    private val mNavController by lazy {
         findNavController(R.id.withdraw_container)
     }
 
@@ -23,5 +21,13 @@ class WithdrawActivity : BaseToolBarActivity<WithdrawViewModel>(WithdrawViewMode
 
     override fun setToolBarName(): String {
         return getString(R.string.withdraw)
+    }
+
+    override fun onBackPressed() {
+        if (mNavController.previousBackStackEntry != null) {
+            mNavController.popBackStack()
+            return
+        }
+        super.onBackPressed()
     }
 }
