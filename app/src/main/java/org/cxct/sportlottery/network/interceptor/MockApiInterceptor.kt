7369e75b0b8f598d4b006414d958d89bdc20e0d7
Dean <b062020315@gmail.com>
2021-01-15 15:48:44 +0800

    [Add] bank delete mock file

--- app/src/main/java/org/cxct/sportlottery/network/interceptor/MockApiInterceptor.kt
+++ app/src/main/java/org/cxct/sportlottery/network/interceptor/MockApiInterceptor.kt
@@ -9,6 +9,7 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.cxct.sportlottery.BuildConfig
+import org.cxct.sportlottery.network.Constants.BANK_DELETE
 import org.cxct.sportlottery.network.Constants.BANK_MY
 import org.cxct.sportlottery.network.Constants.INDEX_CHECK_EXIST
 import org.cxct.sportlottery.network.Constants.INDEX_CONFIG
@@ -141,6 +142,9 @@ class MockApiInterceptor(private val context: Context) : Interceptor {
                 }
                 path.contains(BANK_MY) -> {
                     response = getMockJsonData(request, "user_bank_my")
+                }
+                path.contains(BANK_DELETE) -> {
+                    response = getMockJsonData(request, "user_bank_delete")
                 }
             }
         }
