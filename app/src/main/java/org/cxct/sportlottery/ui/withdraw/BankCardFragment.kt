59ad961502c629451b98f284c7ece51f16de87c8
Dean <b062020315@gmail.com>
2021-01-15 14:13:43 +0800

    [Add] init view when navigate to bank card page

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
@@ -1,33 +1,32 @@
 package org.cxct.sportlottery.ui.withdraw
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_bank_card.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.repository.sLoginData
+import org.cxct.sportlottery.ui.base.BaseFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [BankCardFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BankCardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class BankCardFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::class) {
+
+    private val args: BankCardFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +34,41 @@ class BankCardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bank_card, container, false)
+        return inflater.inflate(R.layout.fragment_bank_card, container, false).apply {
+
+            setupTitle()
+
+            setupInitData(this)
+
+        }
+    }
+
+    private fun setupInitData(view: View) {
+        val initData = args.editBankCard
+        initData?.let {
+            view.apply {
+                btn_delete_bank_card.visibility = View.VISIBLE
+                tv_bank_name.text = initData.bankName
+                edit_create_name.setText(sLoginData?.fullName)
+                edit_bank_card_number.setText(initData.cardNo)
+                edit_network_point.setText("") // TODO Dean : 沒有欄位可以取得, 待釐清
+            }
+            return@setupInitData
+        }
+        view.btn_delete_bank_card.visibility = View.GONE
+    }
+
+    private fun setupTitle(){
+        if (args.editBankCard != null){
+            (this@BankCardFragment.activity as WithdrawActivity).setToolBarName(getString(R.string.edit_bank_card))
+        }else{
+            (this@BankCardFragment.activity as WithdrawActivity).setToolBarName(getString(R.string.add_credit_card))
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BindBankCardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BankCardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() {
+        }
     }
 }
