dac6dc2085bb63d4845ba025a3fd0fb63adc1e6e
Bill <h853047455@gmail.com>
2021-01-15 13:25:59 +0800

    [Refactor] MoneyRechargeActivity

--- app/src/main/java/org/cxct/sportlottery/ui/money/recharge/MoneyRechargeActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/recharge/MoneyRechargeActivity.kt
@@ -1,15 +1,25 @@
 package org.cxct.sportlottery.ui.money.recharge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.base.BaseToolBarActivity
 
-class MoneyRechargeActivity : AppCompatActivity() {
+class MoneyRechargeActivity : BaseToolBarActivity<MoneyRechViewModel>(MoneyRechViewModel::class) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_money_recharge)
+        initData()
     }
 
+    private fun initData() {
+        viewModel.getRechCfg()
+    }
 
+    override fun setContentView(): Int {
+        return R.layout.activity_money_recharge
+    }
+
+    override fun setToolBarName(): String {
+        return "资金"
+    }
 
 }
