26756b7f5c81dbf97d27038363344b2f56a37932
Dean <b062020315@gmail.com>
2021-01-18 10:23:52 +0800

    [Add] note todo in BankCardEdit page

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
@@ -17,15 +17,7 @@ import org.cxct.sportlottery.repository.sUserInfo
 import org.cxct.sportlottery.ui.base.BaseFragment
 import org.cxct.sportlottery.util.MD5Util.MD5Encode
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BankCardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//TODO Dean : 銀行卡綁定完成後因進入的頁面不同而回到不同頁面. Ex:提款、提款設置
 class BankCardFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::class) {
 
     private val mNavController by lazy { findNavController() }
