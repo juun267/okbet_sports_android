958d79b464ee19bb5ebb5995a3d3a7cd08e7fb67
Bill <h853047455@gmail.com>
2021-01-20 10:45:00 +0800

    [Fix] Discussion

--- app/src/main/java/org/cxct/sportlottery/network/money/MoneyRechCfgData.kt
+++ app/src/main/java/org/cxct/sportlottery/network/money/MoneyRechCfgData.kt
@@ -7,13 +7,13 @@ import java.io.Serializable
 @JsonClass(generateAdapter = true)
 data class MoneyRechCfgData(
     @Json(name = "banks")
-    var banks: MutableList<MoneyRechCfg.Bank>,//银行lfny
+    val banks: MutableList<MoneyRechCfg.Bank>,//银行lfny
     @Json(name = "rechCfgs")
-    var rechCfgs: MutableList<MoneyRechCfg.RechConfig>,//充值配置
+    val rechCfgs: MutableList<MoneyRechCfg.RechConfig>,//充值配置
     @Json(name = "rechTypes")
-    var rechTypes: MutableList<MoneyRechCfg.RechType>,//充值类型，name：名称，value：类型（onlinePayment：在线支付，bankTransfer：银行汇款，alipay：支付宝支付，weixin：微信支付，cft：财付通）
+    val rechTypes: MutableList<MoneyRechCfg.RechType>,//充值类型，name：名称，value：类型（onlinePayment：在线支付，bankTransfer：银行汇款，alipay：支付宝支付，weixin：微信支付，cft：财付通）
     @Json(name = "withdrawCfg")
-    var withdrawCfg: MoneyRechCfg.WithdrawCfg//提现相关设定 单笔提现最大金额 单笔提现最小金额 提现费率
+    val withdrawCfg: MoneyRechCfg.WithdrawCfg//提现相关设定 单笔提现最大金额 单笔提现最小金额 提现费率
 )
 
 
