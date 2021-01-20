26b2610838772a4a85d27f853bce7b25bf790e0a
Sky <simon@cxteam.io>
2021-01-20 19:35:53 +0800

    [Fixed] 修正 UploadImg API 傳入參數

--- app/src/main/java/org/cxct/sportlottery/network/uploadImg/UploadImgRequest.kt
+++ app/src/main/java/org/cxct/sportlottery/network/uploadImg/UploadImgRequest.kt
@@ -5,10 +5,9 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import org.cxct.sportlottery.repository.PLATFORM_CODE
 import org.cxct.sportlottery.repository.PROJECT_CODE
-import org.cxct.sportlottery.repository.sLoginData
 import java.io.File
 
-class UploadImgRequest(val userId: String, val path: String, ) {
+class UploadImgRequest(val userId: String, val path: String) {
 
     fun toParts(): List<MultipartBody.Part> {
         val file = File(path)
@@ -18,7 +17,7 @@ class UploadImgRequest(val userId: String, val path: String, ) {
         return MultipartBody.Builder().setType(MultipartBody.FORM)
             .addFormDataPart("projectCode", PROJECT_CODE) //项目代码
             .addFormDataPart("platformCode", PLATFORM_CODE) //平台代码
-            .addFormDataPart("userId", sLoginData?.userId.toString())
+            .addFormDataPart("userId", userId)
             .addFormDataPart("expireAfterDays", "0") //多久过期（天）0：不过期，N：N天后过期（1<=N<=365）
             .addFormDataPart("file", file.name, requestFile)
             .build().parts
