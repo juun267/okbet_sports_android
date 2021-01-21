f41664d40f47eadc0251542b4ec41cea3f440d1f
Bill <h853047455@gmail.com>
2021-01-21 17:13:45 +0800

    [Add] API Add & OnlinePay

---
+++ app/src/main/java/org/cxct/sportlottery/network/money/MoneyAddRequest.kt
@@ -0,0 +1,12 @@
+package org.cxct.sportlottery.network.money
+
+data class MoneyAddRequest(
+    val rechCfgId: Int,//充值配置id
+    val bankCode: String,//银行代码
+    val depositMoney: Int,//充值金额
+    val payer: String,//充值账号
+    val payerName: String,//充值人名称
+    val payerBankName: String,//充值银行名称
+    val payerInfo: String,//充值附加信息
+    val depositDate: String,//充值日期
+)
--- app/src/main/java/org/cxct/sportlottery/network/money/MoneyService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/money/MoneyService.kt
@@ -2,12 +2,32 @@ package org.cxct.sportlottery.network.money
 
 import org.cxct.sportlottery.network.Constants
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface MoneyService {
 
     @GET(Constants.RECHARGE_CONFIG_MAP)
     suspend fun getRechCfg(): Response<MoneyRechCfgResult>
+
+    @POST(Constants.USER_RECHARGE_ADD)
+    suspend fun rechargeAdd(
+        @Body moneyAddRequest: MoneyAddRequest
+    ): Response<MoneyAddResult>
+
+    @POST(Constants.USER_RECHARGE_ONLINE_PAY)
+    suspend fun rechargeOnlinePay(
+        @Body moneyAddRequest: MoneyAddRequest
+//        @Query("rechCfgId") rechCfgId: Int,//充值配置id
+//        @Query("bankCode") bankCode: String,//银行代码
+//        @Query("depositMoney") depositMoney: Int,//充值金额
+//        @Query("payer") payer: String,//充值账号
+//        @Query("payerName") payerName: String,//充值人名称
+//        @Query("payerBankName") payerBankName: String,//充值银行名称
+//        @Query("payerInfo") payerInfo: String,//充值附加信息
+//        @Query("depositDate") depositDate: String,//充值日期
+    ): Response<MoneyAddResult>
 
 }
