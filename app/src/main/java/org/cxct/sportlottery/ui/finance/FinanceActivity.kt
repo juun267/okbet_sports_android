f36242d53f17be777a935443dc6c10744ee4b91b
edward_android <koyo98765@gmail.com>
2021-01-22 14:14:27 +0800

    [Add] finance activity

---
+++ app/src/main/java/org/cxct/sportlottery/ui/finance/FinanceActivity.kt
@@ -0,0 +1,13 @@
+package org.cxct.sportlottery.ui.finance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cxct.sportlottery.R
+
+class FinanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_finance)
+    }
+}
