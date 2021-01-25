98732673264d2553da75a2ebba74357e6f8f475f
Bill <h853047455@gmail.com>
2021-01-25 09:48:31 +0800

    [Add] Add an entrance of recharge

--- app/src/main/java/org/cxct/sportlottery/ui/menu/MenuFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/menu/MenuFragment.kt
@@ -108,9 +108,7 @@ class MenuFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
         }
 
         menu_news.setOnClickListener {
-//            startActivity(Intent(context, InfoCenterActivity::class.java))
-//            mDownMenuListener?.onClick(menu_news)
-            startActivity(Intent(context, MoneyRechargeActivity::class.java))//TODO Bill Test
+            startActivity(Intent(context, InfoCenterActivity::class.java))
             mDownMenuListener?.onClick(menu_news)
         }
     }
