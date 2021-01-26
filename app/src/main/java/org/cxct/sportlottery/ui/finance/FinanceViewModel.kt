e594930e358a53f89272aa3d3587d9cb261f0de8
edward_android <koyo98765@gmail.com>
2021-01-26 15:05:17 +0800

    [Add] recharge row data transform mapping display column

--- app/src/main/java/org/cxct/sportlottery/ui/finance/FinanceViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/finance/FinanceViewModel.kt
@@ -11,6 +11,7 @@ import org.cxct.sportlottery.network.money.list.RechargeListRequest
 import org.cxct.sportlottery.network.money.list.RechargeListResult
 import org.cxct.sportlottery.network.user.money.UserMoneyResult
 import org.cxct.sportlottery.ui.base.BaseViewModel
+import org.cxct.sportlottery.util.TimeUtil
 
 class FinanceViewModel(private val androidContext: Context) : BaseViewModel() {
 
@@ -64,6 +65,20 @@ class FinanceViewModel(private val androidContext: Context) : BaseViewModel() {
                     RechargeListRequest()
                 )
             }
+
+            result?.rows?.map {
+                it.rechState = when (it.status) {
+                    RechargeState.SUCCESS.code -> androidContext.getString(R.string.recharge_state_success)
+                    RechargeState.FAILED.code -> androidContext.getString(R.string.recharge_state_failed)
+                    RechargeState.PROCESSING.code -> androidContext.getString(R.string.recharge_state_processing)
+                    RechargeState.RECHARGING.code -> androidContext.getString(R.string.recharge_state_recharging)
+                    else -> ""
+                }
+
+                it.rechDateStr = TimeUtil.timeFormat(it.rechTime, "yyyy-MM-dd")
+                it.rechTimeStr = TimeUtil.timeFormat(it.rechTime, "HH:mm:ss")
+            }
+
             _userRechargeResult.postValue(result)
         }
     }
