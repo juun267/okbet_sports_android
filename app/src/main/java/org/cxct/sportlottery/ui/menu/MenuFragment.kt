3049d347a9a63bcbaa627b3bf6efdda140df1f96
Sky <simon@cxteam.io>
2021-02-01 14:29:59 +0800

    [Refactor] 側邊欄欄位刪減

--- app/src/main/java/org/cxct/sportlottery/ui/menu/MenuFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/menu/MenuFragment.kt
@@ -12,15 +12,10 @@ import kotlinx.android.synthetic.main.fragment_menu.*
 import org.cxct.sportlottery.BuildConfig
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
-import org.cxct.sportlottery.ui.bet.record.BetRecordActivity
-import org.cxct.sportlottery.ui.helpCenter.HelpCenterActivity
 import org.cxct.sportlottery.ui.home.MainActivity
 import org.cxct.sportlottery.ui.home.MainViewModel
-import org.cxct.sportlottery.ui.infoCenter.InfoCenterActivity
 import org.cxct.sportlottery.ui.profileCenter.ProfileCenterActivity
 import org.cxct.sportlottery.ui.results.ResultsSettlementActivity
-import org.cxct.sportlottery.ui.withdraw.BankActivity
-import org.cxct.sportlottery.ui.withdraw.WithdrawActivity
 import org.cxct.sportlottery.util.ArithUtil
 import org.cxct.sportlottery.util.LanguageManager
 
@@ -71,8 +66,13 @@ class MenuFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
             ChangeLanguageDialog().show(parentFragmentManager, null)
         }
 
-        btn_close.setOnClickListener {
-            mDownMenuListener?.onClick(btn_close)
+        menu_profile_center.setOnClickListener {
+            startActivity(Intent(context, ProfileCenterActivity::class.java))
+            mDownMenuListener?.onClick(menu_profile_center)
+        }
+
+        menu_member_level.setOnClickListener {
+            //TODO 會員層級
         }
 
         menu_game_result.setOnClickListener {
@@ -87,24 +87,6 @@ class MenuFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
             }
         }
 
-        menu_bet_history.setOnClickListener {
-            startActivity(Intent(context, BetRecordActivity::class.java))
-        }
-
-        menu_profile_center.setOnClickListener {
-            startActivity(Intent(context, ProfileCenterActivity::class.java))
-            mDownMenuListener?.onClick(menu_profile_center)
-        }
-
-        menu_news.setOnClickListener {
-            startActivity(Intent(context, InfoCenterActivity::class.java))
-            mDownMenuListener?.onClick(menu_news)
-        }
-
-        menu_help.setOnClickListener {
-            startActivity(Intent(context, HelpCenterActivity::class.java))
-            mDownMenuListener?.onClick(menu_help)
-        }
     }
 
     private fun setupSelectLanguage() {
