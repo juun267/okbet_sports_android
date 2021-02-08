ac4d25084dea9c056539779c99bb38613f5a48b9
Bill <h853047455@gmail.com>
2021-02-08 14:07:16 +0800

    [Add] FeedbackSuggestFragment

--- app/src/main/java/org/cxct/sportlottery/ui/feedback/FeedbackSuggestFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/feedback/FeedbackSuggestFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.fragment_feedback_suggest.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
+import org.cxct.sportlottery.ui.finance.FinanceFragmentDirections
 
 class FeedbackSuggestFragment : BaseFragment<FeedbackViewModel>(FeedbackViewModel::class) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val navController by lazy {
+        view?.findNavController()
     }
 
     override fun onCreateView(
@@ -19,6 +23,26 @@ class FeedbackSuggestFragment : BaseFragment<FeedbackViewModel>(FeedbackViewMode
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_feedback_suggest, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initButton()
+        initView()
+    }
+
+    private fun initView() {
+        btn_sugession.isSelected = true
+        btn_record.isSelected = false
+    }
+
+    private fun initButton() {
+        btn_submit.setOnClickListener {
+            navController?.navigate(R.id.action_feedbackSuggestFragment_to_feedbackSubmitFragment)
+        }
+        btn_record.setOnClickListener {
+            navController?.navigate(R.id.action_feedbackSuggestFragment_to_feedbackRecordListFragment)
+        }
     }
 
 }
