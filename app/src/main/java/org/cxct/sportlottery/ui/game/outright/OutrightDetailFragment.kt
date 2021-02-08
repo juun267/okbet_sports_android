66d9ca69c30a9d4891c0dccc5fa397817716e424
edward_android <koyo98765@gmail.com>
2021-02-08 17:51:11 +0800

    [Fix] outright odd detail list content

--- app/src/main/java/org/cxct/sportlottery/ui/game/outright/OutrightDetailFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/outright/OutrightDetailFragment.kt
@@ -1,32 +1,26 @@
 package org.cxct.sportlottery.ui.game.outright
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_outright_detail.*
+import kotlinx.android.synthetic.main.fragment_outright_detail.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.network.outright.odds.OutrightOddsListResult
+import org.cxct.sportlottery.ui.base.BaseSocketFragment
+import org.cxct.sportlottery.ui.home.MainViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class OutrightDetailFragment : BaseSocketFragment<MainViewModel>(MainViewModel::class) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OutrightDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OutrightDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val outrightOddAdapter by lazy {
+        OutrightOddAdapter().apply {
+            outrightOddListener = OutrightOddAdapter.OutrightOddListener {
+                viewModel.updateOutrightOddsSelectedState(it)
+            }
         }
     }
 
@@ -36,27 +30,40 @@ class OutrightDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_outright_detail, container, false).apply {
-
+            setupOutrightOddList(this)
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OutrightDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OutrightDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun setupOutrightOddList(view: View) {
+        view.outright_detail_list.apply {
+            this.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            this.adapter = outrightOddAdapter
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.outrightOddsListResult.observe(this.viewLifecycleOwner, Observer {
+            if (it != null && it.success) {
+                setupOutrightOddList(it)
             }
+        })
+    }
+
+    private fun setupOutrightOddList(outrightOddsListResult: OutrightOddsListResult) {
+        val matchOdd = outrightOddsListResult.outrightOddsListData?.leagueOdds?.get(0)?.matchOdds?.get(0)
+
+        outright_detail_title.text =
+            outrightOddsListResult.outrightOddsListData?.leagueOdds?.get(0)?.league?.name
+
+        outrightOddAdapter.data =matchOdd?.displayList?: listOf()
     }
 }
