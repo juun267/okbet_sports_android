526be8f994d983849b6142372facbee344a5ed01
Dean <b062020315@gmail.com>
2021-02-19 15:09:57 +0800

    [Refactor] withdraw info structure

--- app/src/main/java/org/cxct/sportlottery/network/user/setWithdrawInfo/WithdrawInfoRequest.kt
+++ app/src/main/java/org/cxct/sportlottery/network/user/setWithdrawInfo/WithdrawInfoRequest.kt
@@ -7,17 +7,17 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class WithdrawInfoRequest(
     @Json(name = "email")
-    val email: String,
+    val email: String?,
     @Json(name = "fullName")
-    val fullName: String,
+    val fullName: String?,
     @Json(name = "fundPwd")
-    val fundPwd: String,
+    val fundPwd: String?,
     @Json(name = "phone")
-    val phone: String,
+    val phone: String?,
     @Json(name = "qq")
-    val qq: String,
+    val qq: String?,
     @Json(name = "userId")
     val userId: Int,
     @Json(name = "wechat")
-    val wechat: String
+    val wechat: String?
 )
