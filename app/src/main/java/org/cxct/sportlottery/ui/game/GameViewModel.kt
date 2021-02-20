15be0db313e668b641f0c8cbd66270857433fe6d
edward_android <koyo98765@gmail.com>
2021-02-20 13:15:23 +0800

    [Add] move parlay count to game activity from main

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.cxct.sportlottery.network.OneBoSportApi
+import org.cxct.sportlottery.network.common.SportType
+import org.cxct.sportlottery.network.sport.SportMenuResult
 import org.cxct.sportlottery.repository.BetInfoRepository
 import org.cxct.sportlottery.repository.LoginRepository
 import org.cxct.sportlottery.repository.SportMenuRepository
@@ -29,6 +31,8 @@ class GameViewModel(
 
     val messageListResult: LiveData<List<String>>
         get() = _messageListResult
+    val sportMenuResult: LiveData<SportMenuResult>
+        get() = _sportMenuResult
 
     val countInPlay: LiveData<Int>
         get() = _countInPlay
@@ -43,8 +47,21 @@ class GameViewModel(
     val countAll: LiveData<Int>
         get() = _countAll
 
+    val countAtStart: LiveData<Int>
+        get() = _countAtStart
+    val countParlayFootball: LiveData<Int>
+        get() = _countParlayFootball
+    val countParlayBasketball: LiveData<Int>
+        get() = _countParlayBasketball
+    val countParlayTennis: LiveData<Int>
+        get() = _countParlayTennis
+    val countParlayBadminton: LiveData<Int>
+        get() = _countParlayBadminton
+    val countParlayVolleyball: LiveData<Int>
+        get() = _countParlayVolleyball
 
     private val _messageListResult = MutableLiveData<List<String>>()
+    private val _sportMenuResult = MutableLiveData<SportMenuResult>()
 
     private val _countInPlay = MutableLiveData<Int>().apply { value = 0 }
     private val _countToday = MutableLiveData<Int>().apply { value = 0 }
@@ -52,6 +69,13 @@ class GameViewModel(
     private val _countParlay = MutableLiveData<Int>().apply { value = 0 }
     private val _countOutright = MutableLiveData<Int>().apply { value = 0 }
     private val _countAll = MutableLiveData<Int>().apply { value = 0 }
+
+    private val _countAtStart = MutableLiveData<Int>().apply { value = 0 }
+    private val _countParlayFootball = MutableLiveData<Int>().apply { value = 0 }
+    private val _countParlayBasketball = MutableLiveData<Int>().apply { value = 0 }
+    private val _countParlayTennis = MutableLiveData<Int>().apply { value = 0 }
+    private val _countParlayBadminton = MutableLiveData<Int>().apply { value = 0 }
+    private val _countParlayVolleyball = MutableLiveData<Int>().apply { value = 0 }
 
 
     private fun checkToken() {
@@ -95,23 +119,50 @@ class GameViewModel(
                     todayStart.toString()
                 )
             }?.let { result ->
-
-                _countInPlay.value = (result.sportMenuData?.menu?.inPlay?.items?.sumBy { it.num }
-                    ?: 0)
-                _countToday.value = (result.sportMenuData?.menu?.today?.items?.sumBy { it.num }
-                    ?: 0)
-                _countEarly.value = (result.sportMenuData?.menu?.early?.items?.sumBy { it.num }
-                    ?: 0)
-                _countParlay.value = (result.sportMenuData?.menu?.parlay?.items?.sumBy { it.num }
-                    ?: 0)
-                _countOutright.value =
-                    (result.sportMenuData?.menu?.outright?.items?.sumBy { it.num }
-                        ?: 0)
-                _countAll.value = (
-                        (_countInPlay.value ?: 0) + (_countToday.value ?: 0) + (_countEarly.value
-                            ?: 0) + (_countParlay.value ?: 0) + (_countOutright.value ?: 0)
-                        )
+                if (result.success) {
+                    updateMatchTypeCount(result)
+                    updateParlayCount(result)
+                    _sportMenuResult.postValue(result)
+                }
             }
         }
+    }
+
+    private fun updateMatchTypeCount(sportMenuResult: SportMenuResult) {
+        _countInPlay.value =
+            sportMenuResult.sportMenuData?.menu?.inPlay?.items?.sumBy { it.num } ?: 0
+
+        _countToday.value = sportMenuResult.sportMenuData?.menu?.today?.items?.sumBy { it.num } ?: 0
+
+        _countEarly.value = sportMenuResult.sportMenuData?.menu?.early?.items?.sumBy { it.num } ?: 0
+
+        _countParlay.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.sumBy { it.num } ?: 0
+
+        _countOutright.value =
+            sportMenuResult.sportMenuData?.menu?.outright?.items?.sumBy { it.num } ?: 0
+
+        _countAll.value = (_countInPlay.value ?: 0) + (_countToday.value ?: 0) + (_countEarly.value
+            ?: 0) + (_countParlay.value ?: 0) + (_countOutright.value ?: 0)
+
+    }
+
+    private fun updateParlayCount(sportMenuResult: SportMenuResult) {
+        _countAtStart.value = sportMenuResult.sportMenuData?.atStart?.num ?: 0
+        _countParlayFootball.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.find { it.code == SportType.FOOTBALL.code }?.num
+                ?: 0
+        _countParlayBasketball.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.find { it.code == SportType.BASKETBALL.code }?.num
+                ?: 0
+        _countParlayTennis.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.find { it.code == SportType.TENNIS.code }?.num
+                ?: 0
+        _countParlayBadminton.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.find { it.code == SportType.BADMINTON.code }?.num
+                ?: 0
+        _countParlayVolleyball.value =
+            sportMenuResult.sportMenuData?.menu?.parlay?.items?.find { it.code == SportType.VOLLEYBALL.code }?.num
+                ?: 0
     }
 }
