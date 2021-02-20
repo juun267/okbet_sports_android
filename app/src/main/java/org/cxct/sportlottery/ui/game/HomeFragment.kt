8d6ad90b2181d3f0cd43ed4787bcb90c00379a38
edward_android <koyo98765@gmail.com>
2021-02-20 13:41:19 +0800

    [Add] move home fragment to game activity from main

---
+++ app/src/main/java/org/cxct/sportlottery/ui/game/HomeFragment.kt
@@ -0,0 +1,19 @@
+package org.cxct.sportlottery.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.base.BaseFragment
+
+
+class HomeFragment : BaseFragment<GameViewModel>(GameViewModel::class) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
