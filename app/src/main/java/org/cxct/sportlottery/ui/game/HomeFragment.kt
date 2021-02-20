3c217448f86505066a2313b788227af5f49fe8e7
edward_android <koyo98765@gmail.com>
2021-02-20 13:53:55 +0800

    [Add] move parlay count to new home fragment form main

--- app/src/main/java/org/cxct/sportlottery/ui/game/HomeFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.home_game_table.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
 
@@ -15,5 +18,38 @@ class HomeFragment : BaseFragment<GameViewModel>(GameViewModel::class) {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initObserver()
+    }
+
+    private fun initObserver() {
+        viewModel.countAtStart.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            card_game_soon.tv_count.text = it.toString()
+        })
+        viewModel.countParlayFootball.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            tv_football_count.text = it.toString()
+        })
+        viewModel.countParlayBasketball.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            tv_basketball_count.text = it.toString()
+        })
+        viewModel.countParlayTennis.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            card_tennis.tv_count.text = it.toString()
+        })
+        viewModel.countParlayBadminton.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            card_badminton.tv_count.text = it.toString()
+        })
+        viewModel.countParlayVolleyball.observe(viewLifecycleOwner, Observer {
+            hideLoading()
+            card_volleyball.tv_count.text = it.toString()
+        })
     }
 }
