9cfb79cf333f25c1656a7977d8b98d3acce09c28
Dean <b062020315@gmail.com>
2021-02-23 11:12:30 +0800

    [Fix] 7520 【Andriod】開啟「註冊」頁面，各欄位內提示錯誤

--- app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterViewModel.kt
@@ -182,6 +182,7 @@ class RegisterViewModel(
 
     private fun checkFullName(context: Context, fullName: String?): String? {
         return when {
+            fullName.isNullOrBlank() -> context.getString(R.string.error_input_empty)
             !VerifyConstUtil.verifyFullName(fullName ?: "") -> context.getString(R.string.error_incompatible_format)
             else -> null
         }
@@ -189,7 +190,8 @@ class RegisterViewModel(
 
     private fun checkFundPwd(context: Context, fundPwd: String?): String? {
         return when {
-            !VerifyConstUtil.verifyPayPwd(fundPwd ?: "") -> context.getString(R.string.hint_withdrawal_pwd)
+            fundPwd.isNullOrBlank() -> context.getString(R.string.error_input_empty)
+            !VerifyConstUtil.verifyPayPwd(fundPwd) -> context.getString(R.string.error_withdrawal_pwd)
             else -> null
         }
     }
@@ -236,7 +238,7 @@ class RegisterViewModel(
 
     private fun checkFacebook(context: Context, facebook: String?): String? {
         return when {
-            facebook.isNullOrBlank() -> context.getString(R.string.hint_facebook)
+            facebook.isNullOrBlank() -> context.getString(R.string.error_input_empty)
             !VerifyConstUtil.verifyFacebook(facebook) -> context.getString(R.string.error_facebook)
             else -> null
         }
@@ -244,7 +246,7 @@ class RegisterViewModel(
 
     private fun checkWhatsApp(context: Context, whatsApp: String?): String? {
         return when {
-            whatsApp.isNullOrBlank() -> context.getString(R.string.hint_whats_app)
+            whatsApp.isNullOrBlank() -> context.getString(R.string.error_input_empty)
             !VerifyConstUtil.verifyWhatsApp(whatsApp) -> context.getString(R.string.error_whats_app)
             else -> null
         }
