364222483a5d91a188316b0b09837e102210e438
Cheryl <a222025115@gmail.com>
2021-02-23 15:05:13 +0800

    added background & title image.

--- app/src/main/java/org/cxct/sportlottery/ui/main/next/DZFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/main/next/DZFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_dz.*
 import org.cxct.sportlottery.R
 
 class DZFragment : Fragment() {
@@ -12,7 +17,49 @@ class DZFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dz, container, false)
     }
 
+    private val tabIconList by lazy {
+        listOf(R.drawable.ic_image_load, R.drawable.ic_cp, R.drawable.ic_live, R.drawable.ic_qp, R.drawable.ic_dz, R.drawable.ic_by)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view_pager.adapter = DzVpAdapter(tabIconList)
+
+        // attaching tab mediator
+        TabLayoutMediator(tab_layout, view_pager) { tab, position ->
+            tab.setCustomView(R.layout.tab_item_game2)
+            tab.customView?.apply {
+                findViewById<ImageView>(R.id.iv_icon)?.setImageResource(tabIconList[position])
+            }
+        }.attach()
+
     }
+
+}
+
+class DzVpAdapter(private val titleList: List<Int>): RecyclerView.Adapter<DzVpAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ItemViewHolder {
+        val layout = LayoutInflater.from(viewGroup.context).inflate(R.layout.view_dz_adapter, viewGroup, false)
+        return ItemViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return titleList.size
+    }
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind() {
+            itemView.apply {
+
+            }
+        }
+
+    }
+
 }
