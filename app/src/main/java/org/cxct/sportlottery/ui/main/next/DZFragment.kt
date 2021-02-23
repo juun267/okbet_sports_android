946adff5dc3e2a0e70ebe8fa9888e1ab151d6ea9
Cheryl <a222025115@gmail.com>
2021-02-23 18:16:47 +0800

    clean code.

--- app/src/main/java/org/cxct/sportlottery/ui/main/next/DZFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/main/next/DZFragment.kt
@@ -3,21 +3,16 @@ package org.cxct.sportlottery.ui.main.next
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.content_dz_adapter.view.*
 import kotlinx.android.synthetic.main.fragment_dz.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
 import org.cxct.sportlottery.ui.home.MainViewModel
-import kotlin.math.abs
 
 class DZFragment : BaseFragment<MainViewModel>(MainViewModel::class)  {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,24 +22,12 @@ class DZFragment : BaseFragment<MainViewModel>(MainViewModel::class)  {
     private val tabIconList by lazy {
         listOf(R.drawable.ic_image_load, R.drawable.ic_cp, R.drawable.ic_live, R.drawable.ic_qp, R.drawable.ic_dz, R.drawable.ic_by)
     }
-    private var x1 = 0f
-    private var x2 = 0f
-    private val MIN_DISTANCE = 150
-
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
         view_pager.adapter = DzVpAdapter(tabIconList)
-
-        view_pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                viewModel.setSwipeable((view_pager.currentItem == 0) || (view_pager.currentItem == tabIconList.size - 1))
-            }
-        })
-
 
         // attaching tab mediator
         TabLayoutMediator(tab_layout, view_pager) { tab, position ->
@@ -57,45 +40,6 @@ class DZFragment : BaseFragment<MainViewModel>(MainViewModel::class)  {
     }
 
 }
-
-class DzPageAdapter(private val tabPageDataList: List<Int>): PagerAdapter() {
-
-    private val testDataList = listOf(R.string.sport, R.string.cp, R.string.live, R.string.qp, R.string.dz, R.string.by)
-    private val dzSubRvAdapter = DzSubRvAdapter()
-
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val rootView = LayoutInflater.from(container.context).inflate(R.layout.content_dz_adapter, container, false)
-        rootView.apply {
-            try {
-                rv_dz_sub.adapter = dzSubRvAdapter
-                dzSubRvAdapter.dataList = testDataList
-                container.addView(rootView)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        return rootView
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as View)
-    }
-
-    override fun getItemPosition(`object`: Any): Int {
-        //強迫調用 notifyDataSetChanged() 後，每次都重新加載 data //會比較耗資源
-        return POSITION_NONE
-    }
-
-    override fun getCount(): Int {
-        return tabPageDataList.size
-    }
-
-    override fun isViewFromObject(view: View, obj: Any): Boolean {
-        return obj == view
-    }
-
-}
-
 
 class DzVpAdapter(private val tabPageDataList: List<Int>): RecyclerView.Adapter<DzVpAdapter.ItemViewHolder>() {
 
--- app/src/main/java/org/cxct/sportlottery/ui/main/next/MainCategoryFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/main/next/MainCategoryFragment.kt
@@ -1,25 +1,13 @@
 package org.cxct.sportlottery.ui.main.next
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_main_category.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseFragment
@@ -39,54 +27,12 @@ class MainCategoryFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        initObserver()
-//        view_pager.adapter = VPFragmentAdapter(tabTitleList, this)
         view_pager.adapter = VpFragmentPagerAdapter(tabTitleList, parentFragmentManager)
 
         tab_layout.clearOnTabSelectedListeners()
         view_pager.clearOnPageChangeListeners()
         tab_layout.addOnTabSelectedListener(object : TabLayout.ViewPagerOnTabSelectedListener(view_pager) {})
         view_pager.addOnPageChangeListener(object : TabLayout.TabLayoutOnPageChangeListener(tab_layout) {})
-/*
-        tabTitleList.forEachIndexed { index, data ->
-            tab_layout.addTab(createGameTab(data, tabIconList[index]))
-        }
-        */
-/*
-        // attaching tab mediator
-        TabLayoutMediator(tab_layout, view_pager) { tab, position ->
-            tab.setCustomView(R.layout.tab_item_game)
-            tab.customView?.apply {
-                findViewById<TextView>(R.id.tv_title)?.text = getString(tabTitleList[position])
-                findViewById<ImageView>(R.id.iv_icon)?.setImageResource(tabIconList[position])
-            }
-        }.attach()
-        */
-    }
-
-    private fun initObserver() {
-        viewModel.isMainCateVpSwipeable.observe(viewLifecycleOwner) {
-//            view_pager.isUserInputEnabled = it
-//            view_pager.requestDisallowInterceptTouchEvent(true)
-
-        }
-    }
-
-    private fun createGameTab(title: Int, img: Int): TabLayout.Tab {
-        val customTab = tab_layout.newTab().setCustomView(R.layout.tab_item_game)
-        customTab.tag = title
-        try {
-            customTab.customView?.apply {
-                findViewById<TextView>(R.id.tv_title).text = getString(title)
-                findViewById<ImageView>(R.id.iv_icon).setImageResource(img)
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return customTab
     }
 }
 
