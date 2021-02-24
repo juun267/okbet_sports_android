efa91765cefa5e63fd6234158030831f40b63e8e
Bill <h853047455@gmail.com>
2021-02-24 17:11:00 +0800

    [Refactor] Tab width [Fix] move function of getMsgDialog from MainActivity to MainFragment

--- app/src/main/java/org/cxct/sportlottery/ui/home/news/NewsDiaolog.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/home/news/NewsDiaolog.kt
@@ -1,10 +1,11 @@
 package org.cxct.sportlottery.ui.home.news
 
-import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import kotlinx.android.synthetic.main.dialog_event_msg.*
@@ -12,8 +13,9 @@ import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.message.Row
 import org.cxct.sportlottery.ui.base.BaseDialog
 import org.cxct.sportlottery.ui.home.MainViewModel
+import timber.log.Timber
 
-class NewsDiaolog(activity: Activity, messageListResult: List<Row>?) :
+class NewsDiaolog(activity: FragmentActivity?, messageListResult: List<Row>?) :
     BaseDialog<MainViewModel>(MainViewModel::class) {
 
     init {
@@ -32,8 +34,7 @@ class NewsDiaolog(activity: Activity, messageListResult: List<Row>?) :
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.dialog_event_msg, container, false).apply {
-            var a = this.width
-            var b = a
+
         }
     }
 
@@ -42,12 +43,13 @@ class NewsDiaolog(activity: Activity, messageListResult: List<Row>?) :
         initTabRecyclerView()
         initViewPager()
         initButton()
-
     }
 
     private fun initButton() {
-        img_close.setOnClickListener {
-            dismiss()
+        img_close.let {
+            it.setOnClickListener {
+                dismiss()
+            }
         }
     }
 
@@ -57,15 +59,18 @@ class NewsDiaolog(activity: Activity, messageListResult: List<Row>?) :
     }
 
     private fun initTabRecyclerView() {
-        rv_tab.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)//LinearLayout
-        rv_tab.adapter = NewsAdapter(context, NewsAdapter.ItemClickListener {
-            mNewsViewPagerAdapter = NewsViewPagerAdapter(mActivity)
-            mNewsViewPagerAdapter.data = getTypeMsg(it) as MutableList<Row>
-            vp_msg.adapter = mNewsViewPagerAdapter
+        view?.post( Runnable() {
+            rv_tab.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)//LinearLayout
+            rv_tab.adapter = NewsAdapter(img_top.width /3 ,NewsAdapter.ItemClickListener {
+                mNewsViewPagerAdapter = NewsViewPagerAdapter(mActivity)
+                mNewsViewPagerAdapter.data = getTypeMsg(it) as MutableList<Row>
+                vp_msg.adapter = mNewsViewPagerAdapter
+            })
+            var mPagerSnapHelper = PagerSnapHelper()
+            mPagerSnapHelper.attachToRecyclerView(rv_tab)
         })
-        var mPagerSnapHelper = PagerSnapHelper()
-        mPagerSnapHelper.attachToRecyclerView(rv_tab)
+
     }
 
     private fun getTypeMsg(index: Int): List<Row>? {
