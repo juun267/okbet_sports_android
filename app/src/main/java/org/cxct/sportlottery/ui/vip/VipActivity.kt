1591bdbe07a85fc6ac29246d61413b9cd9a85589
Dean <b062020315@gmail.com>
2021-02-25 13:53:38 +0800

    [新增] 會員層級 UI

--- app/src/main/java/org/cxct/sportlottery/ui/vip/VipActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/vip/VipActivity.kt
@@ -2,11 +2,18 @@ package org.cxct.sportlottery.ui.vip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_vip.*
 import org.cxct.sportlottery.R
 
 class VipActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vip)
+
+        initView()
+    }
+
+    private fun initView() {
+        bubble_level_one.isSelected = true
     }
 }
