3c9327c580384c31f6b78ad2cf424b26c860c3e3
Dean <b062020315@gmail.com>
2021-03-03 16:39:33 +0800

    [Add] start symbol at necessary input field

--- app/src/main/java/org/cxct/sportlottery/ui/login/LoginEditText.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/login/LoginEditText.kt
@@ -8,13 +8,13 @@ import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.annotation.IntDef
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.edittext_login.view.*
 import org.cxct.sportlottery.R
@@ -65,6 +65,7 @@ class LoginEditText @JvmOverloads constructor(context: Context, attrs: Attribute
             val inputType = typedArray.getInt(R.styleable.CustomView_cvInputType, 0x00000001)
             view.et_input.inputType = inputType
 
+            view.tv_start.visibility = typedArray.getInt(R.styleable.CustomView_necessarySymbol, 0x00000008) //預設隱藏 需要再打開
             view.btn_withdraw_all.visibility = View.GONE //預設關閉 需要再打開
             view.btn_clear.visibility = if (inputType == 0x00000081) View.GONE else View.VISIBLE
             view.btn_eye.visibility = if (inputType == 0x00000081) View.VISIBLE else View.GONE
@@ -172,6 +173,12 @@ class LoginEditText @JvmOverloads constructor(context: Context, attrs: Attribute
             mOnFocusChangeListener.onFocusChange(v, hasFocus)
             listener.invoke(v, hasFocus)
         }
+    }
+
+    @IntDef(View.VISIBLE, View.INVISIBLE, View.GONE)
+    annotation class Visibility
+    fun setNecessarySymbolVisibility(@Visibility visibility: Int) {
+        tv_start.visibility = visibility
     }
 }
 
