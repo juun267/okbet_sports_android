7eff54396bb51724d9c7d3e312d59199ab57bcf3
edward_android <koyo98765@gmail.com>
2021-03-11 11:30:41 +0800

    [Add] play type 1x2 to league odd list

--- app/src/main/java/org/cxct/sportlottery/ui/game/v3/LeagueOddAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/v3/LeagueOddAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.itemview_game_league_odd.view.*
+import kotlinx.android.synthetic.main.itemview_game_league_odd_1x2.view.*
+import kotlinx.android.synthetic.main.itemview_game_league_odd_hdp_ou.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.network.common.PlayType
 import org.cxct.sportlottery.network.odds.list.MatchOdd
 
-class LeagueOddAdapter : RecyclerView.Adapter<LeagueOddAdapter.ViewHolder>() {
+class LeagueOddAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var data = listOf<MatchOdd>()
         set(value) {
@@ -16,21 +18,35 @@ class LeagueOddAdapter : RecyclerView.Adapter<LeagueOddAdapter.ViewHolder>() {
             notifyDataSetChanged()
         }
 
+    var playType: PlayType = PlayType.OU_HDP
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
     var leagueOddListener: LeagueOddListener? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (playType) {
+            PlayType.OU_HDP -> ViewHolderHdpOu.from(parent)
+            else -> ViewHolder1x2.from(parent)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = data[position]
 
-        holder.bind(item, leagueOddListener)
+        when (holder) {
+            is ViewHolderHdpOu -> holder.bind(item, leagueOddListener)
+            is ViewHolder1x2 -> holder.bind(item, leagueOddListener)
+        }
     }
 
     override fun getItemCount(): Int = data.size
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    class ViewHolderHdpOu private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: MatchOdd, leagueOddListener: LeagueOddListener?) {
             itemView.game_name_home.text = item.matchInfo?.homeName
             itemView.game_name_away.text = item.matchInfo?.awayName
@@ -41,12 +57,33 @@ class LeagueOddAdapter : RecyclerView.Adapter<LeagueOddAdapter.ViewHolder>() {
         }
 
         companion object {
-            fun from(parent: ViewGroup): ViewHolder {
+            fun from(parent: ViewGroup): ViewHolderHdpOu {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val view = layoutInflater
-                    .inflate(R.layout.itemview_game_league_odd, parent, false)
+                    .inflate(R.layout.itemview_game_league_odd_hdp_ou, parent, false)
 
-                return ViewHolder(view)
+                return ViewHolderHdpOu(view)
+            }
+        }
+    }
+
+    class ViewHolder1x2 private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MatchOdd, leagueOddListener: LeagueOddListener?) {
+            itemView.game_name_home_1x2.text = item.matchInfo?.homeName
+            itemView.game_name_away_1x2.text = item.matchInfo?.awayName
+
+            itemView.match_live_1x2.setOnClickListener {
+                leagueOddListener?.onClickLive(item)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder1x2 {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.itemview_game_league_odd_1x2, parent, false)
+
+                return ViewHolder1x2(view)
             }
         }
     }
