0def8870b383e8ae2b65a713126eb7844c51f540
Sky <simon@cxteam.io>
2021-03-23 16:56:15 +0800

    [Refactor] 移除冗余代碼、整理 code

--- app/src/main/java/org/cxct/sportlottery/service/ServiceMessage.kt
+++ app/src/main/java/org/cxct/sportlottery/service/ServiceMessage.kt
@@ -1,12 +1,10 @@
-package org.cxct.sportlottery.ui.home.broadcast
+package org.cxct.sportlottery.service
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.cxct.sportlottery.network.service.global_stop.GlobalStopEvent
 import org.cxct.sportlottery.network.service.match_clock.MatchClockEvent
 import org.cxct.sportlottery.network.service.match_odds_change.MatchOddsChangeEvent
-import org.cxct.sportlottery.network.service.match_odds_change.Odd
 import org.cxct.sportlottery.network.service.match_status_change.MatchStatusChangeEvent
 import org.cxct.sportlottery.network.service.notice.NoticeEvent
 import org.cxct.sportlottery.network.service.odds_change.OddsChangeEvent
@@ -16,12 +14,11 @@ import org.cxct.sportlottery.network.service.producer_up.ProducerUpEvent
 import org.cxct.sportlottery.network.service.sys_maintenance.SysMaintenanceEvent
 import org.cxct.sportlottery.network.service.user_money.UserMoneyEvent
 import org.cxct.sportlottery.network.service.user_notice.UserNoticeEvent
-import org.cxct.sportlottery.util.MoshiUtil
 
 
 object ServiceMessage {
 
-    private val moshi: Moshi by lazy { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() } //.add(KotlinJsonAdapterFactory())
+    private val moshi: Moshi by lazy { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
 
     fun getGlobalStop(messageStr: String): GlobalStopEvent? {
         val adapter = moshi.adapter(GlobalStopEvent::class.java)
@@ -55,11 +52,6 @@ object ServiceMessage {
 
     fun getMatchOddsChange(messageStr: String): MatchOddsChangeEvent? {
         val adapter = moshi.adapter(MatchOddsChangeEvent::class.java)
-//        val data = adapter.fromJson(messageStr)
-//        val jsonStructure = adapter.toJsonValue(obj)
-//        val type = Types.newParameterizedType(MutableMap::class.java, String::class.java, Odd::class.java)
-//        val adapter2 = moshi.adapter<Map<String, Odd>>(type)
-//        MoshiUtil.fromJson<List<Odd>>(getRechargeConfig(mContext), Types.newParameterizedType(MutableList::class.java, MoneyPayWayData::class.java))
         return adapter.fromJson(messageStr)
     }
 
