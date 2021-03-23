298bfd1de84874ecfd698db4b5fc81199393dbf0
edward_android <koyo98765@gmail.com>
2021-03-23 14:02:29 +0800

    [Add] score data to odd list

--- app/src/main/java/org/cxct/sportlottery/ui/game/v3/LeagueOddAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/v3/LeagueOddAdapter.kt
@@ -67,6 +67,9 @@ class LeagueOddAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
             itemView.game_name_home.text = item.matchInfo?.homeName
             itemView.game_name_away.text = item.matchInfo?.awayName
+
+            itemView.game_score_home.text = (item.matchInfo?.homeScore ?: 0).toString()
+            itemView.game_score_away.text = (item.matchInfo?.awayScore ?: 0).toString()
         }
 
         private fun setupOddButton(item: MatchOdd, leagueOddListener: LeagueOddListener?) {
