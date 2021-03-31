56e7de01f080f2906cfd3d7d59455afdfb168e69
Edward <edward@EdwarddeiMac.local>
2021-03-31 10:29:35 +0800

    [Add] global stop event to outright bet list

--- app/src/main/java/org/cxct/sportlottery/ui/game/v3/GameOutrightFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/v3/GameOutrightFragment.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_game_outright.*
 import kotlinx.android.synthetic.main.fragment_game_outright.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.common.CateMenuCode
+import org.cxct.sportlottery.network.odds.list.BetStatus
 import org.cxct.sportlottery.network.odds.list.Odd
 import org.cxct.sportlottery.network.odds.list.OddState
 import org.cxct.sportlottery.ui.base.BaseSocketFragment
@@ -177,6 +178,30 @@ class GameOutrightFragment : BaseSocketFragment<GameViewModel>(GameViewModel::cl
                             }
                         }
                     }
+                }
+            }
+        })
+
+        receiver.globalStop.observe(this.viewLifecycleOwner, Observer {
+            it?.let { globalStopEvent ->
+
+                val odds = outrightOddAdapter.data.filterIsInstance<Odd>()
+
+                odds.forEach { odd ->
+                    when (globalStopEvent.producerId) {
+                        null -> {
+                            odd.status = BetStatus.DEACTIVATED.code
+                        }
+                        else -> {
+                            odd.producerId?.let { producerId ->
+                                if (producerId == globalStopEvent.producerId) {
+                                    odd.status = BetStatus.DEACTIVATED.code
+                                }
+                            }
+                        }
+                    }
+
+                    outrightOddAdapter.notifyItemChanged(odds.indexOf(odd))
                 }
             }
         })
