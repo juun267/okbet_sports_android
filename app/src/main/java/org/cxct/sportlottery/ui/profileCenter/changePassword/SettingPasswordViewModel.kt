ccb8ad0e95367a2827a17f69c37b7c408ecd2574
Dean <b062020315@gmail.com>
2021-04-08 17:47:21 +0800

    [Fix] 8796 [Android][个人中心]初次设置提现密码时，点选新密码栏位，不应立即判断确认密码栏位未填写

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/changePassword/SettingPasswordViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/changePassword/SettingPasswordViewModel.kt
@@ -18,6 +18,7 @@ import org.cxct.sportlottery.repository.InfoCenterRepository
 import org.cxct.sportlottery.repository.LoginRepository
 import org.cxct.sportlottery.repository.UserInfoRepository
 import org.cxct.sportlottery.ui.base.BaseOddButtonViewModel
+import org.cxct.sportlottery.util.MD5Util
 import org.cxct.sportlottery.util.VerifyConstUtil
 
 class SettingPasswordViewModel(
@@ -31,6 +32,9 @@ class SettingPasswordViewModel(
     private val _passwordFormState = MutableLiveData<PasswordFormState>()
     private val _updatePwdResult = MutableLiveData<UpdatePwdResult?>()
     private val _updateFundPwdResult = MutableLiveData<UpdateFundPwdResult?>()
+    private val _currentPwdError = MutableLiveData<String>()
+    private val _newPwdError = MutableLiveData<String>()
+    private val _confirmPwdError = MutableLiveData<String>()
 
     val passwordFormState: LiveData<PasswordFormState>
         get() = _passwordFormState
@@ -38,23 +42,47 @@ class SettingPasswordViewModel(
         get() = _updatePwdResult
     val updateFundPwdResult: LiveData<UpdateFundPwdResult?>
         get() = _updateFundPwdResult
+    val currentPwdError: LiveData<String>
+        get() = _currentPwdError
+    val newPwdError: LiveData<String>
+        get() = _newPwdError
+    val confirmPwdError: LiveData<String>
+        get() = _confirmPwdError
 
     val userInfo: LiveData<UserInfo?> = userInfoRepository.userInfo.asLiveData()
 
-    fun checkInputField(pwdPage: SettingPasswordActivity.PwdPage, context: Context, currentPwd: String, newPwd: String, confirmPwd: String): Boolean {
-        val currentPwdError = checkCurrentPwd(context, currentPwd)
-        val newPwdError = checkNewPwd(pwdPage, context, currentPwd, newPwd)
-        val confirmPwdError = checkConfirmPwd(context, newPwd, confirmPwd)
-        val isDataValid = currentPwdError == null && newPwdError == null && confirmPwdError == null
-        _passwordFormState.value = PasswordFormState(
-            currentPwdError = currentPwdError,
-            newPwdError = newPwdError,
-            confirmPwdError = confirmPwdError
-        )
-        return isDataValid
+    fun checkInputField(pwdPage: SettingPasswordActivity.PwdPage, currentPwd: String, newPwd: String, confirmPwd: String) {
+        checkCurrentPwd(currentPwd)
+        checkNewPwd(pwdPage, currentPwd, newPwd)
+        checkConfirmPwd(newPwd, confirmPwd)
+
+        if (checkInputFieldVerify()) {
+            when (pwdPage) {
+                SettingPasswordActivity.PwdPage.LOGIN_PWD -> createUpdatePwdRequest(currentPwd, newPwd)?.let { request -> updatePwd(request) }
+                SettingPasswordActivity.PwdPage.BANK_PWD -> createUpdateFunPwd(currentPwd, newPwd)?.let { updateFundPwd(it) }
+            }
+        }
     }
 
-    fun updatePwd(updatePwdRequest: UpdatePwdRequest) {
+    private fun createUpdatePwdRequest(currentPwd: String, newPwd: String): UpdatePwdRequest? {
+        return UpdatePwdRequest(
+            userId = userInfo.value?.userId ?: return null,
+            platformId = userInfo.value?.platformId ?: return null,
+            oldPassword = MD5Util.MD5Encode(currentPwd),
+            newPassword = MD5Util.MD5Encode(newPwd)
+        )
+    }
+
+    private fun createUpdateFunPwd(currentPwd: String, newPwd: String): UpdateFundPwdRequest? {
+        return UpdateFundPwdRequest(
+            userId = userInfo.value?.userId ?: return null,
+            platformId = userInfo.value?.platformId ?: return null,
+            oldPassword = MD5Util.MD5Encode(currentPwd),
+            newPassword = MD5Util.MD5Encode(newPwd)
+        )
+    }
+
+    private fun updatePwd(updatePwdRequest: UpdatePwdRequest) {
         viewModelScope.launch {
             val result = doNetwork(androidContext) {
                 OneBoSportApi.userService.updatePwd(updatePwdRequest)
@@ -64,7 +92,7 @@ class SettingPasswordViewModel(
         }
     }
 
-    fun updateFundPwd(updateFundPwdRequest: UpdateFundPwdRequest) {
+    private fun updateFundPwd(updateFundPwdRequest: UpdateFundPwdRequest) {
         viewModelScope.launch {
             val result = doNetwork(androidContext) {
                 OneBoSportApi.userService.updateFundPwd(updateFundPwdRequest)
@@ -77,33 +105,43 @@ class SettingPasswordViewModel(
         }
     }
 
-    private fun checkCurrentPwd(context: Context, currentPwd: String): String? {
-        return when {
-            currentPwd.isBlank() -> context.getString(R.string.error_input_empty)
-            else -> null
+    private fun checkInputFieldVerify(): Boolean {
+        if (currentPwdError.value != "")
+            return false
+        if (newPwdError.value != "")
+            return false
+        if (confirmPwdError.value != "")
+            return false
+        return true
+    }
+
+    fun checkCurrentPwd(currentPwd: String) {
+        _currentPwdError.value = when {
+            currentPwd.isBlank() -> androidContext.getString(R.string.error_input_empty)
+            else -> ""
         }
     }
 
-    private fun checkNewPwd(pwdPage: SettingPasswordActivity.PwdPage, context: Context, currentPwd: String, newPwd: String): String? {
-        return when {
-            newPwd.isBlank() -> context.getString(R.string.error_input_empty)
+    fun checkNewPwd(pwdPage: SettingPasswordActivity.PwdPage, currentPwd: String, newPwd: String) {
+        _newPwdError.value = when {
+            newPwd.isBlank() -> androidContext.getString(R.string.error_input_empty)
             pwdPage == SettingPasswordActivity.PwdPage.LOGIN_PWD -> when {
-                !VerifyConstUtil.verifyPwdFormat(newPwd) -> context.getString(R.string.error_password_format)
-                newPwd.length !in 6..20 -> context.getString(R.string.error_register_password)
-                !VerifyConstUtil.verifyPwd(newPwd) -> context.getString(R.string.error_incompatible_format)
-                else -> null
+                !VerifyConstUtil.verifyPwdFormat(newPwd) -> androidContext.getString(R.string.error_password_format)
+                newPwd.length !in 6..20 -> androidContext.getString(R.string.error_register_password)
+                !VerifyConstUtil.verifyPwd(newPwd) -> androidContext.getString(R.string.error_incompatible_format)
+                else -> ""
             }
-            pwdPage == SettingPasswordActivity.PwdPage.BANK_PWD && !VerifyConstUtil.verifyPayPwd(newPwd) -> context.getString(R.string.error_withdrawal_pwd)
-            currentPwd == newPwd -> context.getString(R.string.error_password_cannot_be_same)
-            else -> null
+            pwdPage == SettingPasswordActivity.PwdPage.BANK_PWD && !VerifyConstUtil.verifyPayPwd(newPwd) -> androidContext.getString(R.string.error_withdrawal_pwd)
+            currentPwd == newPwd -> androidContext.getString(R.string.error_password_cannot_be_same)
+            else -> ""
         }
     }
 
-    private fun checkConfirmPwd(context: Context, newPwd: String, confirmPwd: String): String? {
-        return when {
-            confirmPwd.isBlank() -> context.getString(R.string.error_input_empty)
-            newPwd != confirmPwd -> context.getString(R.string.error_confirm_password)
-            else -> null
+    fun checkConfirmPwd(newPwd: String, confirmPwd: String) {
+        _confirmPwdError.value = when {
+            confirmPwd.isBlank() -> androidContext.getString(R.string.error_input_empty)
+            newPwd != confirmPwd -> androidContext.getString(R.string.error_confirm_password)
+            else -> ""
         }
     }
 
