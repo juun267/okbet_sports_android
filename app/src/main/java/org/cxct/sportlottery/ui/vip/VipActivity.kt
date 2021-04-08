a45e4f2938eb8ca88fe2a7eb3351fdf05c7d202b
Dean <b062020315@gmail.com>
2021-04-08 11:39:58 +0800

    [Fix] 8746 【Android】[遊客] 開啟「會員層級」頁面，頁面顯示不正確

--- app/src/main/java/org/cxct/sportlottery/ui/vip/VipActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/vip/VipActivity.kt
@@ -18,6 +18,8 @@ import org.cxct.sportlottery.network.user.info.UserInfoData
 import org.cxct.sportlottery.network.vip.growth.GROWTH_CONFIG_BET_ID
 import org.cxct.sportlottery.network.vip.growth.GROWTH_CONFIG_RECHARGE_ID
 import org.cxct.sportlottery.network.vip.growth.GrowthConfig
+import org.cxct.sportlottery.repository.StaticData
+import org.cxct.sportlottery.repository.TestFlag
 import org.cxct.sportlottery.ui.base.BaseOddButtonActivity
 
 class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
@@ -34,6 +36,8 @@ class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
             }
         })
     }
+
+    private var userVipLevel: Level? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -115,24 +119,25 @@ class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
     }
 
     private fun userInfoUpdateView(userInfo: UserInfoData) {
+        updateUserVipLevel(userInfo.testFlag, userInfo.userLevelId)
         setupViewByUserInfo(userInfo)
-        updateUserLevel(userInfo.userLevelId)
+        updateUserLevelBlock()
         updateUserGrowthBar(userInfo)
-        updateNextLevelTips(userInfo)
+        updateNextLevelTips()
     }
 
-    private fun updateNextLevelTips(userInfo: UserInfoData) {
-        if (verifyMaxLevel(userInfo.userLevelId)) {
+    private fun updateNextLevelTips() {
+        if (verifyMaxLevel()) {
             tv_next_level_tips.text = getString(R.string.level_max)
             return
         }
-        getNextLevel(userInfo.userLevelId)?.let { nextLevel ->
+        getNextLevel()?.let { nextLevel ->
             tv_next_level_tips.text = "${getString(nextLevel.levelRequirement.level)}  ${nextLevel.levelRequirement.levelName}"
         }
     }
 
     private fun updateUserGrowthBar(userInfo: UserInfoData) {
-        if (verifyMaxLevel(userInfo.userLevelId)) {
+        if (verifyMaxLevel()) {
             pb_user_growth.apply {
                 max = 1
                 progress = 1
@@ -140,7 +145,7 @@ class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
             tv_requirement_amount.text = getString(R.string.level_max)
             return
         }
-        val growthRequirement = getUpgradeGrowthRequirement(userInfo.userLevelId)
+        val growthRequirement = getUpgradeGrowthRequirement()
         val userGrowth = userInfo.growth?.toInt() ?: 0
         val nextLevelRequirement = (growthRequirement - userGrowth).let { if (it < 0) 0 else it }.toLong()
         pb_user_growth.apply {
@@ -150,26 +155,38 @@ class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
         tv_requirement_amount.text = String.format(getString(R.string.next_level_tips), nextLevelRequirement)
     }
 
-    private fun verifyMaxLevel(levelId: Int): Boolean {
-        return Level.values().last() === Level.values().find { level -> level.levelRequirement.levelId == levelId }
+    private fun verifyMaxLevel(): Boolean {
+        return Level.values().last() === userVipLevel
     }
 
-    private fun getUpgradeGrowthRequirement(levelId: Int): Int {
-        return getNextLevel(levelId)?.levelRequirement?.growthRequirement ?: 0
+    private fun getUpgradeGrowthRequirement(): Int {
+        return getNextLevel()?.levelRequirement?.growthRequirement ?: 0
     }
 
-    private fun getNextLevel(levelId: Int): Level? {
-        return Level.values().find { level -> level.levelRequirement.levelId == levelId + 1 }
+    private fun getNextLevel(): Level? {
+        val nextLevelId = userVipLevel?.name?.let { Level.valueOf(it).levelRequirement.levelId?.plus(1) }
+        return Level.values().find { level -> level.levelRequirement.levelId == nextLevelId }
     }
 
     private fun setupViewByUserInfo(userInfo: UserInfoData) {
         userInfo.let { user ->
-            tv_greet.text = user.nickName
+            tv_greet.text = if (user.fullName.isNullOrEmpty()) user.nickName else user.fullName
         }
     }
 
-    private fun updateUserLevel(levelId: Int) {
-        Level.values().find { it.levelRequirement.levelId == levelId }?.apply {
+    private fun updateUserVipLevel(testFlag: Long, levelId: Int) {
+        userVipLevel = when (StaticData.getTestFlag(testFlag)) {
+            TestFlag.GUEST -> {
+                Level.values().first()
+            }
+            else -> {
+                Level.values().find { it.levelRequirement.levelId == levelId }
+            }
+        }
+    }
+
+    private fun updateUserLevelBlock() {
+        userVipLevel?.apply {
             tv_vip_name.text = levelRequirement.levelName
             iv_vip.setImageDrawable(ContextCompat.getDrawable(this@VipActivity, levelRequirement.levelTitleIcon))
             updateLevelBar(ordinal)
@@ -209,17 +226,17 @@ class VipActivity : BaseOddButtonActivity<VipViewModel>(VipViewModel::class) {
                 tvLevel?.text = getString(cardInfo.level)
                 ivLevel?.setImageDrawable(ContextCompat.getDrawable(this@VipActivity, cardInfo.levelIcon))
                 tvLevelName?.text = cardInfo.levelName
-                tvGrowthRequirement?.text = getGrowthRequirementTips(cardInfo.growthRequirement)
+                tvGrowthRequirement?.text = getGrowthRequirementTips(cardInfo.levelId, cardInfo.growthRequirement)
             }
         }
     }
 
-    private fun getGrowthRequirementTips(requirement: Int?): String {
+    private fun getGrowthRequirementTips(level: Int?, requirement: Int?): String {
         return when {
-            requirement == null -> ""
-            (requirement <= 1) -> {
+            level == 2 -> {
                 getString(R.string.no_value)
             }
+            requirement == null -> ""
             else -> "$requirement ${getString(R.string.level_requirement_unit)}"
         }
     }
