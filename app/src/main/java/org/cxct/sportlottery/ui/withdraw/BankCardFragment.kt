1ec11b8b2b9d836816515eb8e3afa975110287f8
Cheryl <a222025115@gmail.com>
2021-04-19 17:53:17 +0800

    clean code.

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardFragment.kt
@@ -555,10 +555,8 @@ class ProtocolAdapter(private val selectedListener: OnSelectProtocol) :
         dataCheckedList.forEachIndexed { index, _ ->
             dataCheckedList[index] = index == 0
         }
-        if (dataList.isNotEmpty()) {
-            dataList[0].let {
-                selectedListener.onSelected(it)
-            }
+        dataList.firstOrNull()?.let {
+            selectedListener.onSelected(it)
         }
         notifyItemChanged(selectedPosition)
         notifyItemChanged(0)
