15ae5a00edc9fe57a733ac6f71432768ebf73625
Edward <edward@EdwarddeiMac.local>
2021-04-22 11:56:58 +0800

    [Fix] odds detail live fragment back with match type

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
@@ -300,12 +300,16 @@ class GameActivity : BaseNoticeActivity<GameViewModel>(GameViewModel::class) {
                 val navOptions = NavOptions.Builder().setLaunchSingleTop(true).build()
                 mNavController.navigate(action, navOptions)
             }
-            R.id.gameLeagueFragment, R.id.gameOutrightFragment, R.id.oddsDetailLiveFragment -> {
+            R.id.gameLeagueFragment, R.id.gameOutrightFragment -> {
                 mNavController.popBackStack(R.id.gameV3Fragment, false)
             }
             R.id.oddsDetailFragment -> {
                 val action =
                     OddsDetailFragmentDirections.actionOddsDetailFragmentToGameV3Fragment(matchType)
+                mNavController.navigate(action)
+            }
+            R.id.oddsDetailLiveFragment -> {
+                val action =  OddsDetailLiveFragmentDirections.actionOddsDetailLiveFragmentToGameV3Fragment(matchType)
                 mNavController.navigate(action)
             }
         }
