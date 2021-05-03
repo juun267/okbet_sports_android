dc6b4ddaec704838d7fcd2085d2e389384948fdb
Dean <b062020315@gmail.com>
2021-05-03 15:32:56 +0800

    [Fix] 10580 [v.3]【Android】[提款] 虛擬幣，協議/幣種欄位，應顯示為「選擇地址」

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawFragment.kt
@@ -81,7 +81,7 @@ class WithdrawFragment : BaseSocketFragment<WithdrawViewModel>(WithdrawViewModel
                 et_withdrawal_amount.setTitle(getString(R.string.withdraw_amount))
             }
             TransferType.CRYPTO -> {
-                tv_channel_select.text = getString(R.string.currency_protocol)
+                tv_channel_select.text = getString(R.string.select_crypto_address)
                 et_withdrawal_amount.setTitle(getString(R.string.withdraw_number))
             }
         }
