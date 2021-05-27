b011bc85f79906559e539542d4e44c962f1978e7
liweihao <koyo98765@gmail.com>
2021-05-27 18:10:29 +0800

    [Refactor] remove play category package warning

--- app/src/main/java/org/cxct/sportlottery/network/playcate/PlayCateListService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/playcate/PlayCateListService.kt
@@ -3,8 +3,6 @@ package org.cxct.sportlottery.network.playcate
 import org.cxct.sportlottery.network.Constants
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface PlayCateListService {
