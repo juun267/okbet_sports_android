cd0fb53b48510f5975796cbde231794d84b2a914
liweihao <koyo98765@gmail.com>
2021-06-09 11:38:15 +0800

    [Refactor] change pass oddsDetailFragment nav argument to match info array

--- app/src/main/java/org/cxct/sportlottery/network/odds/list/MatchOdd.kt
+++ app/src/main/java/org/cxct/sportlottery/network/odds/list/MatchOdd.kt
@@ -1,27 +1,23 @@
 package org.cxct.sportlottery.network.odds.list
 
 
-import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.android.parcel.Parcelize
-import kotlinx.android.parcel.RawValue
 import org.cxct.sportlottery.network.common.PlayType
 import org.cxct.sportlottery.network.odds.MatchInfo
 import org.cxct.sportlottery.network.service.match_status_change.MatchStatus
 
-@Parcelize
 @JsonClass(generateAdapter = true)
 data class MatchOdd(
     @Json(name = "matchInfo")
-    val matchInfo: @RawValue MatchInfo? = null,
+    val matchInfo: MatchInfo? = null,
     @Json(name = "odds")
-    var odds: @RawValue MutableMap<String, MutableList<Odd?>> = mutableMapOf(
+    var odds: MutableMap<String, MutableList<Odd?>> = mutableMapOf(
         PlayType.HDP.code to mutableListOf(),
         PlayType.OU.code to mutableListOf(),
         PlayType.X12.code to mutableListOf()
     )
-) : Parcelable {
+) {
     var isExpand = false
     var leagueTime: Int? = null
     var matchStatusList: List<MatchStatus> = listOf()
