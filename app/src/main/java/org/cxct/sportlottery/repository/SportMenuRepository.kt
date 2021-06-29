cd02c467199d679ff9ebeaf1bd3fb3418ae6e2fe
Bill <h853047455@gmail.com>
2021-06-29 16:13:18 +0800

    [Add]  新增我的最愛API

--- app/src/main/java/org/cxct/sportlottery/repository/SportMenuRepository.kt
+++ app/src/main/java/org/cxct/sportlottery/repository/SportMenuRepository.kt
@@ -1,8 +1,8 @@
 package org.cxct.sportlottery.repository
 
 import org.cxct.sportlottery.network.OneBoSportApi
-import org.cxct.sportlottery.network.sport.SportMenuRequest
-import org.cxct.sportlottery.network.sport.SportMenuResult
+import org.cxct.sportlottery.network.common.BaseResult
+import org.cxct.sportlottery.network.sport.*
 import retrofit2.Response
 
 class SportMenuRepository {
@@ -14,4 +14,17 @@ class SportMenuRepository {
             )
         )
     }
+
+    suspend fun getMyFavorite(): Response<SportMenuFavoriteResult> {
+        return OneBoSportApi.sportService.getMyFavorite()
+    }
+
+    suspend fun getMyFavoriteMatch(myFavoriteMatchRequest: MyFavoriteMatchRequest): Response<MyFavoriteMatchResult> {
+        return OneBoSportApi.sportService.getMyFavoriteMatch(myFavoriteMatchRequest)
+    }
+
+    suspend fun saveMyFavorite(saveMyFavoriteRequest: SaveMyFavoriteRequest): Response<BaseResult> {
+        return OneBoSportApi.sportService.saveMyFavorite(saveMyFavoriteRequest)
+    }
+
 }
