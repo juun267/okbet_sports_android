66eabfcfe974f556ac3747383eaa295055117658
liweihao <koyo98765@gmail.com>
2021-07-12 17:05:20 +0800

    [Add] Main market play type and string res

--- app/src/main/java/org/cxct/sportlottery/network/common/PlayType.kt
+++ app/src/main/java/org/cxct/sportlottery/network/common/PlayType.kt
@@ -9,5 +9,16 @@ enum class PlayType(val code: String) {
     OUTRIGHT("OUTRIGHT"),
     HDP_INCL_OT("HDP-INCL-OT"),
     OU_INCL_OT("O/U-INCL-OT"),
-    X12_INCL_OT("1X2-INCL-OT")
+    X12_INCL_OT("1X2-INCL-OT"),
+    HDP_SEG1("HDP-SEG1"),
+    X12_SEG1("1X2-SEG1"),
+    TG_OU_H_INCL_OT("TG&O/U-H-INCL-OT"),
+    TG_OU_C_INCL_OT("TG&O/U-C-INCL-OT"),
+    HDP_1ST("HDP-1ST"),
+    OU_1ST("O/U-1ST"),
+    TG_OU_H_1ST("TG&O/U-H-1ST"),
+    TG_OU_C_1ST("TG&O/U-C-1ST"),
+    X12_1ST("1X2-1ST"),
+    BTS("BTS"),
+    OE("O/E")
 }
