7a6e058128b5247756eb85abcc9d2291c7e5e41a
liweihao <koyo98765@gmail.com>
2021-07-13 14:21:50 +0800

    [Add] Data play cate list for sport query api

---
+++ app/src/main/java/org/cxct/sportlottery/network/sport/query/PlayCate.kt
@@ -0,0 +1,11 @@
+package org.cxct.sportlottery.network.sport.query
+
+
+import com.squareup.moshi.Json
+
+data class PlayCate(
+    @Json(name = "code")
+    val code: String?,
+    @Json(name = "name")
+    val name: String?
+)
