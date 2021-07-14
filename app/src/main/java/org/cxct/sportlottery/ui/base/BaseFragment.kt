33ba3cc80c506abda906783dc4404a1a96ca8d78
liweihao <koyo98765@gmail.com>
2021-07-14 14:19:41 +0800

    [Add] Base bottom sheet dialog to base fragment

--- app/src/main/java/org/cxct/sportlottery/ui/base/BaseFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/base/BaseFragment.kt
@@ -1,16 +1,31 @@
 package org.cxct.sportlottery.ui.base
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.dialog_bottom_sheet_custom.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.common.StatusSheetAdapter
+import org.cxct.sportlottery.ui.common.StatusSheetData
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import kotlin.reflect.KClass
 
+
+@SuppressLint("InflateParams")
 open class BaseFragment<T : BaseViewModel>(clazz: KClass<T>) : Fragment() {
 
     val viewModel: T by sharedViewModel(clazz = clazz)
+
+    private val bottomSheetView: View by lazy {
+        LayoutInflater.from(context).inflate(R.layout.dialog_bottom_sheet_custom, null)
+    }
+    private val bottomSheet: BottomSheetDialog by lazy { BottomSheetDialog(requireContext()) }
+
 
     /*弹出加载界面*/
     open fun loading() {
@@ -71,6 +86,36 @@ open class BaseFragment<T : BaseViewModel>(clazz: KClass<T>) : Fragment() {
                 (activity as BaseActivity<*>).showErrorPromptDialog(title, message, positiveClickListener)
             }
         }
+    }
+
+    fun showBottomSheetDialog(
+        title: String?,
+        dataList: List<StatusSheetData>,
+        defaultData: StatusSheetData,
+        itemClickListener: StatusSheetAdapter.ItemCheckedListener,
+        isShowSelectAll: Boolean = false,
+    ) {
+        bottomSheetView.apply {
+            sheet_tv_title.text = title
+
+            checkbox_select_all.visibility = if (isShowSelectAll) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+
+            sheet_rv_more.adapter =
+                StatusSheetAdapter(itemClickListener).apply {
+                    this.dataList = dataList
+                    this.defaultCheckedCode = defaultData.code
+                }
+
+            sheet_tv_close.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+        }
+        bottomSheet.setContentView(bottomSheetView)
+        bottomSheet.show()
     }
 
     override fun onDestroy() {
