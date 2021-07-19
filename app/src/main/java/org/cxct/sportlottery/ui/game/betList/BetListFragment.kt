f58198342ae2ad6e80495496c61b0d513c69312c
Dean <b062020315@gmail.com>
2021-07-19 16:12:59 +0800

    clean code

--- app/src/main/java/org/cxct/sportlottery/ui/game/betList/BetListFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/betList/BetListFragment.kt
@@ -17,22 +17,14 @@ import org.cxct.sportlottery.ui.game.GameViewModel
 import org.cxct.sportlottery.util.TextUtil
 import timber.log.Timber
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [BetListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class BetListFragment : BaseSocketFragment<GameViewModel>(GameViewModel::class) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private lateinit var binding: FragmentBetListBinding
+
     private val betListDiffAdapter by lazy { BetListDiffAdapter() }
 
     private val deleteAllLayoutAnimationListener by lazy {
@@ -52,10 +44,6 @@ class BetListFragment : BaseSocketFragment<GameViewModel>(GameViewModel::class) 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -121,7 +109,6 @@ class BetListFragment : BaseSocketFragment<GameViewModel>(GameViewModel::class) 
 
         viewModel.betInfoRepository.betInfoList.observe(this.viewLifecycleOwner, {
             it.getContentIfNotHandled()?.let { list ->
-                Timber.e("Dean, bet list = $list")
                 betListDiffAdapter.submitList(list)
             }
         })
@@ -143,18 +130,9 @@ class BetListFragment : BaseSocketFragment<GameViewModel>(GameViewModel::class) 
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment BetListFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance() =
-            BetListFragment().apply {
-                /*arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }*/
-            }
+        fun newInstance() = BetListFragment()
     }
 }
