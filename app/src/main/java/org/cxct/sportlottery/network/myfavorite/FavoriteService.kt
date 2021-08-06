8c82610251486d89985e8541a27069118a2320cb
Edward <edward@EdwarddeiMac.local>
2021-08-06 14:15:12 +0800

    [Refactor] Move Favorite Service to dependent package

--- app/src/main/java/org/cxct/sportlottery/network/myfavorite/FavoriteService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/myfavorite/FavoriteService.kt
@@ -1,6 +1,7 @@
-package org.cxct.sportlottery.network.sport
+package org.cxct.sportlottery.network.myfavorite
 
 import org.cxct.sportlottery.network.Constants
+import org.cxct.sportlottery.network.sport.*
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
