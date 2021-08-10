05ea8cdd305aa7e8a66e3a930ddf3bbfee607758
Cheryl <a222025115@gmail.com>
2021-08-10 11:04:06 +0800

    fix finished-button.

--- app/src/main/java/org/cxct/sportlottery/ui/bet/list/receipt/BetInfoCarReceiptDialog.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/bet/list/receipt/BetInfoCarReceiptDialog.kt
@@ -1,17 +1,14 @@
 package org.cxct.sportlottery.ui.bet.list.receipt
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import kotlinx.android.synthetic.main.dialog_bottom_sheet_betinfo_item.*
+import kotlinx.android.synthetic.main.dialog_bottom_sheet_betinfo_item_receipt.*
 import kotlinx.android.synthetic.main.item_match_receipt.view.*
 import kotlinx.android.synthetic.main.view_match_receipt_bet.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.bet.add.betReceipt.BetAddResult
-import org.cxct.sportlottery.ui.base.BaseBottomSheetFragment
 import org.cxct.sportlottery.ui.base.BaseSocketBottomSheetFragment
 import org.cxct.sportlottery.ui.game.GameViewModel
 import org.cxct.sportlottery.util.TextUtil
@@ -63,7 +60,14 @@ class BetInfoCarReceiptDialog(val result: BetAddResult) :
         super.onViewCreated(view, savedInstanceState)
 
         getCurrentMoney()
+        initOnclick()
         initObserver()
+    }
+
+    private fun initOnclick() {
+        btn_done.setOnClickListener {
+                this@BetInfoCarReceiptDialog.dismiss()
+        }
     }
 
     private fun getCurrentMoney() {
