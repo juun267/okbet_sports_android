c72e88a2f9252ba94081ddadb9f1a4ba96abbf08
Edward <edward@EdwarddeiMac.local>
2021-08-10 20:00:10 +0800

    [Add] Make match odd parcelable for navigation pass

--- app/src/main/java/org/cxct/sportlottery/network/odds/list/MatchOdd.kt
+++ app/src/main/java/org/cxct/sportlottery/network/odds/list/MatchOdd.kt
@@ -1,29 +1,33 @@
 package org.cxct.sportlottery.network.odds.list
 
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 import org.cxct.sportlottery.network.common.PlayCate
 import org.cxct.sportlottery.network.odds.Odd
 import org.cxct.sportlottery.network.service.match_status_change.MatchStatus
 import org.cxct.sportlottery.network.odds.MatchInfo
 import org.cxct.sportlottery.network.outright.odds.DynamicMarket
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class MatchOdd(
     @Json(name = "matchInfo")
     val matchInfo: MatchInfo? = null,
     @Json(name = "odds")
-    var odds: MutableMap<String, MutableList<Odd?>> = mutableMapOf(
+    var odds: @RawValue MutableMap<String, MutableList<Odd?>> = mutableMapOf(
         PlayCate.HDP.value to mutableListOf(),
         PlayCate.OU.value to mutableListOf(),
         PlayCate.SINGLE.value to mutableListOf()
     ),
     @Json(name = "dynamicMarkets")
-    val dynamicMarkets: Map<String, DynamicMarket>? = null,
+    val dynamicMarkets: @RawValue Map<String, DynamicMarket>? = null,
     @Json(name = "quickPlayCateList")
-    val quickPlayCateList: List<QuickPlayCate>? = null
-) {
+    val quickPlayCateList: @RawValue List<QuickPlayCate>? = null
+) : Parcelable {
     var isExpand = false
     var leagueTime: Int? = null
     var matchStatusList: List<MatchStatus> = listOf()
