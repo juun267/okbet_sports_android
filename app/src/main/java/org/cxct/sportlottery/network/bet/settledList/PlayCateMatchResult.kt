f0f32b8056f617d3e8f7a166040ab9e1008e9900
Cheryl <a222025115@gmail.com>
2021-08-12 15:09:00 +0800

    fix data type error. change ui.

---
+++ app/src/main/java/org/cxct/sportlottery/network/bet/settledList/PlayCateMatchResult.kt
@@ -0,0 +1,12 @@
+package org.cxct.sportlottery.network.bet.settledList
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PlayCateMatchResult(
+    @Json(name = "statusNameI18n")
+    val statusNameI18n: Map<String, String>?,
+    @Json(name = "score")
+    val score: String?,
+)
