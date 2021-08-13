05a524f9ecb67bd1ddf2dfad6c8043d9f86bb95d
Edward <edward@EdwarddeiMac.local>
2021-08-13 16:38:43 +0800

    [Add] League filter page ui

---
+++ app/src/main/java/org/cxct/sportlottery/ui/game/filter/LeagueFilterFragment.kt
@@ -0,0 +1,19 @@
+package org.cxct.sportlottery.ui.game.filter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cxct.sportlottery.R
+
+
+class LeagueFilterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_league_filter, container, false)
+    }
+}
