a8b02e3740484f547865aad49d89aba8ccf1d721
Edward <edward@EdwarddeiMac.local>
2021-08-13 17:58:55 +0800

    [Add] League filter page back behavior

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
@@ -468,7 +468,7 @@ class GameActivity : BaseSocketActivity<GameViewModel>(GameViewModel::class) {
             return
         }
         when (mNavController.currentDestination?.id) {
-            R.id.gameLeagueFragment, R.id.gameOutrightFragment, R.id.gameOutrightMoreFragment, R.id.oddsDetailFragment, R.id.oddsDetailLiveFragment -> {
+            R.id.gameLeagueFragment, R.id.gameOutrightFragment, R.id.gameOutrightMoreFragment, R.id.oddsDetailFragment, R.id.oddsDetailLiveFragment, R.id.leagueFilterFragment -> {
                 mNavController.navigateUp()
             }
 
