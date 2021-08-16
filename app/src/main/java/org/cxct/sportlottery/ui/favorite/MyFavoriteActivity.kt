7a03849dc775569e9c19d981a401c7c8d119d887
Bill <h853047455@gmail.com>
2021-08-16 14:04:38 +0800

    [Fix] 修正我的賽事點選投注單邏輯

--- app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteActivity.kt
@@ -7,13 +7,17 @@ import kotlinx.android.synthetic.main.activity_my_favorite.*
 import kotlinx.android.synthetic.main.fragment_game_v3.*
 import kotlinx.android.synthetic.main.view_bottom_navigation_sport.*
 import org.cxct.sportlottery.R
-import org.cxct.sportlottery.ui.base.BaseFavoriteActivity
+import org.cxct.sportlottery.network.bet.add.betReceipt.Receipt
+import org.cxct.sportlottery.network.bet.info.ParlayOdd
+import org.cxct.sportlottery.ui.base.BaseSocketActivity
 import org.cxct.sportlottery.ui.bet.list.BetInfoCarDialog
 import org.cxct.sportlottery.ui.game.GameActivity
+import org.cxct.sportlottery.ui.game.betList.BetListFragment
+import org.cxct.sportlottery.ui.game.betList.receipt.BetReceiptFragment
 import org.cxct.sportlottery.ui.main.accountHistory.AccountHistoryActivity
 import org.cxct.sportlottery.ui.transactionStatus.TransactionStatusActivity
 
-class MyFavoriteActivity : BaseFavoriteActivity<MyFavoriteViewModel>(MyFavoriteViewModel::class) {
+class MyFavoriteActivity : BaseSocketActivity<MyFavoriteViewModel>(MyFavoriteViewModel::class) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +53,7 @@ class MyFavoriteActivity : BaseFavoriteActivity<MyFavoriteViewModel>(MyFavoriteV
                     true
                 }
                 R.id.item_bet_list -> {
-                    showBetListDialog()
+                    showBetListPage()
                     false
                 }
                 R.id.navigation_account_history -> {
@@ -73,6 +77,29 @@ class MyFavoriteActivity : BaseFavoriteActivity<MyFavoriteViewModel>(MyFavoriteV
                 else -> false
             }
         }
+    }
+
+    private fun showBetListPage() {
+        val betListFragment = BetListFragment.newInstance(object : BetListFragment.BetResultListener {
+            override fun onBetResult(betResultData: Receipt?, betParlayList: List<ParlayOdd>) {
+                supportFragmentManager.beginTransaction()
+                    .setCustomAnimations(R.anim.push_right_to_left_enter, R.anim.pop_bottom_to_top_exit, R.anim.push_right_to_left_enter, R.anim.pop_bottom_to_top_exit)
+                    .replace(R.id.fl_bet_list, BetReceiptFragment.newInstance(betResultData, betParlayList))
+                    .addToBackStack(BetReceiptFragment::class.java.simpleName)
+                    .commit()
+            }
+
+        })
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.push_bottom_to_top_enter,
+                R.anim.pop_bottom_to_top_exit,
+                R.anim.push_bottom_to_top_enter,
+                R.anim.pop_bottom_to_top_exit
+            )
+            .add(R.id.fl_bet_list, betListFragment)
+            .addToBackStack(BetListFragment::class.java.simpleName)
+            .commit()
     }
 
     private fun initObserver() {
