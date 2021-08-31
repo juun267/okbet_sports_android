091ac660789a409a8f151343e35608e16ee872c6
Edward <edward@EdwarddeiMac.local>
2021-08-31 10:21:58 +0800

    [Add] Statistics page

---
+++ app/src/main/java/org/cxct/sportlottery/ui/statistics/StatisticsActivity.kt
@@ -0,0 +1,62 @@
+package org.cxct.sportlottery.ui.statistics
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_statistics.*
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.repository.sConfigData
+import org.cxct.sportlottery.ui.base.BaseActivity
+import org.cxct.sportlottery.util.LanguageManager
+
+
+const val KEY_MATCH_ID = "matchId"
+
+class StatisticsActivity : BaseActivity<StatisticsViewModel>(StatisticsViewModel::class) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_statistics)
+
+        setupToolbar()
+
+        setupWebView(intent.extras?.getString(KEY_MATCH_ID))
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(statistics_toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        statistics_close.setOnClickListener {
+            finish()
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView(matchId: String?) {
+
+        statistics_web.apply {
+            settings.javaScriptEnabled = true
+
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+
+                    hideLoading()
+
+                }
+            }
+
+            loadUrl(
+                sConfigData?.analysisUrl?.replace(
+                    "{lang}",
+                    LanguageManager.getSelectLanguage(context).key
+                )?.replace("{eventId}", matchId ?: "")
+            ).run {
+                loading()
+            }
+        }
+    }
+}
