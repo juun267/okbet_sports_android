9a28789b130aa61760fb91c7d1b7acfe395ee709
Edward <edward@EdwarddeiMac.local>
2021-09-01 10:44:38 +0800

    [Fix] Fetch new page not post value

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/creditrecord/CreditRecordViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/creditrecord/CreditRecordViewModel.kt
@@ -91,7 +91,11 @@ class CreditRecordViewModel(
                     it.postRemainDay()
                     _userCreditCircleHistory.postValue(it)
                 } else {
-                    _userCreditCircleHistory.value?.toMutableList()?.addAll(it)
+                    val currentList =
+                        _userCreditCircleHistory.value?.toMutableList() ?: mutableListOf()
+                    currentList.addAll(it)
+
+                    _userCreditCircleHistory.postValue(currentList)
                 }
             }
 
