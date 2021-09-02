e0e2886ccb7ef9b6ccb747bd10984a19e007649d
Cheryl <a222025115@gmail.com>
2021-09-02 10:18:40 +0800

    clean code.

--- app/src/main/java/org/cxct/sportlottery/ui/main/accountHistory/next/AccountHistoryNextFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/main/accountHistory/next/AccountHistoryNextFragment.kt
@@ -1,7 +1,6 @@
 package org.cxct.sportlottery.ui.main.accountHistory.next
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
