4ca91c0f21371c3e6333a057cae833b737190f51
Bill <h853047455@gmail.com>
2021-09-03 10:54:22 +0800

    [Ffx] 13104 [v.4]【Android】[滾球] 已選擇過有下單選單的盤口，切換至其他盤口，再切回有下拉選單的盤口，應直接開啟不需重新選擇

--- app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteFragment.kt
@@ -452,7 +452,8 @@ class MyFavoriteFragment : BaseSocketFragment<MyFavoriteViewModel>(MyFavoriteVie
         })
 
         viewModel.curPlay.observe(this.viewLifecycleOwner, {
-            showPlayCateBottomSheet(it)
+            if (it.selectionType == SelectionType.SELECTABLE.code && it.isLocked == false)
+                showPlayCateBottomSheet(it)
         })
 
         viewModel.favorMatchOddList.observe(this.viewLifecycleOwner, {
