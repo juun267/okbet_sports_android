5c453de131ac1efcd67d61adb97a39dd6278bff3
Edward <edward@EdwarddeiMac.local>
2021-09-08 15:52:16 +0800

    [Add] Odd button pair basic data to adapter

--- app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPairAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPairAdapter.kt
@@ -4,8 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.itemview_odd_btn_pair.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.odds.Odd
+import org.cxct.sportlottery.ui.menu.OddsType
 
 class OddButtonPairAdapter : RecyclerView.Adapter<OddButtonPairViewHolder>() {
 
@@ -15,6 +17,14 @@ class OddButtonPairAdapter : RecyclerView.Adapter<OddButtonPairViewHolder>() {
 
             data = field.withIndex().groupBy {
                 it.index / 2
+            }
+        }
+
+    var oddsType: OddsType = OddsType.EU
+        set(value) {
+            if (value != field) {
+                field = value
+                notifyDataSetChanged()
             }
         }
 
@@ -35,6 +45,9 @@ class OddButtonPairAdapter : RecyclerView.Adapter<OddButtonPairViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: OddButtonPairViewHolder, position: Int) {
+        data[position]?.let {
+            holder.bind(it, oddsType)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -46,6 +59,20 @@ class OddButtonPairViewHolder private constructor(
     itemView: View,
     override val oddStateChangeListener: OddStateChangeListener
 ) : OddStateViewHolder(itemView) {
+
+    fun bind(oddPair: List<IndexedValue<Odd?>>, oddsType: OddsType) {
+        itemView.quick_odd_btn_pair_one_1.apply pair1ButtonSettings@{
+            if (oddPair.size < 2) return@pair1ButtonSettings
+
+            setupOdd(oddPair[0].value, oddsType)
+        }
+
+        itemView.quick_odd_btn_pair_one_2.apply pair2ButtonSettings@{
+            if (oddPair.size < 2) return@pair2ButtonSettings
+
+            setupOdd(oddPair[1].value, oddsType)
+        }
+    }
 
     companion object {
         fun from(
