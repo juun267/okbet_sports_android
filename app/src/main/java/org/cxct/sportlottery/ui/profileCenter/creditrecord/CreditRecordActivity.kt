943997af792a9e6e2e9e7e7299bd3031753a56c6
Cheryl <a222025115@gmail.com>
2021-09-08 22:39:01 +0800

    Revert "fixed 14409 [v.4]【Android】[信用盤] 周期歷史紀錄，下拉後，結算倒數日與最新一期區塊中間多了空白"
    This reverts commit 3cf745e3e0ff2617b143cc63d56e912ebb304ecd.

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/creditrecord/CreditRecordActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/creditrecord/CreditRecordActivity.kt
@@ -70,6 +70,12 @@ class CreditRecordActivity :
 
     private fun setupCreditRecordList() {
         credit_record_list.apply {
+            addItemDecoration(
+                SpaceItemDecoration(
+                    context,
+                    R.dimen.item_spacing_credit_record
+                )
+            )
 
             adapter = creditRecordAdapter
 
