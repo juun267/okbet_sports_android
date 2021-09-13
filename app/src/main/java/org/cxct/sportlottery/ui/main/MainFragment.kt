a88570b61d509e23a8b66ff55623c1e2f760d747
Bill <h853047455@gmail.com>
2021-09-13 15:45:27 +0800

    [Add] 新增首頁版本更新UI語系判斷

--- app/src/main/java/org/cxct/sportlottery/ui/main/MainFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/main/MainFragment.kt
@@ -35,6 +35,7 @@ import org.cxct.sportlottery.ui.main.entity.GameItemData
 import org.cxct.sportlottery.ui.main.entity.ThirdGameCategory
 import org.cxct.sportlottery.ui.profileCenter.versionUpdate.VersionUpdateActivity
 import org.cxct.sportlottery.util.JumpUtil
+import org.cxct.sportlottery.util.LanguageManager
 
 
 class MainFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
@@ -487,8 +488,14 @@ class MainFragment : BaseFragment<MainViewModel>(MainViewModel::class) {
     }
 
     private fun setupUpdate() {
-        btn_update.setOnClickListener {
-            startActivity(Intent(activity, VersionUpdateActivity::class.java))
+        btn_update.apply {
+            when (LanguageManager.getSelectLanguage(context)) {
+                LanguageManager.Language.ZH, LanguageManager.Language.ZHT -> setImageResource(R.drawable.ic_main_btn_update_v4_vn)
+                else -> setImageResource(R.drawable.ic_main_btn_update_v4_en)
+            }
+            setOnClickListener {
+                startActivity(Intent(activity, VersionUpdateActivity::class.java))
+            }
         }
     }
 
