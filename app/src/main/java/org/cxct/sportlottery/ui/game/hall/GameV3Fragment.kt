aabe4a104aa1ec66ee49ac165e14601461191818
Bill <h853047455@gmail.com>
2021-10-07 13:40:15 +0800

    [Add] Add display logic of champion Icon

--- app/src/main/java/org/cxct/sportlottery/ui/game/hall/GameV3Fragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/hall/GameV3Fragment.kt
@@ -279,7 +279,13 @@ class GameV3Fragment : BaseSocketFragment<GameViewModel>(GameViewModel::class) {
 
         view.game_toolbar_champion.apply {
             visibility = when (args.matchType) {
-                MatchType.IN_PLAY, MatchType.AT_START -> View.VISIBLE
+                MatchType.IN_PLAY -> View.VISIBLE
+                MatchType.AT_START -> {
+                    if (leagueAdapter.data.isEmpty())
+                        View.GONE
+                    else
+                        View.VISIBLE
+                }
                 else -> View.GONE
             }
 
