7430a48a734b3afde2163ef54c05fb34baf7456c
Dean <>
2021-10-14 11:50:53 +0800

    [Fix] intercept crash when response exception

--- app/src/main/java/org/cxct/sportlottery/network/interceptor/LogInterceptor.kt
+++ app/src/main/java/org/cxct/sportlottery/network/interceptor/LogInterceptor.kt
@@ -1,15 +1,14 @@
 package org.cxct.sportlottery.network.interceptor
 
-import okhttp3.Headers
-import okhttp3.Interceptor
-import okhttp3.Protocol
-import okhttp3.Response
+import okhttp3.*
+import okhttp3.ResponseBody.Companion.toResponseBody
 import okio.Buffer
 import org.json.JSONObject
 import timber.log.Timber
 import java.io.IOException
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.TimeUnit
+import kotlin.Exception
 import kotlin.jvm.Throws
 
 class LogInterceptor @JvmOverloads constructor(private val logger: Logger = Logger.DEFAULT) :
@@ -61,127 +60,137 @@ class LogInterceptor @JvmOverloads constructor(private val logger: Logger = Logg
     override fun intercept(chain: Interceptor.Chain): Response {
         val level = level
         val request = chain.request()
-        if (level == Level.NONE) {
-            return chain.proceed(request)
-        }
-        val logBody = level == Level.BODY
-        val logHeaders =
-            logBody || level == Level.HEADERS
-        val requestBody = request.body
-        val hasRequestBody = requestBody != null
-        val connection = chain.connection()
-        val protocol =
-            connection?.protocol() ?: Protocol.HTTP_1_1
-        var requestStartMessage =
-            "--> " + request.method + ' ' + request.url + ' ' + protocol(
-                protocol
-            )
-        if (!logHeaders && hasRequestBody) {
-            requestStartMessage += " (" + requestBody!!.contentLength() + "-byte body)"
-        }
-        logger.log(requestStartMessage)
-        if (logHeaders) {
-            if (hasRequestBody) {
-                // Request body headers are only present when installed as a network interceptor. Force
-                // them to be included (when available) so there values are known.
-                if (requestBody!!.contentType() != null) {
-                    logger.log("Content-Type: " + requestBody.contentType())
-                }
-                if (requestBody.contentLength() != -1L) {
-                    logger.log("Content-Length: " + requestBody.contentLength())
-                }
+        return try {
+
+            if (level == Level.NONE) {
+                return chain.proceed(request)
             }
-            val headers = request.headers
-            var i = 0
-            val count = headers.size
-            while (i < count) {
-                val name = headers.name(i)
-                // Skip headers from the request body as they are explicitly logged above.
-                if (!"Content-Type".equals(
-                        name,
-                        ignoreCase = true
-                    ) && !"Content-Length".equals(name, ignoreCase = true)
-                ) {
-                    logger.log("Header: " + name + ": " + headers.value(i))
-                }
-                i++
-            }
-            if (!logBody || !hasRequestBody) {
-                logger.log("--> END " + request.method)
-            } else if (bodyEncoded(request.headers)) {
-                logger.log("--> END " + request.method + " (encoded body omitted)")
-            } else {
-                val buffer = Buffer()
-                requestBody!!.writeTo(buffer)
-                var charset = UTF8
-                val contentType = requestBody.contentType()
-                if (contentType != null) {
-                    charset = contentType.charset(UTF8)
-                }
-                logger.log("")
-                logger.log(buffer.readString(charset!!))
-                logger.log(
-                    "--> END " + request.method + " (" + requestBody.contentLength() + "-byte body)"
+            val logBody = level == Level.BODY
+            val logHeaders =
+                logBody || level == Level.HEADERS
+            val requestBody = request.body
+            val hasRequestBody = requestBody != null
+            val connection = chain.connection()
+            val protocol =
+                connection?.protocol() ?: Protocol.HTTP_1_1
+            var requestStartMessage =
+                "--> " + request.method + ' ' + request.url + ' ' + protocol(
+                    protocol
                 )
+            if (!logHeaders && hasRequestBody) {
+                requestStartMessage += " (" + requestBody!!.contentLength() + "-byte body)"
             }
-        }
-        val startNs = System.nanoTime()
-        val response = chain.proceed(request)
-        val tookMs =
-            TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
-        val responseBody = response.body
-        val contentLength = responseBody!!.contentLength()
-        val bodySize =
-            if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
-        logger.log(
-            "<-- "
-                    + response.code
-                    + ' '
-                    // + response.message
-                    // + ' '
-                    + response.request.url
-                    + " ("
-                    + tookMs
-                    + "ms"
-                    + (if (!logHeaders) ", $bodySize body" else "")
-                    + ')'
-        )
-        if (logHeaders) {
-            val headers = response.headers
-            val head = StringBuilder()
-            var i = 0
-            val count = headers.size
-            while (i < count) {
-                head.append(headers.name(i)).append(": ").append(headers.value(i)).append(",")
-                i++
-            }
-            logger.log(head.toString())
-            if (!logBody /*|| !HttpEngine.hasBody(response)*/) {
-                logger.log("<-- END HTTP")
-            } else if (bodyEncoded(response.headers)) {
-                logger.log("<-- END HTTP (encoded body omitted)")
-            } else {
-                val source = responseBody.source()
-                source.request(Long.MAX_VALUE) // Buffer the entire body.
-                val buffer = source.buffer
-                var charset = UTF8
-                val contentType = responseBody.contentType()
-                if (contentType != null) {
-                    charset = contentType.charset(UTF8)
-                }
-                if (contentLength != 0L) {
-                    logger.log("")
-                    val string = buffer.clone().readString(charset!!)
-                    try {
-                        logger.log(JSONObject(string).toString(4))
-                    } catch (e: Exception) {
-                        logger.log(string)
+            logger.log(requestStartMessage)
+            if (logHeaders) {
+                if (hasRequestBody) {
+                    // Request body headers are only present when installed as a network interceptor. Force
+                    // them to be included (when available) so there values are known.
+                    if (requestBody!!.contentType() != null) {
+                        logger.log("Content-Type: " + requestBody.contentType())
+                    }
+                    if (requestBody.contentLength() != -1L) {
+                        logger.log("Content-Length: " + requestBody.contentLength())
                     }
                 }
-                logger.log("<-- END HTTP (" + buffer.size + "-byte body)")
+                val headers = request.headers
+                var i = 0
+                val count = headers.size
+                while (i < count) {
+                    val name = headers.name(i)
+                    // Skip headers from the request body as they are explicitly logged above.
+                    if (!"Content-Type".equals(
+                            name,
+                            ignoreCase = true
+                        ) && !"Content-Length".equals(name, ignoreCase = true)
+                    ) {
+                        logger.log("Header: " + name + ": " + headers.value(i))
+                    }
+                    i++
+                }
+                if (!logBody || !hasRequestBody) {
+                    logger.log("--> END " + request.method)
+                } else if (bodyEncoded(request.headers)) {
+                    logger.log("--> END " + request.method + " (encoded body omitted)")
+                } else {
+                    val buffer = Buffer()
+                    requestBody!!.writeTo(buffer)
+                    var charset = UTF8
+                    val contentType = requestBody.contentType()
+                    if (contentType != null) {
+                        charset = contentType.charset(UTF8)
+                    }
+                    logger.log("")
+                    logger.log(buffer.readString(charset!!))
+                    logger.log(
+                        "--> END " + request.method + " (" + requestBody.contentLength() + "-byte body)"
+                    )
+                }
             }
+            val startNs = System.nanoTime()
+            val response = chain.proceed(request)
+            val tookMs =
+                TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
+            val responseBody = response.body
+            val contentLength = responseBody!!.contentLength()
+            val bodySize =
+                if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
+            logger.log(
+                "<-- "
+                        + response.code
+                        + ' '
+                        // + response.message
+                        // + ' '
+                        + response.request.url
+                        + " ("
+                        + tookMs
+                        + "ms"
+                        + (if (!logHeaders) ", $bodySize body" else "")
+                        + ')'
+            )
+            if (logHeaders) {
+                val headers = response.headers
+                val head = StringBuilder()
+                var i = 0
+                val count = headers.size
+                while (i < count) {
+                    head.append(headers.name(i)).append(": ").append(headers.value(i)).append(",")
+                    i++
+                }
+                logger.log(head.toString())
+                if (!logBody /*|| !HttpEngine.hasBody(response)*/) {
+                    logger.log("<-- END HTTP")
+                } else if (bodyEncoded(response.headers)) {
+                    logger.log("<-- END HTTP (encoded body omitted)")
+                } else {
+                    val source = responseBody.source()
+                    source.request(Long.MAX_VALUE) // Buffer the entire body.
+                    val buffer = source.buffer
+                    var charset = UTF8
+                    val contentType = responseBody.contentType()
+                    if (contentType != null) {
+                        charset = contentType.charset(UTF8)
+                    }
+                    if (contentLength != 0L) {
+                        logger.log("")
+                        val string = buffer.clone().readString(charset!!)
+                        try {
+                            logger.log(JSONObject(string).toString(4))
+                        } catch (e: Exception) {
+                            logger.log(string)
+                        }
+                    }
+                    logger.log("<-- END HTTP (" + buffer.size + "-byte body)")
+                }
+            }
+            return response
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Response.Builder()
+                .request(request)
+                .protocol(Protocol.HTTP_1_1)
+                .code(999).message(e.message ?: "")
+                .body("$e".toResponseBody(null)).build()
         }
-        return response
     }
 
     private fun bodyEncoded(headers: Headers): Boolean {
