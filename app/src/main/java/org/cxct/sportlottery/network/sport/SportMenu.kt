1d4b741a6488fa24b434f7adbd52caa9ee53de12
Dean <>
2021-10-18 11:47:53 +0800

    [Add] api /api/front/sport/list

---
+++ app/src/main/java/org/cxct/sportlottery/network/sport/SportMenu.kt
@@ -0,0 +1,11 @@
+package org.cxct.sportlottery.network.sport
+
+import androidx.annotation.DrawableRes
+import org.cxct.sportlottery.network.common.GameType
+
+data class SportMenu(
+    val gameType: GameType,
+    val sportName: String,
+    val sportEnName: String?,
+    @DrawableRes val icon: Int?
+)
