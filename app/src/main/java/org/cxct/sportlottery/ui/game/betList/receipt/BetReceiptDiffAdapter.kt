aec6d79e23e43d74807ae4275c90c9fbc849eb82
Dean <>
2021-10-27 16:32:48 +0800

    [Fix] 投注單收據串關名稱英文語系

--- app/src/main/java/org/cxct/sportlottery/ui/game/betList/receipt/BetReceiptDiffAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/betList/receipt/BetReceiptDiffAdapter.kt
@@ -21,6 +21,7 @@ import org.cxct.sportlottery.network.bet.info.ParlayOdd
 import org.cxct.sportlottery.network.common.MatchType
 import org.cxct.sportlottery.network.common.PlayCate.Companion.needShowSpread
 import org.cxct.sportlottery.ui.menu.OddsType
+import org.cxct.sportlottery.ui.transactionStatus.ParlayType.Companion.getParlayStringRes
 import org.cxct.sportlottery.util.*
 
 class BetReceiptDiffAdapter : ListAdapter<DataItem, RecyclerView.ViewHolder>(BetReceiptCallback()) {
@@ -158,7 +159,10 @@ class BetReceiptDiffAdapter : ListAdapter<DataItem, RecyclerView.ViewHolder>(Bet
             itemView.apply {
                 itemData.apply {
                     matchOdds?.firstOrNull()?.apply {
-                        parlayType?.let { tv_play_name_parlay.text = TextUtil.replaceParlayByC(it) }
+                        parlayType?.let { parlayTypeCode ->
+                            tv_play_name_parlay.text =
+                                getParlayStringRes(parlayTypeCode)?.let { context.getString(it) } ?: ""
+                        }
                     }
 
                     tv_match_at.visibility = if (firstItem) View.VISIBLE else View.GONE
