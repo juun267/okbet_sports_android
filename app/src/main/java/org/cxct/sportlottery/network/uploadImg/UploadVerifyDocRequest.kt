5cebf0e33b6542afb7eb40603350c61ee2a8d550
Dean <>
2021-11-06 13:01:24 +0800

    [Add] 實名制文件、照片上傳

---
+++ app/src/main/java/org/cxct/sportlottery/network/uploadImg/UploadVerifyDocRequest.kt
@@ -0,0 +1,24 @@
+package org.cxct.sportlottery.network.uploadImg
+
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import org.cxct.sportlottery.repository.PLATFORM_CODE
+import org.cxct.sportlottery.repository.PROJECT_CODE
+import java.io.File
+
+class UploadVerifyDocRequest(val userId: String, private val docFile: File) {
+    fun toPars(): List<MultipartBody.Part> {
+        val mediaType = "image/*".toMediaTypeOrNull()
+        val requestFile = docFile.asRequestBody(mediaType)
+
+        return MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("projectCode", PROJECT_CODE) //项目代码
+            .addFormDataPart("platformCode", "id/img") //平台代码
+            .addFormDataPart("userId", userId)
+            .addFormDataPart("expireAfterDays", "0") //多久过期（天）0：不过期，N：N天后过期（1<=N<=365）
+            .addFormDataPart("file", docFile.name, requestFile)
+            .build().parts
+    }
+}
