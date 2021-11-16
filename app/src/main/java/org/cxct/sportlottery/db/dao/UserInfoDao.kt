4143dd98c9d07adab99fc03c531ada179fa62115
Bill <h853047455@gmail.com>
2021-11-16 17:53:06 +0800

    [Add] add new method to get data of bet money max from database

--- app/src/main/java/org/cxct/sportlottery/db/dao/UserInfoDao.kt
+++ app/src/main/java/org/cxct/sportlottery/db/dao/UserInfoDao.kt
@@ -52,11 +52,20 @@ interface UserInfoDao {
     @Query("UPDATE user_info_table SET maxBetMoney=:maxBetMoney WHERE user_id=:userId")
     suspend fun updateMaxBetMoney(userId: Long, maxBetMoney: Int)
 
+    @Query("SELECT maxBetMoney FROM user_info_table WHERE user_id = :userId")
+    fun getMaxBetMoney(userId: Long): Int
+
     @Query("UPDATE user_info_table SET maxParlayBetMoney=:maxParlayBetMoney WHERE user_id=:userId")
     suspend fun updateMaxParlayBetMoney(userId: Long, maxParlayBetMoney: Int)
 
+    @Query("SELECT maxParlayBetMoney FROM user_info_table WHERE user_id = :userId")
+    fun getMaxParlayBetMoney(userId: Long): Int
+
     @Query("UPDATE user_info_table SET maxCpBetMoney=:maxCpBetMoney WHERE user_id=:userId")
     suspend fun updateMaxCpBetMoney(userId: Long, maxCpBetMoney: Int)
+
+    @Query("SELECT maxCpBetMoney FROM user_info_table WHERE user_id = :userId")
+    fun getMaxCpBetMoney(userId: Long): Int
 
     @Query("UPDATE user_info_table SET discount=:discount WHERE user_id=:userId")
     suspend fun updateDiscount(userId: Long, discount: Float)
