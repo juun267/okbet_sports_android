f34908d04134f001ddb145387bb1c4aed64fb890
Bill <h853047455@gmail.com>
2021-12-01 14:37:24 +0800

    [Refactor] new game type content of CB

--- app/src/main/java/org/cxct/sportlottery/ui/game/hall/adapter/GameTypeAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/hall/adapter/GameTypeAdapter.kt
@@ -96,6 +96,9 @@ class GameTypeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 }
                 GameType.BX.key -> {
                     itemView.sport_type_img.setImageResource(R.drawable.selector_sport_type_item_img_bx_v5)
+                GameType.CB.key -> {
+                    itemView.sport_type_img.setImageResource(R.drawable.selector_sport_type_item_img_cb_v5)
+                }
                 GameType.BB.key -> {
                     itemView.sport_type_img.setImageResource(R.drawable.selector_sport_type_item_img_bb_v5)
                 }
