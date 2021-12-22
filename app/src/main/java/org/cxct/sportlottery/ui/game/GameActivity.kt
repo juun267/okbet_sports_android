a9391c2eb3e76f3d61b011a2dd07bdffdc8a24a6
Bill <h853047455@gmail.com>
2021-12-22 16:57:06 +0800

    [Deleted] clear unused import

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
@@ -3,7 +3,6 @@ package org.cxct.sportlottery.ui.game
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
