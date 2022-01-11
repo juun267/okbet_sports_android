eeda982a1c4e17bf248282f2d4df23edf300af1f
Bill <h853047455@gmail.com>
2022-01-11 11:12:58 +0800

    [Refactor] 修改UI 與 h5 一致

--- app/src/main/java/org/cxct/sportlottery/ui/game/home/gameTable4/RvGameTable4Adapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/home/gameTable4/RvGameTable4Adapter.kt
@@ -235,7 +235,7 @@ class RvGameTable4Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 indicator_view.setupWithViewPager2(view_pager)
                 indicator_view.apply {
                     visibility = if (data.matchOdds!!.size <= 1) {
-                        View.INVISIBLE
+                        View.GONE
                     } else {
                         View.VISIBLE
                     }
