45514bf925328f20a0baf2931e738f9c6c6bf681
Martin <>
2022-01-19 15:58:09 +0800

    修改按鈕紅綠為一秒

--- app/src/main/java/org/cxct/sportlottery/ui/game/common/OddStateViewHolder.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/common/OddStateViewHolder.kt
@@ -13,7 +13,7 @@ abstract class OddStateViewHolder(itemView: View) : RecyclerView.ViewHolder(item
     }
 
     companion object {
-        private const val HIGH_LIGHT_TIME: Long = 3000
+        private const val HIGH_LIGHT_TIME: Long = 1000
     }
 
     abstract val oddStateChangeListener: OddStateChangeListener
