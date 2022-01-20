ded9b74b594f4d28f35dddd73c7adefd2a90510f
Martin <>
2022-01-20 17:32:51 +0800

    no message

--- app/src/main/java/org/cxct/sportlottery/db/dao/UserInfoDao.kt
+++ app/src/main/java/org/cxct/sportlottery/db/dao/UserInfoDao.kt
@@ -19,9 +19,6 @@ interface UserInfoDao {
     @Query("SELECT discount FROM user_info_table WHERE user_id = :userId")
     fun getDiscount(userId: Long): Float?
 
-    @Query("SELECT phone FROM user_info_table WHERE user_id = :userId")
-    fun getUserPhone(userId: Long): String?
-
     @Update(onConflict = OnConflictStrategy.REPLACE)
     suspend fun update(vararg userInfo: UserInfo)
 
