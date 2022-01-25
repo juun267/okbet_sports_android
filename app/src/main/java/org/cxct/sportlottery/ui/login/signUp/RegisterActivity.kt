65eb920d930a82a5e8e8ff644f1e63c791f63003
Cheryl <a222025115@gmail.com>
2022-01-25 16:52:06 +0800

    fixed 19414 [v1217]【Android】註冊頁，必填資料未填寫或取消勾選必要註冊條款時按鈕需無法點擊(反灰)

--- app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterActivity.kt
@@ -1,6 +1,5 @@
 package org.cxct.sportlottery.ui.login.signUp
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -11,16 +10,11 @@ import android.text.method.HideReturnsTransformationMethod
 import android.text.method.LinkMovementMethod
 import android.text.method.PasswordTransformationMethod
 import android.text.style.ClickableSpan
-import android.text.style.URLSpan
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import cn.jpush.android.api.JPushInterface
 import com.bumptech.glide.Glide
-import org.cxct.sportlottery.MultiLanguagesApplication.Companion.UUID
-import org.cxct.sportlottery.MultiLanguagesApplication.Companion.UUID_DEVICE_CODE
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.databinding.ActivityRegisterBinding
 import org.cxct.sportlottery.network.Constants
@@ -34,11 +28,7 @@ import org.cxct.sportlottery.ui.common.CustomAlertDialog
 import org.cxct.sportlottery.ui.login.signIn.LoginActivity
 import org.cxct.sportlottery.util.BitmapUtil
 import org.cxct.sportlottery.util.JumpUtil
-import org.cxct.sportlottery.util.LanguageManager
 import org.cxct.sportlottery.util.ToastUtil
-import org.cxct.sportlottery.widget.boundsEditText.ExtendedEditText
-import org.cxct.sportlottery.widget.boundsEditText.TextFieldBoxes
-import timber.log.Timber
 import java.util.*
 
 class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::class) ,View.OnClickListener{
@@ -72,10 +62,6 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
         setContentView(binding.root)
 
         setupBackButton()
-        setupRecommendCode()
-        setupMemberAccount()
-        setupLoginPassword()
-        setupConfirmPassword()
         setupFullName()
         setupWithdrawalPassword()
         setupQQ()
@@ -89,7 +75,6 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
         setupValidCode()
         setupSmsValidCode()
         setupAgreement()
-        setupRegisterAgreementButton()
         setupRegisterButton()
         setupGoToLoginButton()
         initObserve()
@@ -129,8 +114,6 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
 
         binding.ivReturn.setOnClickListener(this)
         binding.tvDuty.setOnClickListener(this)
-        //binding.tvPrivacy.setOnClickListener(this)
-        //binding.tvAgreement.setOnClickListener(this)
         binding.tvPrivacy.text = getString(R.string.register_privacy)+getString(R.string.register_privacy_policy)+getString(R.string.register_privacy_policy_promotions)
         binding.tvPrivacy.makeLinks(
             Pair(applicationContext.getString(R.string.register_privacy_policy), View.OnClickListener {
@@ -153,144 +136,50 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
         binding.btnBack.setOnClickListener { finish() }
     }
 
-//    private fun setupEditTextFocusListener(
-//        editText: ExtendedEditText,
-//        doFun: (inputText: String) -> Unit
-//    ) {
-//        editText.setEditTextOnFocusChangeListener { _, hasFocus ->
-//            if (!hasFocus)
-//                doFun.invoke(editText.getText())
-//        }
-//    }
-
-
-    private fun setupRecommendCode() {
-//        et_recommend_code.apply {
-//            setNecessarySymbolVisibility(if (sConfigData?.enableInviteCode == FLAG_OPEN) View.VISIBLE else View.INVISIBLE)
-//            setupEditTextFocusListener(this) { viewModel.checkInviteCode(it) }
-//        }
-
-    }
-
-    private fun setupMemberAccount() {
-//        setupEditTextFocusListener(et_member_account) { viewModel.checkAccountExist(it) }
-    }
-
-    private fun setupLoginPassword() {
-
-        //setupEditTextFocusListener(et_login_password) { viewModel.checkLoginPassword(it) }
-    }
-
-    private fun setupConfirmPassword() {
-//        setupEditTextFocusListener(et_confirm_password) {
-//            viewModel.checkConfirmPassword(
-//                et_login_password.getText(),
-//                it
-//            )
-//        }
-
-        //20210427 紀錄：當確認密碼下方的 label 都被關時，要動態調整 MarginBottom 高度，為了符合排版要求
-//        if (sConfigData?.enableFullName != FLAG_OPEN &&
-//            sConfigData?.enableFundPwd != FLAG_OPEN &&
-//            sConfigData?.enableQQ != FLAG_OPEN &&
-//            sConfigData?.enablePhone != FLAG_OPEN &&
-//            sConfigData?.enableEmail != FLAG_OPEN &&
-//            sConfigData?.enableWechat != FLAG_OPEN &&
-//            sConfigData?.enableZalo != FLAG_OPEN &&
-//            sConfigData?.enableFacebook != FLAG_OPEN &&
-//            sConfigData?.enableWhatsApp != FLAG_OPEN &&
-//            sConfigData?.enableTelegram != FLAG_OPEN &&
-//            sConfigData?.enableTelegram != FLAG_OPEN &&
-//            sConfigData?.enableRegValidCode != FLAG_OPEN &&
-//            sConfigData?.enableSmsValidCode != FLAG_OPEN
-//        )
-//            et_confirm_password.setMarginBottom(10.dp)
-    }
-
     private fun setupFullName() {
-//        et_full_name.visibility = if (sConfigData?.enableFullName == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_full_name) { viewModel.checkFullName(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etFullName.visibility = if (sConfigData?.enableFullName == FLAG_OPEN) View.VISIBLE else View.GONE
     }
 
     private fun setupWithdrawalPassword() {
-//        et_withdrawal_pwd.visibility = if (sConfigData?.enableFundPwd == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_withdrawal_pwd) { viewModel.checkFundPwd(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etWithdrawalPwd.visibility = if (sConfigData?.enableFundPwd == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupQQ() {
-//        et_qq.visibility = if (sConfigData?.enableQQ == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_qq) { viewModel.checkQQ(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etQq.visibility = if (sConfigData?.enableQQ == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupPhone() {
-//        et_phone.visibility = if (sConfigData?.enablePhone == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_phone) { viewModel.checkPhone(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etPhone.visibility = if (sConfigData?.enablePhone == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupMail() {
-//        et_mail.visibility = if (sConfigData?.enableEmail == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_mail) { viewModel.checkEmail(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etMail.visibility = if (sConfigData?.enableEmail == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupWeChat() {
-//        et_we_chat.visibility = if (sConfigData?.enableWechat == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_we_chat) { viewModel.checkWeChat(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etWeChat.visibility = if (sConfigData?.enableWechat == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupZalo() {
-//        et_zalo.visibility = if (sConfigData?.enableZalo == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_zalo) { viewModel.checkZalo(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etZalo.visibility = if (sConfigData?.enableZalo == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupFacebook() {
-//        et_facebook.visibility = if (sConfigData?.enableFacebook == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_facebook) { viewModel.checkFacebook(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etFacebook.visibility = if (sConfigData?.enableFacebook == FLAG_OPEN) View.VISIBLE else View.GONE
     }
 
     private fun setupWhatsApp() {
-//        et_whats_app.visibility = if (sConfigData?.enableWhatsApp == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_whats_app) { viewModel.checkWhatsApp(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etWhatsApp.visibility = if (sConfigData?.enableWhatsApp == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
 
     private fun setupTelegram() {
-//        et_telegram.visibility = if (sConfigData?.enableTelegram == FLAG_OPEN) {
-//            setupEditTextFocusListener(et_telegram) { viewModel.checkTelegram(it) }
-//            View.VISIBLE
-//        } else View.GONE
         binding.etTelegram.visibility = if (sConfigData?.enableTelegram == FLAG_OPEN) View.VISIBLE else View.GONE
 
     }
@@ -299,14 +188,9 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
         if (sConfigData?.enableRegValidCode == FLAG_OPEN) {
             binding.blockValidCode.visibility = View.VISIBLE
             updateValidCode()
-            //setupEditTextFocusListener(et_verification_code) { viewModel.checkValidCode(it) }
         } else {
             binding.blockValidCode.visibility = View.GONE
         }
-
-//        binding.etVerificationCode.setVerificationCodeBtnOnClickListener {
-//            updateValidCode()
-//        }
     }
 
     private fun setupSmsValidCode() {
@@ -316,7 +200,6 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
             //手機驗證碼開啟，必定需要手機號欄位輸入
             binding.etPhone.visibility = View.VISIBLE
             binding.blockSmsValidCode.visibility = View.VISIBLE
-            //setupEditTextFocusListener(et_sms_valid_code) { viewModel.checkSecurityCode(it) }
         } else {
             binding.blockSmsValidCode.visibility = View.GONE
         }
@@ -327,23 +210,9 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
     }
 
     private fun setupAgreement() {
-//        when (LanguageManager.getSelectLanguage(this@RegisterActivity)) {
-//            LanguageManager.Language.ZH -> binding.llAgreement.orientation = LinearLayout.HORIZONTAL
-//            else -> binding.llAgreement.orientation = LinearLayout.VERTICAL
-//        }
-//
         binding.cbAgreement.setOnClickListener {
-            binding.cbAgreement.setTextColor(ContextCompat.getColor(this, R.color.colorGray))
-            binding.cbAgreement.buttonTintList = null
             viewModel.checkAgreement(binding.cbAgreement.isChecked)
         }
-    }
-
-    private fun setupRegisterAgreementButton() {
-//        binding.btnAgreement.setOnClickListener {
-//            AgreementDialog().show(supportFragmentManager, null)
-//        }
-
     }
 
     private fun setupRegisterButton() {
@@ -366,8 +235,6 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
             val validCode = binding.eetVerificationCode.text.toString()
             val agreementChecked = binding.cbAgreement.isChecked
             val deviceSn = JPushInterface.getRegistrationID(applicationContext)
-//            val deviceSn = getSharedPreferences(UUID_DEVICE_CODE, Context.MODE_PRIVATE).getString(UUID, "") ?: ""
-//            Timber.d("UUID = $deviceSn")
 
             viewModel.registerSubmit(
                 inviteCode,
@@ -423,56 +290,77 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
 
     private fun initObserve() {
 
-        viewModel.registerResult.observe(this, {
+        viewModel.registerResult.observe(this) {
             updateUiWithResult(it)
-        })
+        }
 
-        viewModel.validCodeResult.observe(this, {
+        viewModel.validCodeResult.observe(this) {
             updateUiWithResult(it)
-        })
+        }
 
-        viewModel.smsResult.observe(this, {
+        viewModel.smsResult.observe(this) {
             updateUiWithResult(it)
-        })
+        }
 
-        viewModel.loginForGuestResult.observe(this, {
+        viewModel.loginForGuestResult.observe(this) {
             updateUiWithResult(it)
-        })
-
-        viewModel.agreementChecked.observe(this, {
-            if (it?.not() == true) {
-                binding.cbAgreement.setTextColor(ContextCompat.getColor(this, R.color.colorRedDark))
-                binding.cbAgreement.buttonTintList =
-                    ContextCompat.getColorStateList(this, R.color.colorRedDark)
-            } else {
-                binding.cbAgreement.setTextColor(ContextCompat.getColor(this, R.color.colorGray))
-                binding.cbAgreement.buttonTintList = null
-            }
-        })
+        }
 
         /**
          * 輸入欄位判斷後錯誤提示
          */
         viewModel.apply {
-            inviteCodeMsg.observe(this@RegisterActivity, { binding.etRecommendCode.setError(it,false) })
-            memberAccountMsg.observe(this@RegisterActivity, { binding.etMemberAccount.setError(it,false) })
-            loginPasswordMsg.observe(this@RegisterActivity, { binding.etLoginPassword.setError(it,false) })
-            confirmPasswordMsg.observe(this@RegisterActivity, { binding.etConfirmPassword.setError(it,false) })
-            fullNameMsg.observe(this@RegisterActivity, { binding.etFullName.setError(it,false) })
-            fundPwdMsg.observe(this@RegisterActivity, { binding.etWithdrawalPwd.setError(it,false) })
-            qqMsg.observe(this@RegisterActivity, { binding.etQq.setError(it,false) })
-            phoneMsg.observe(this@RegisterActivity, { binding.etPhone.setError(it,false) })
-            emailMsg.observe(this@RegisterActivity, { binding.etMail.setError(it,false) })
-            weChatMsg.observe(this@RegisterActivity, { binding.etWeChat.setError(it,false) })
-            zaloMsg.observe(this@RegisterActivity, {binding.etZalo.setError(it,false) })
-            facebookMsg.observe(this@RegisterActivity, { binding.etFacebook.setError(it,false) })
-            whatsAppMsg.observe(this@RegisterActivity, { binding.etWhatsApp.setError(it,false) })
-            telegramMsg.observe(this@RegisterActivity, { binding.etTelegram.setError(it,false) })
-            securityCodeMsg.observe(this@RegisterActivity, { binding.etSmsValidCode.setError(it,false) })
-            validCodeMsg.observe(this@RegisterActivity, { binding.etSmsValidCode.setError(it,false) })
-//            registerEnable.observe(
-//                this@RegisterActivity,
-//                { it?.let { binding.btnRegister.isEnabled = it } })
+            inviteCodeMsg.observe(this@RegisterActivity) {
+                binding.etRecommendCode.setError(
+                    it,
+                    false
+                )
+            }
+            memberAccountMsg.observe(this@RegisterActivity) {
+                binding.etMemberAccount.setError(
+                    it,
+                    false
+                )
+            }
+            loginPasswordMsg.observe(this@RegisterActivity) {
+                binding.etLoginPassword.setError(
+                    it,
+                    false
+                )
+            }
+            confirmPasswordMsg.observe(this@RegisterActivity) {
+                binding.etConfirmPassword.setError(
+                    it,
+                    false
+                )
+            }
+            fullNameMsg.observe(this@RegisterActivity) { binding.etFullName.setError(it, false) }
+            fundPwdMsg.observe(this@RegisterActivity) {
+                binding.etWithdrawalPwd.setError(
+                    it,
+                    false
+                )
+            }
+            qqMsg.observe(this@RegisterActivity) { binding.etQq.setError(it, false) }
+            phoneMsg.observe(this@RegisterActivity) { binding.etPhone.setError(it, false) }
+            emailMsg.observe(this@RegisterActivity) { binding.etMail.setError(it, false) }
+            weChatMsg.observe(this@RegisterActivity) { binding.etWeChat.setError(it, false) }
+            zaloMsg.observe(this@RegisterActivity) { binding.etZalo.setError(it, false) }
+            facebookMsg.observe(this@RegisterActivity) { binding.etFacebook.setError(it, false) }
+            whatsAppMsg.observe(this@RegisterActivity) { binding.etWhatsApp.setError(it, false) }
+            telegramMsg.observe(this@RegisterActivity) { binding.etTelegram.setError(it, false) }
+            securityCodeMsg.observe(this@RegisterActivity) {
+                binding.etSmsValidCode.setError(
+                    it,
+                    false
+                )
+            }
+            validCodeMsg.observe(this@RegisterActivity) {
+                binding.etSmsValidCode.setError(
+                    it,
+                    false
+                )
+            }
         }
 
     }
