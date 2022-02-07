20da5a6fea91ea0067339cac8b3421d598c8f8fa
Bill <h853047455@gmail.com>
2022-02-07 17:39:50 +0800

    [Add] 判斷是否顯示手機驗證參數

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterViewModel.kt
@@ -56,6 +56,8 @@ class ProfileCenterViewModel(
         withdrawRepository.needToCompleteProfileInfo //提款頁面是否需要完善個人資料 true: 需要, false: 不需要
     val needToBindBankCard =
         withdrawRepository.needToBindBankCard //提款頁面是否需要新增銀行卡 -1 : 不需要新增, else : 以value作為string id 顯示彈窗提示
+    val needToSendTwoFactor =
+        withdrawRepository.needToSendTwoFactor //判斷是不是要進行手機驗證 (登入後只需一次) true: 需要, false: 不需要
 
     val editIconUrlResult: LiveData<Event<IconUrlResult?>> = avatarRepository.editIconUrlResult
 
