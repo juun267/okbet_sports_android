726f7de026df7fbbb5b4cd75664146e6f8fad230
Bill <h853047455@gmail.com>
2022-02-08 15:09:08 +0800

    [Add] 簡訊認證狀態參數

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterViewModel.kt
@@ -77,9 +77,20 @@ class ProfileCenterViewModel(
         get() = _uploadVerifyPhotoResult
     private val _uploadVerifyPhotoResult = MutableLiveData<Event<UploadVerifyPhotoResult?>>()
 
-    val smsResult: LiveData<SmsResult?>
-        get() = _smsResult
-    private val _smsResult = MutableLiveData<SmsResult?>()
+    //發送簡訊碼之後60s無法再發送
+    val twoFactorResult: LiveData<BaseSecurityCodeResult?>
+        get() = _twoFactorResult
+    private val _twoFactorResult = MutableLiveData<BaseSecurityCodeResult?>()
+
+    //錯誤提示
+    val errorMessageDialog: LiveData<String?>
+        get() = _errorMessageDialog
+    private val _errorMessageDialog = MutableLiveData<String?>()
+
+    //認證成功
+    val twoFactorSuccess: LiveData<Boolean?>
+        get() = _twoFactorSuccess
+    private val _twoFactorSuccess = MutableLiveData<Boolean?>()
 
 
     fun getUserInfo() {
