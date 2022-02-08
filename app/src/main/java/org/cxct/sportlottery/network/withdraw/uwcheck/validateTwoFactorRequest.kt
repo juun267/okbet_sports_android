62a95cfe2cef24f7b139f26f537fa0e618074d4c
Bill <h853047455@gmail.com>
2022-02-08 15:02:03 +0800

    [Refactor] securityCode 型別修改

--- app/src/main/java/org/cxct/sportlottery/network/withdraw/uwcheck/validateTwoFactorRequest.kt
+++ app/src/main/java/org/cxct/sportlottery/network/withdraw/uwcheck/validateTwoFactorRequest.kt
@@ -4,5 +4,5 @@ import org.cxct.sportlottery.network.common.PagingParams
 import org.cxct.sportlottery.network.common.TimeRangeParams
 
 data class ValidateTwoFactorRequest(
-    val securityCode: Int? = null
+    val securityCode: String? = null
 )
