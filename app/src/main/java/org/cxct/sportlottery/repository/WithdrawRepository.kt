ffb39531c28f6726982fb12b77dd8cf340fe72b0
Bill <h853047455@gmail.com>
2022-02-14 16:54:42 +0800

    [Add] 新增註解

--- app/src/main/java/org/cxct/sportlottery/repository/WithdrawRepository.kt
+++ app/src/main/java/org/cxct/sportlottery/repository/WithdrawRepository.kt
@@ -174,7 +174,7 @@ class WithdrawRepository(
 
     //確認使用者有無手機碼 true：有手機碼 false：無手機碼
     private suspend fun checkUserPhoneNumber(): Boolean {
-        if(!getTwoFactorStatus())
+        if(!getTwoFactorStatus())//後台有開簡訊驗證的狀況下才需要顯示Dialog
             _hasPhoneNumber.value = Event(userInfoRepository.userInfo?.firstOrNull()?.phone.toString().isNotEmpty())
         
         return userInfoRepository.userInfo?.firstOrNull()?.phone.toString().isNotEmpty()
