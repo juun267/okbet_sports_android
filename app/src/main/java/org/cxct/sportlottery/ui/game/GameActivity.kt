4caad5f66c9d91cb639d46662321efe65dbc2a14
Bill <h853047455@gmail.com>
2022-02-18 14:34:43 +0800

    [Fix] 底部導覽切換語言，清除注單

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameActivity.kt
@@ -182,7 +182,9 @@ class GameActivity : BaseBottomNavActivity<GameViewModel>(GameViewModel::class) 
         }
 
         iv_language.setOnClickListener {
-            ChangeLanguageDialog().show(supportFragmentManager, null)
+            ChangeLanguageDialog(ChangeLanguageDialog.ClearBetListListener{
+                viewModel.betInfoRepository.clear()
+            }).show(supportFragmentManager, null)
         }
     }
 
