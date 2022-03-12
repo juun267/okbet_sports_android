d6523ff35f51c6cca1c5118555290686f3f9f272
Bill <h853047455@gmail.com>
2022-03-12 19:26:12 +0800

    [Fix] 賠率排序

--- app/src/main/java/org/cxct/sportlottery/ui/game/GameViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GameViewModel.kt
@@ -876,7 +876,7 @@ class GameViewModel(
                 //mapping 下注單裡面項目 & 賠率按鈕 選擇狀態
                 result.rows?.forEach { row ->
                     row.leagueOdds?.matchOdds?.forEach { oddData ->
-                        //oddData.sortOddsMap() //按照188排序 不使用markSort by Bill
+                        oddData.sortOddsMap() //按照188排序 不使用markSort by Bill
                         oddData.oddsMap?.forEach { map ->
                             map.value?.forEach { odd ->
                                 odd?.isSelected =
@@ -910,6 +910,7 @@ class GameViewModel(
             }?.let { result ->
                 //mapping 下注單裡面項目 & 賠率按鈕 選擇狀態
                 result.t?.odds?.forEach { oddData ->
+                    oddData.sortOddsMap()
                     oddData.oddsMap?.forEach { map ->
                         map.value?.forEach { odd ->
                             odd?.isSelected =
