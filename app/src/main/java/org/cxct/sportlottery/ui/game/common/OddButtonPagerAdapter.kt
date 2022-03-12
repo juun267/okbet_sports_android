5789a27a195a4d7bbe188c64dff45fc3e07df1f8
Bill <h853047455@gmail.com>
2022-03-12 09:33:32 +0800

    [Refactor] 調整

--- app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPagerAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPagerAdapter.kt
@@ -739,29 +739,12 @@ class OddButtonPagerViewHolder private constructor(
         }
     }
 
-    fun getOrdinalNumbers(number:String):String{
+    private fun getOrdinalNumbers(number:String):String{
         return when(number){
             "1" -> "1st"
             "2" -> "2nd"
             "3" -> "3rd"
-            "4" -> "4th"
-            "5" -> "5th"
-            "6" -> "6th"
-            "7" -> "7th"
-            "8" -> "8th"
-            "9" -> "9th"
-            "10" -> "10th"
-            "11" -> "11th"
-            "12" -> "12th"
-            "13" -> "13th"
-            "14" -> "14th"
-            "15" -> "15th"
-            "16" -> "16th"
-            "17" -> "17th"
-            "18" -> "18th"
-            "19" -> "19th"
-            "20" -> "20th"
-            else -> ""
+            else -> "${number}th"
         }
     }
 
