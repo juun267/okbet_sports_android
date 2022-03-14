0b58e17526f084554c60a8faa5fabae4bdc35497
Bill <h853047455@gmail.com>
2022-03-14 22:27:35 +0800

    [Refactor] UI更新調整

--- app/src/main/java/org/cxct/sportlottery/ui/game/hall/GameV3Fragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/hall/GameV3Fragment.kt
@@ -1319,7 +1319,9 @@ class GameV3Fragment : BaseBottomNavigationFragment<GameViewModel>(GameViewModel
                             when{
                                 !hasLeagueIdList -> {
                                     //全刷
-                                    viewModel.getGameHallList(args.matchType,false)
+                                    withContext(Dispatchers.Main) {
+                                        viewModel.getGameHallList(args.matchType,false)
+                                    }
                                 }
                                 else -> {
                                     unSubscribeChannelHall(nowGameType ?: GameType.FT.key, getPlaySelectedCode(), leagueChangeEvent.matchIdList?.firstOrNull())
