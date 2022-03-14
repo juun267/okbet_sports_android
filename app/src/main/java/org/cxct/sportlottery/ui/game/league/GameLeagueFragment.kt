aae927821738c52c02fff399fe6d1effeebca6cd
Bill <h853047455@gmail.com>
2022-03-14 15:39:10 +0800

    [Fix] 修正Socket 訂閱錯誤問題

--- app/src/main/java/org/cxct/sportlottery/ui/game/league/GameLeagueFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/league/GameLeagueFragment.kt
@@ -643,6 +643,10 @@ class GameLeagueFragment : BaseBottomNavigationFragment<GameViewModel>(GameViewM
         }
     }
 
+    private fun getPlaySelectedCode(): String? {
+        return playCategoryAdapter.data.find { it.isSelected }?.code
+    }
+
     private fun getPlayCateMenuCode(): String? {
         val playSelected = playCategoryAdapter.data.find { it.isSelected }
 
@@ -663,7 +667,7 @@ class GameLeagueFragment : BaseBottomNavigationFragment<GameViewModel>(GameViewM
                 true -> {
                     subscribeChannelHall(
                         leagueOdd.gameType?.key,
-                        getPlayCateMenuCode(),
+                        getPlaySelectedCode(),
                         matchOdd.matchInfo?.id
                     )
 
