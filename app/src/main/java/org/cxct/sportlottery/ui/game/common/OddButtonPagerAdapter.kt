b46eb90ad00f91436a7918315ca430433428e251
KevinLin <kevin_android@cxgi.org>
2022-03-18 15:40:16 +0800

    [Fix]odd button visibility

--- app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPagerAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/common/OddButtonPagerAdapter.kt
@@ -495,6 +495,11 @@ class OddButtonPagerViewHolder private constructor(
             oddBtnAway.visibility = View.INVISIBLE
             oddBtnDraw.visibility = View.INVISIBLE
             return
+        } else {
+            oddBtnType.visibility = View.VISIBLE
+            oddBtnHome.visibility = View.VISIBLE
+            oddBtnAway.visibility = View.VISIBLE
+            oddBtnDraw.visibility = View.VISIBLE
         }
 
         if (matchInfo.status == null || matchInfo.status == 2 || odds.first.toString()
@@ -810,6 +815,11 @@ class OddButtonPagerViewHolder private constructor(
             oddBtnAway.visibility = View.INVISIBLE
             oddBtnDraw.visibility = View.INVISIBLE
             return
+        } else {
+            oddBtnType.visibility = View.VISIBLE
+            oddBtnHome.visibility = View.VISIBLE
+            oddBtnAway.visibility = View.VISIBLE
+            oddBtnDraw.visibility = View.VISIBLE
         }
         if (matchInfo.status == null || matchInfo.status == 2 || odds.first.toString()
                 .contains("EmptyData")
