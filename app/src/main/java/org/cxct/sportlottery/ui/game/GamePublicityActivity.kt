c9193cc74836770582cc7493601907c733b852d3
Dean <>
2022-03-18 17:59:50 +0800

    [新增] 進入離開宣傳頁邏輯

--- app/src/main/java/org/cxct/sportlottery/ui/game/GamePublicityActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/GamePublicityActivity.kt
@@ -1,12 +1,54 @@
 package org.cxct.sportlottery.ui.game
 
+import android.content.Intent
 import android.os.Bundle
-import org.cxct.sportlottery.R
+import android.view.View
+import org.cxct.sportlottery.databinding.ActivityGamePublicityBinding
 import org.cxct.sportlottery.ui.base.BaseSocketActivity
+import org.cxct.sportlottery.ui.login.signIn.LoginActivity
+import org.cxct.sportlottery.ui.login.signUp.RegisterActivity
 
-class GamePublicityActivity : BaseSocketActivity<GameViewModel>(GameViewModel::class) {
+class GamePublicityActivity : BaseSocketActivity<GameViewModel>(GameViewModel::class), View.OnClickListener {
+    private lateinit var binding: ActivityGamePublicityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_publicity)
+        binding = ActivityGamePublicityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        initObservers()
+    }
+
+    private fun initViews() {
+        initOnClickListener()
+    }
+
+    private fun initOnClickListener() {
+        binding.tvRegister.setOnClickListener(this)
+        binding.tvLogin.setOnClickListener(this)
+    }
+
+    private fun initObservers() {
+        viewModel.isLogin.observe(this) {
+            if (it) {
+                startActivity(Intent(this, GameActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        avoidFastDoubleClick()
+        with(binding) {
+            when (v) {
+                tvRegister -> {
+                    startActivity(Intent(this@GamePublicityActivity, RegisterActivity::class.java))
+                }
+                tvLogin -> {
+                    startActivity(Intent(this@GamePublicityActivity, LoginActivity::class.java))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/org/cxct/sportlottery/ui/splash/SplashActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/splash/SplashActivity.kt
@@ -13,6 +13,7 @@ import org.cxct.sportlottery.repository.FLAG_OPEN
 import org.cxct.sportlottery.ui.base.BaseActivity
 import org.cxct.sportlottery.ui.common.CustomAlertDialog
 import org.cxct.sportlottery.ui.game.GameActivity
+import org.cxct.sportlottery.ui.game.GamePublicityActivity
 import org.cxct.sportlottery.ui.main.MainActivity
 import org.cxct.sportlottery.ui.maintenance.MaintenanceActivity
 import org.cxct.sportlottery.ui.permission.GooglePermissionActivity
@@ -67,6 +68,11 @@ class SplashActivity : BaseActivity<SplashViewModel>(SplashViewModel::class) {
         finish()
     }
 
+    private fun goGamePublicityPage() {
+        startActivity(Intent(this@SplashActivity, GamePublicityActivity::class.java))
+        finish()
+    }
+
     //過程中任一流程請求失敗，點擊確定按鈕重試
     private fun showErrorRetryDialog(title: String, message: String) {
         val dialog = CustomAlertDialog(this)
@@ -113,6 +119,17 @@ class SplashActivity : BaseActivity<SplashViewModel>(SplashViewModel::class) {
                 }
                 false -> {
                     goHomePage()
+                }
+            }
+        }
+
+        viewModel.isLogin.observe(this) {
+            when (it) {
+                true -> {
+                    goGamePage()
+                }
+                false -> {
+                    goGamePublicityPage()
                 }
             }
         }
