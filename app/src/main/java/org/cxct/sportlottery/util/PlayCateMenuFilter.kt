ac2de837b6c34399eceaa7efdd981e4192036bb9
Bill <h853047455@gmail.com>
2022-03-23 11:45:29 +0800

    [Add] 排序、翻譯篩選

--- app/src/main/java/org/cxct/sportlottery/util/PlayCateMenuFilter.kt
+++ app/src/main/java/org/cxct/sportlottery/util/PlayCateMenuFilter.kt
@@ -1,4 +1,20 @@
 package org.cxct.sportlottery.util
 
+import org.cxct.sportlottery.network.sport.SportMenuFilter
+
 object PlayCateMenuFilter {
+    var filterList : MutableMap<String?, MutableMap<String?, SportMenuFilter>?>? = mutableMapOf()
+
+    fun filterOddsSort(sportType: String?, playCateMenuCode: String?): String? {
+       return filterList?.get(sportType)?.get(playCateMenuCode)?.oddsSort
+    }
+
+    fun filterPlayCateNameMap(sportType: String?, playCateMenuCode: String?): MutableMap<String?, Map<String?, String?>?>? {
+        return filterList?.get(sportType)?.get(playCateMenuCode)?.playCateNameMap
+    }
+
+    fun filterSelectablePlayCateNameMap(sportType: String?, playSelectedCode: String?, playCateMenuCode: String?): MutableMap<String?, Map<String?, String?>?>? {
+        return filterList?.get(sportType)?.get(playSelectedCode)?.playCateMap?.get(playCateMenuCode)?.playCateNameMap
+    }
+
 }
