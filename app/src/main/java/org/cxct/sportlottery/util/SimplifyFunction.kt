a7c9e2174d020c5884979483844d55cba1aeeb09
kevin9594 <k57764328@gmail.com>
2022-04-09 22:09:37 +0800

    [Refactor] subscribe logic

--- app/src/main/java/org/cxct/sportlottery/util/SimplifyFunction.kt
+++ app/src/main/java/org/cxct/sportlottery/util/SimplifyFunction.kt
@@ -3,48 +3,97 @@ package org.cxct.sportlottery.util
 import android.graphics.Rect
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.itemview_league_v5.view.*
+import org.cxct.sportlottery.ui.game.common.LeagueAdapter
 
 /**
  * @author kevin
  * @create 2022/3/31
  * @description
  */
-fun RecyclerView.addScrollWithItemVisibility(onVisible: (visibleList: List<Int>) -> Unit) {
-
+fun RecyclerView.addScrollWithItemVisibility(onScrolling: () -> Unit, onVisible: (visibleList: List<Pair<Int, Int>>) -> Unit) {
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
-
-        private fun emitVisibleItems() {
-            val manager = layoutManager
-            if (manager is LinearLayoutManager) {
-                val firstPosition = manager.findFirstVisibleItemPosition()
-                val lastPosition = manager.findLastVisibleItemPosition()
-                val visibleRange = mutableListOf<Int>()
-                for (i in firstPosition..lastPosition) {
-                    val view = manager.findViewByPosition(i) ?: continue
-                    val rect = Rect()
-                    val isVisible = view.getGlobalVisibleRect(rect)
-                    if (isVisible) {
-                        visibleRange.add(i)
-                    }
-                }
-                onVisible(visibleRange)
-            }
-        }
-
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
-            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                emitVisibleItems()
+            when(newState){
+                //停止
+                RecyclerView.SCROLL_STATE_IDLE -> {
+
+                    val visibleRangePair = mutableListOf<Pair<Int, Int>>()
+
+                    getVisibleRangePosition().forEach { leaguePosition ->
+                        val viewByPosition = layoutManager?.findViewByPosition(leaguePosition)
+                        viewByPosition?.let {
+                            if (getChildViewHolder(it) is LeagueAdapter.ItemViewHolder) {
+                                val viewHolder = getChildViewHolder(it) as LeagueAdapter.ItemViewHolder
+                                viewHolder.itemView.league_odd_list.getVisibleRangePosition().forEach { matchPosition ->
+                                    visibleRangePair.add(Pair(leaguePosition, matchPosition))
+                                }
+                            }
+                        }
+                    }
+
+                    onVisible(visibleRangePair)
+
+
+//                    val manager = layoutManager
+//                    if (manager is LinearLayoutManager) {
+//                        val firstPosition = manager.findFirstVisibleItemPosition()
+//                        val lastPosition = manager.findLastVisibleItemPosition()
+//                        val visibleRangePair = mutableListOf<Pair<Int, Int>>()
+//                        for (leaguePosition in firstPosition..lastPosition) {
+//                            val view = manager.findViewByPosition(leaguePosition) ?: continue
+//                            val rect = Rect()
+//                            val isVisible = view.getGlobalVisibleRect(rect)
+//                            if (isVisible) {
+//                                val viewByPosition = manager.findViewByPosition(leaguePosition)
+//                                viewByPosition?.let {
+//                                    if (getChildViewHolder(it) is LeagueAdapter.ItemViewHolder) {
+//                                        val viewHolder = getChildViewHolder(it) as LeagueAdapter.ItemViewHolder
+//                                        val m = viewHolder.itemView.league_odd_list.layoutManager
+//                                        if (m is LinearLayoutManager) {
+//                                            val first: Int = m.findFirstVisibleItemPosition()
+//                                            val last: Int = m.findLastVisibleItemPosition()
+//                                            for (matchPosition in first..last) {
+//                                                val mView = m.findViewByPosition(matchPosition) ?: continue
+//                                                val mRect = Rect()
+//                                                val mIsVisible = mView.getGlobalVisibleRect(mRect)
+//                                                if (mIsVisible) {
+//                                                    visibleRangePair.add(Pair(leaguePosition, matchPosition))
+//                                                }
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                        onVisible(visibleRangePair)
+//                    }
+                }
+
+                //手指滾動
+                RecyclerView.SCROLL_STATE_DRAGGING -> {
+                    onScrolling()
+                }
             }
         }
-
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            super.onScrolled(recyclerView, dx, dy)
-            if (dx == 0 && dy == 0) {
-                emitVisibleItems()
-            }
-        }
-
     })
+}
 
+fun RecyclerView.getVisibleRangePosition(): List<Int>{
+    return mutableListOf<Int>().apply {
+        val manager = layoutManager
+        if (manager is LinearLayoutManager) {
+            val first: Int = manager.findFirstVisibleItemPosition()
+            val last: Int = manager.findLastVisibleItemPosition()
+            for (i in first..last) {
+                val v = manager.findViewByPosition(i) ?: continue
+                val r = Rect()
+                val isVisible = v.getGlobalVisibleRect(r)
+                if (isVisible) {
+                    this.add(i)
+                }
+            }
+        }
+    }
 }
