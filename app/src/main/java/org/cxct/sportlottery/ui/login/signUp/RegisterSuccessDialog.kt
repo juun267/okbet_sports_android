625bdca4798a80222b63f22a090bedc646946ea6
Martin <>
2022-04-19 15:26:58 +0800

    完成註冊Dialog

---
+++ app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterSuccessDialog.kt
@@ -0,0 +1,116 @@
+package org.cxct.sportlottery.ui.login.signUp
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Spanned
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.dialog_custom_alert.*
+import kotlinx.android.synthetic.main.dialog_register_success.*
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.ui.game.publicity.GamePublicityActivity
+import org.cxct.sportlottery.ui.money.recharge.MoneyRechargeActivity
+
+class RegisterSuccessDialog(context: Context) : DialogFragment() {
+
+
+    private var mContext: Context = context
+    private var mNegativeClickListener: View.OnClickListener = View.OnClickListener { dismiss() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return inflater.inflate(R.layout.dialog_register_success, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+    val timer= object: CountDownTimer(5000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            var diff = millisUntilFinished
+            val secondsInMilli: Long = 1000
+            val elapsedSeconds = diff / secondsInMilli
+
+            tvCount?.let {
+                it.text = "($elapsedSeconds)"
+            }
+        }
+
+        override fun onFinish() {
+            dismiss()
+            startActivity(Intent(mContext, GamePublicityActivity::class.java))
+        }
+    }
+    private fun initView() {
+
+        timer.start()
+        tvVisitFirst.setOnClickListener {
+            dismiss()
+            startActivity(Intent(mContext, GamePublicityActivity::class.java))
+        }
+        tvJump.text = getString(R.string.register_jump,getString(R.string.app_name))
+        btnRecharge.setOnClickListener (mNegativeClickListener)
+//            mNegativeClickListener
+//            dismiss()
+//            startActivity(Intent(mContext, MoneyRechargeActivity::class.java))
+//        }
+//        when (mTitle) {
+//            null -> tv_title.visibility = View.GONE
+//            else -> tv_title.text = mTitle
+//        }
+//
+//        tv_message.gravity = mGravity
+//        when {
+//            mSpannedMessage != null -> tv_message.text = mSpannedMessage
+//            mMessage == null -> sv_block_content.visibility = View.GONE
+//            else -> tv_message.text = mMessage
+//        }
+//
+//        if (mPositiveText == null) {
+//            btn_positive.visibility = View.GONE
+//        } else
+//            btn_positive.text = mPositiveText
+//
+//        if (mNegativeText == null) {
+//            btn_negative.visibility = View.GONE
+//        } else
+//            btn_negative.text = mNegativeText
+//
+//        if(mPositiveText == null || mNegativeText == null){
+//            view_line.visibility = View.GONE
+//        }
+//
+//        if(mPositiveText == null && mNegativeText == null){
+//            block_bottom_bar.visibility = View.GONE
+//        }
+//
+//        tv_message.setTextColor(ContextCompat.getColor(context?:requireContext(), mTextColor))
+//
+//        divider2.visibility = if (isShowDivider) View.VISIBLE else View.GONE
+//        divider.visibility = if (isShowDividerBottom) View.VISIBLE else View.GONE
+//
+//        btn_positive.setOnClickListener(mPositiveClickListener)
+//        btn_negative.setOnClickListener(mNegativeClickListener)
+    }
+    fun setNegativeClickListener(negativeClickListener: View.OnClickListener) {
+        mNegativeClickListener = negativeClickListener
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+    }
+}
