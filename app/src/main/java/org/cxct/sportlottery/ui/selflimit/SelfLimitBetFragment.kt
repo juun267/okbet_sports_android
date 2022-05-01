6034a600c38df734d09749904e0b73f760d135f2
Bee <bee@Bees-MacBook-Pro.local>
2022-05-02 02:12:12 +0800

    修正自我限制閃退的問題

--- app/src/main/java/org/cxct/sportlottery/ui/selflimit/SelfLimitBetFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/selflimit/SelfLimitBetFragment.kt
@@ -105,11 +105,14 @@ class SelfLimitBetFragment : BaseFragment<SelfLimitViewModel>(SelfLimitViewModel
                 TextUtil.formatMoney(viewModel.userInfo.value?.perBetLimit?.toDouble() ?: 0.0)
             ) + " " + sConfigData?.systemCurrency
         }
+
+        val perBetMinAmount = if (sConfigData?.perBetMinAmount.isNullOrEmpty()) 0.0 else sConfigData?.perBetMinAmount?.toDouble() ?: 0.0
+        val perBetMaxAmount = if (sConfigData?.perBetMaxAmount.isNullOrEmpty()) 0.0 else sConfigData?.perBetMaxAmount?.toDouble() ?: 0.0
         binding.tvLimit.text = String.format(
             getString(R.string.self_limit_per_bet_limit_user_limit),
-            TextUtil.formatMoney(sConfigData?.perBetMinAmount?.toDouble() ?: 0.0),
-            TextUtil.formatMoney(sConfigData?.perBetMaxAmount?.toDouble() ?: 0.0),
-            sConfigData?.systemCurrency
+            TextUtil.formatMoney(perBetMinAmount),
+            TextUtil.formatMoney(perBetMaxAmount),
+            sConfigData?.systemCurrency ?: " "
         )
     }
 
