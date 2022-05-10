b123c82eb7a30849488914677ee57762c2b8d3d0
Bee <bee@cx.com>
2022-05-10 11:19:16 +0800

    調整首頁排版和翻譯、資金明細頁面的金額千位符號

--- app/src/main/java/org/cxct/sportlottery/ui/game/home/RecommendViewHolder.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/home/RecommendViewHolder.kt
@@ -26,8 +26,8 @@ import org.cxct.sportlottery.util.TimeUtil
  */
 class RecommendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val mRequestOptions = RequestOptions()
-        .placeholder(R.drawable.ic_image_load)
-        .error(R.drawable.ic_image_broken)
+//        .placeholder(R.drawable.ic_image_load)
+//        .error(R.drawable.ic_image_broken)
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .dontTransform()
 
