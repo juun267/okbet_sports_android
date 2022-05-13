f246e7c7e2831d0afdb86a7ce1002faf2a909cdd
Bill <h853047455@gmail.com>
2022-05-13 15:17:47 +0800

    [Fix] 24081【Android】越南文註冊頁面勾選鈕第二列文字顏色須修改

--- app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/login/signUp/RegisterActivity.kt
@@ -163,10 +163,13 @@ class RegisterActivity : BaseActivity<RegisterViewModel>(RegisterViewModel::clas
         )
         val appName = getString(R.string.app_name)
         //中英appName在前半 越南文appName會在後半
-        binding.tvAgreement.text =
-            String.format(getString(R.string.register_over_21), appName) + String.format(getString(R.string.register_rules), appName)
+        when (LanguageManager.getSelectLanguage(this@RegisterActivity)) {
+            LanguageManager.Language.VI -> binding.tvAgreement.text = String.format(getString(R.string.register_over_21), appName) + getString(R.string.register_rules) + String.format(getString(R.string.register_rules_2nd_half), appName)
+            else -> binding.tvAgreement.text = String.format(getString(R.string.register_over_21), appName) + getString(R.string.register_rules)
+        }
+
         binding.tvAgreement.makeLinks(
-            Pair(String.format(getString(R.string.register_rules), appName), View.OnClickListener {
+            Pair(getString(R.string.register_rules), View.OnClickListener {
                 JumpUtil.toInternalWeb(
                     this,
                     Constants.getAgreementRuleUrl(this),
