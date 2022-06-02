689c67ff5e00a19aaf3781b55e9baefe2127a5bb
“stefan” <stefan@ak.com>
2022-06-02 17:02:45 +0800

    fix：存款记录 加入4状态

--- app/src/main/java/org/cxct/sportlottery/util/BindingUtils.kt
+++ app/src/main/java/org/cxct/sportlottery/util/BindingUtils.kt
@@ -262,7 +262,7 @@ fun TextView.setBetStatusMoney(status: Int?, money: Double?) {
 fun TextView.setRecordStatus(status: Int?) {
     status?.let {
         text = when (it) {
-            1 -> context.getString(R.string.recharge_state_processing)
+            1,4 -> context.getString(R.string.recharge_state_processing)
             2 -> context.getString(R.string.recharge_state_success)
             3 -> context.getString(R.string.recharge_state_failed)
             else -> ""
