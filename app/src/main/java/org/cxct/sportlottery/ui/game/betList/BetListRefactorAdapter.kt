f44cd4bcf925ad3ddd90a5472e81b9ed7d052ca6
LouisWu <louis_android@cxgi.org>
2022-06-08 20:36:25 +0800

    [修正] 25154 [注單]【Android】投注單，反波膽賠率少了@

--- app/src/main/java/org/cxct/sportlottery/ui/game/betList/BetListRefactorAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/betList/BetListRefactorAdapter.kt
@@ -650,9 +650,9 @@ class BetListRefactorAdapter(private val onItemClickListener: OnItemClickListene
                     oldOdds = TextUtil.formatForOdd(getOdds(itemData.matchOdd, currentOddsType))
                 }
                 //反波膽顯示 %
-                var tvOdd = "@ " + TextUtil.formatForOdd(getOdds(itemData.matchOdd, currentOddsType))
+                var tvOdd = "@" + TextUtil.formatForOdd(getOdds(itemData.matchOdd, currentOddsType))
                 if(itemData.matchOdd.playCode == PlayCate.LCS.value)
-                    tvOdd = TextUtil.formatForOddPercentage(getOdds(itemData.matchOdd, currentOddsType)-1)
+                    tvOdd = "@" + TextUtil.formatForOddPercentage(getOdds(itemData.matchOdd, currentOddsType)-1)
 
                 tvOdds.text = if (itemData.matchOdd.status == BetStatus.ACTIVATED.code) tvOdd else "–"
 
