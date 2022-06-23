28e181a0b97968f5ed20e0bbf9e9f68d996133fc
Bill <h853047455@gmail.com>
2022-06-23 14:58:34 +0800

    [Fix] 25790 [建議]【Android】 我的賽事，未加入賽事時，也要顯示 footer 以達四端統一

--- app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/favorite/MyFavoriteFragment.kt
@@ -28,6 +28,7 @@ import org.cxct.sportlottery.network.odds.list.QuickPlayCate
 import org.cxct.sportlottery.network.service.odds_change.OddsChangeEvent
 import org.cxct.sportlottery.network.sport.Item
 import org.cxct.sportlottery.network.sport.query.Play
+import org.cxct.sportlottery.ui.base.BaseBottomNavigationFragment
 import org.cxct.sportlottery.ui.base.BaseSocketFragment
 import org.cxct.sportlottery.ui.base.ChannelType
 import org.cxct.sportlottery.ui.common.EdgeBounceEffectHorizontalFactory
@@ -47,7 +48,7 @@ import org.cxct.sportlottery.util.*
  * @app_destination 我的賽事
  */
 @SuppressLint("LogNotTimber")
-class MyFavoriteFragment : BaseSocketFragment<MyFavoriteViewModel>(MyFavoriteViewModel::class) {
+class MyFavoriteFragment : BaseBottomNavigationFragment<MyFavoriteViewModel>(MyFavoriteViewModel::class) {
 
     private var isReloadPlayCate: Boolean? = null //是否重新加載玩法篩選Layout
 
@@ -265,6 +266,7 @@ class MyFavoriteFragment : BaseSocketFragment<MyFavoriteViewModel>(MyFavoriteVie
         super.onViewCreated(view, savedInstanceState)
         initObserver()
         initSocketObserver()
+        initBottomNavigation()
     }
 
     private fun initSocketObserver() {
