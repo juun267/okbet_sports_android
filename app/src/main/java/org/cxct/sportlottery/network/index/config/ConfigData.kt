22fa010593e117b86335feda941091d8b10321cf
Dean <>
2022-06-25 13:03:22 +0800

    [新增] config 參數

--- app/src/main/java/org/cxct/sportlottery/network/index/config/ConfigData.kt
+++ app/src/main/java/org/cxct/sportlottery/network/index/config/ConfigData.kt
@@ -100,7 +100,12 @@ data class ConfigData(
     val supportLanguage: String = "",
     val wsHost: String = "",
     val creditSystem: Int? = null,
-    val firstRechLessAmountLimit: String?
+    val firstRechLessAmountLimit: String?,
+    val enableBirthday: String?, //是否开启Birthday(1：开启，0：关闭)
+    val enableSalarySource: String?, //是否开启SalarySource(1：开启，0：关闭)
+    val enableIdentityNumber: String?, //是否开启IdentityNumber(1：开启，0：关闭)
+    val enableBettingStation: String?, //是否开启BettingStation(1：开启，0：关闭)
+    var salarySource: List<SalarySource>?
 ) {
     var enterCertified: Int? = -1 //ProfileCenterViewModel.SecurityEnter
     var hasGetTwoFactorResult: Boolean? = false //判斷是不是已經成功發送過簡訊認證碼 (關掉彈窗要重新設置為false)
