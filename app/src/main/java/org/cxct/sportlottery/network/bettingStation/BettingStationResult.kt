906d3183891ef720fad1d8ff7cd6d94fe18ff937
Dean <>
2022-06-25 13:41:09 +0800

    [新增] 查詢投注站api

---
+++ app/src/main/java/org/cxct/sportlottery/network/bettingStation/BettingStationResult.kt
@@ -0,0 +1,20 @@
+package org.cxct.sportlottery.network.bettingStation
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import org.cxct.sportlottery.network.common.BaseResult
+
+@JsonClass(generateAdapter = true)
+data class BettingStationResult(
+    @Json(name = "code")
+    override val code: Int,
+    @Json(name = "msg")
+    override val msg: String,
+    @Json(name = "rows")
+    val bettingStationList: List<BettingStation>,
+    @Json(name = "success")
+    override val success: Boolean,
+    @Json(name = "total")
+    val total: Int
+) : BaseResult()
