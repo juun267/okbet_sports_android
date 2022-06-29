b7dd8e2f3adc852245586b698105afb85fd36c70
KevinLin <kevin_android@cxgi.org>
2022-06-29 18:30:58 +0800

    [Fix] 今日-日期選擇雙位數

--- app/src/main/java/org/cxct/sportlottery/util/TimeUtil.kt
+++ app/src/main/java/org/cxct/sportlottery/util/TimeUtil.kt
@@ -25,7 +25,7 @@ object TimeUtil {
     const val MD_HMS_FORMAT = "MM-dd HH:mm:ss"
     const val MD_HM_FORMAT = "MM/dd HH:mm"
     const val DM_HM_FORMAT = "MM/dd HH:mm"
-    private const val YMDE_FORMAT = "yyyy-MMMM-d-EEE"
+    private const val YMDE_FORMAT = "yyyy-MMMM-dd-EEE"
     private const val YMDE_HMS_FORMAT = "yyyy-MMMM-d-EEE HH:mm:ss"
     private const val DMY_HM_FORMAT = "yyyy-MM-dd HH:mm"
     private const val BIRTHDAY_FORMAT = "yyyy / MM / dd"
