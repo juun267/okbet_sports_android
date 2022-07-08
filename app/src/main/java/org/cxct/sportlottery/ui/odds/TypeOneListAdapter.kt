3fe7bbc9c124d6e899a4a46ef7d92e93fd318108
KevinLin <kevin_android@cxgi.org>
2022-07-08 15:15:03 +0800

    [Fix] 26381 【Android】赛事内页，winning margin 玩法选项大于5个时，只需展示5个，剩余的隐藏，不能全部展示

--- app/src/main/java/org/cxct/sportlottery/ui/odds/TypeOneListAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/odds/TypeOneListAdapter.kt
@@ -1,9 +1,11 @@
 package org.cxct.sportlottery.ui.odds
 
-
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.odds.Odd
@@ -11,23 +13,22 @@ import org.cxct.sportlottery.ui.game.common.OddStateViewHolder
 import org.cxct.sportlottery.ui.game.widget.OddsButton
 import org.cxct.sportlottery.ui.menu.OddsType
 
-
+@SuppressLint("NotifyDataSetChanged")
 class TypeOneListAdapter(
     private var oddsDetail: OddsDetailListData,
     private val onOddClickListener: OnOddClickListener,
     private val oddsType: OddsType,
-    private val isOddPercentage:Boolean? = false
-) : RecyclerView.Adapter<TypeOneListAdapter.ViewHolder>() {
-
+    private val isOddPercentage: Boolean? = false,
+    private val onMoreClickListener: OnMoreClickListener? = null
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val mOddStateRefreshListener by lazy {
         object : OddStateViewHolder.OddStateChangeListener {
             override fun refreshOddButton(odd: Odd) {
-                notifyItemChanged(oddsDetail.oddArrayList.indexOf(oddsDetail.oddArrayList.find { o -> o == odd }))
+                notifyItemChanged(oddsDetail.needShowItem.indexOf(oddsDetail.needShowItem.find { o -> o == odd }))
             }
         }
     }
-
 
     var mOddsDetail: OddsDetailListData? = null
         set(value) {
@@ -36,16 +37,37 @@ class TypeOneListAdapter(
             notifyDataSetChanged()
         }
 
+    override fun getItemViewType(position: Int): Int {
+        return when (onMoreClickListener) {
+            null -> return ItemType.ITEM.ordinal
+            else -> when (position) {
+                oddsDetail.needShowItem.size -> ItemType.MORE.ordinal
+                else -> ItemType.ITEM.ordinal
+            }
+        }
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.content_type_one_list_item, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            ItemType.ITEM.ordinal -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.content_type_one_list_item, parent, false))
+            else -> MoreViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.content_type_more_item, parent, false))
+        }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) =
+        when (holder) {
+            is ViewHolder -> holder.bindModel(oddsDetail.needShowItem[position])
+            is MoreViewHolder -> {
+                holder.bind()
+            }
+            else -> {
+            }
+        }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bindModel(oddsDetail.oddArrayList[position])
-
-
-    override fun getItemCount(): Int = oddsDetail.oddArrayList.size
-
+    override fun getItemCount(): Int {
+        return if (onMoreClickListener == null)
+            oddsDetail.needShowItem.size
+        else oddsDetail.needShowItem.size + 1
+    }
 
     inner class ViewHolder(view: View) : OddStateViewHolder(view) {
 
@@ -63,8 +85,26 @@ class TypeOneListAdapter(
 
         override val oddStateChangeListener: OddStateChangeListener
             get() = mOddStateRefreshListener
-
     }
 
+    inner class MoreViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvExpandControl: TextView = itemView.findViewById(R.id.tv_expand_control)
+        fun bind() {
+            tvExpandControl.apply {
+                setOnClickListener {
+                    onMoreClickListener?.click()
+                }
+                text = if (oddsDetail.isMoreExpand) context.getString(R.string.odds_detail_less) else context.getString(R.string.odds_detail_more)
+            }
+        }
+    }
+
+    interface OnMoreClickListener {
+        fun click()
+    }
+
+    enum class ItemType {
+        ITEM, MORE
+    }
 
 }
