e069eb6ba41139e121b0e6015b0c2aab7e45e1e0
anderson <devanderson1108@gmail.com>
2022-07-26 17:04:48 +0800

    解决投注站代码冲突

--- app/src/main/java/org/cxct/sportlottery/network/bettingStation/BettingStationService.kt
+++ app/src/main/java/org/cxct/sportlottery/network/bettingStation/BettingStationService.kt
@@ -5,9 +5,6 @@ import org.cxct.sportlottery.network.index.chechBetting.CheckBettingResult
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface BettingStationService {
