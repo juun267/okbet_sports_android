e39d6fd6e4efc77cf7d138c7cbe315b9ca59465f
KevinLin <kevin_android@cxgi.org>
2022-07-27 18:24:29 +0800

    fix merge

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BetStationFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BetStationFragment.kt
@@ -3,6 +3,7 @@ package org.cxct.sportlottery.ui.withdraw
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
@@ -19,7 +20,6 @@ import com.bigkoo.pickerview.builder.TimePickerBuilder
 import com.bigkoo.pickerview.view.TimePickerView
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.google.android.gms.location.*
-import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.edittext_login.view.*
 import kotlinx.android.synthetic.main.fragment_bank_card.btn_submit
 import kotlinx.android.synthetic.main.fragment_bet_station.*
