074019c749b0a464f0840fe22acdfb0993e2d6f8
an_duck <android_duck@okbet.com>
2022-09-14 17:10:15 +0800

    调整银行卡列表图标获取方案修改

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardListAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/BankCardListAdapter.kt
@@ -77,12 +77,18 @@ class BankCardListAdapter(private val mBankCardListClickListener: BankCardListCl
             val bankOpen = moneyConfig?.uwTypes?.find { it.type == TransferType.BANK.type }?.open == MoneyRechCfg.Switch.ON.code
             val cryptoOpen = moneyConfig?.uwTypes?.find { it.type == TransferType.CRYPTO.type }?.open == MoneyRechCfg.Switch.ON.code
             itemView.apply {
-                iv_bank_icon.setImageResource(MoneyManager.getBankIconByBankName(data.bankName)) //银行卡图标
+                if (data.transferType == TransferType.CRYPTO) {
+                    iv_bank_icon.setImageResource(MoneyManager.getCryptoIconByCryptoName(data.bankName))//虚拟币图标
+                }else{
+                    iv_bank_icon.setImageResource(MoneyManager.getBankIconByBankName(data.bankName)) //银行卡图标
+                }
+
                 tv_bank_name.text = data.bankName //银行名字
                 tv_tail_number.text =
                     if (data.cardNo.length > 4) data.cardNo.substring(data.cardNo.length - 4) else  data.cardNo //尾號四碼
                 when(data.transferType){
                     TransferType.CRYPTO ->
+
                         if (sConfigData?.enableModifyBank == "1" && cryptoOpen) {
                             rl_item_content.apply {
                                 setOnClickListener {
