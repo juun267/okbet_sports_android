d2248b4fbd6855f69ac303f93e70f1555c0a5706
an_duck <android_duck@okbet.com>
2022-09-16 10:43:46 +0800

    注销账号页面BUG处理

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/cancelaccount/CancelAccountActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/cancelaccount/CancelAccountActivity.kt
@@ -2,12 +2,17 @@ package org.cxct.sportlottery.ui.profileCenter.cancelaccount
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_cancel_account.*
+import kotlinx.android.synthetic.main.activity_cancel_account.custom_tool_bar
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_setting_password.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.ui.base.BaseActivity
 import org.cxct.sportlottery.ui.feedback.FeedbackMainActivity
 import org.cxct.sportlottery.ui.maintab.MainTabActivity
+import org.cxct.sportlottery.widget.boundsEditText.AsteriskPasswordTransformationMethod
 
 /**
  * 注销账号页面
@@ -18,8 +23,9 @@ class CancelAccountActivity :BaseActivity<CancelAccountViewModel>(CancelAccountV
         super.onCreate(savedInstanceState)
         setStatusbar(R.color.color_232C4F_FFFFFF,true)
         setContentView(R.layout.activity_cancel_account)
-        initObserve()
+        init()
         onClick()
+        initObserve()
     }
     private fun initObserve() {
         viewModel.cancelResult.observe(this, Observer {
@@ -34,13 +40,37 @@ class CancelAccountActivity :BaseActivity<CancelAccountViewModel>(CancelAccountV
         })
     }
     private fun onClick(){
+
+        et_qq_number.endIconImageButton.setOnClickListener{
+            if (et_qq_number.endIconResourceId == R.drawable.ic_eye_open) {
+                et_password.transformationMethod =
+                    AsteriskPasswordTransformationMethod()
+                et_qq_number.setEndIcon(R.drawable.ic_eye_close)
+            } else {
+                et_qq_number.setEndIcon(R.drawable.ic_eye_open)
+                et_password.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+            }
+            et_qq_number.hasFocus = true
+            et_password.setSelection(et_password.text.toString().length)
+        }
         btn_cancel_account.setOnClickListener{
             et_password.text?.let {
                 viewModel.cancelAccount(it.toString())
             }
         }
         btn_feedback.setOnClickListener{
-            startActivity(Intent(this, FeedbackMainActivity::class.java))
+            startActivity(Intent(this@CancelAccountActivity, FeedbackMainActivity::class.java))
         }
+
+        custom_tool_bar.setOnBackPressListener {
+            finish()
+        }
+    }
+
+    private fun init(){
+        custom_tool_bar.titleText = getString(R.string.cancel_account)
+        et_password.transformationMethod =
+            AsteriskPasswordTransformationMethod()
     }
 }
