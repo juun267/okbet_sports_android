3f21fba28fcf43b0d03d17cfff46173373f6a80d
duck <android_duck@okbet.com>
2022-09-25 14:30:57 +0800

    OK-965 文案修改 图标icon替换

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/ProfileCenterFragment.kt
@@ -632,6 +632,7 @@ class ProfileCenterFragment :
                         startActivity(Intent(requireActivity(), BankActivity::class.java))
                     }
                 } else {
+                    LogUtil.d("duck点击提款准备跳转")
                     startActivity(Intent(requireActivity(), WithdrawActivity::class.java))
                 }
             }
@@ -738,8 +739,9 @@ class ProfileCenterFragment :
                 iconUrlResult?.msg?.let { msg -> showErrorPromptDialog(title = "", msg) {} }
         }
 
-        viewModel.intoWithdraw.observe(viewLifecycleOwner) {
+        viewModel.intoWithdraw.observe(viewLifecycleOwner) { it ->
             it.getContentIfNotHandled()?.let {
+                LogUtil.d("duck$it")
                 startActivity(Intent(requireActivity(), WithdrawActivity::class.java))
             }
         }
