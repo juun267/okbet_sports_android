002aa2db7fe88cb38594475a3be3960260933454
duck <android_duck@okbet.com>
2022-11-03 18:12:50 +0800

    电子，棋牌，直播主页的数据刷新

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotFragment.kt
@@ -26,10 +26,7 @@ import org.cxct.sportlottery.ui.maintab.HomeFragment
 import org.cxct.sportlottery.ui.maintab.HomeTabAdapter
 import org.cxct.sportlottery.ui.maintab.MainHomeViewModel
 import org.cxct.sportlottery.ui.maintab.MainTabActivity
-import org.cxct.sportlottery.util.JumpUtil
-import org.cxct.sportlottery.util.TextUtil
-import org.cxct.sportlottery.util.isOKPlat
-import org.cxct.sportlottery.util.observe
+import org.cxct.sportlottery.util.*
 import org.greenrobot.eventbus.EventBus
 
 /**
