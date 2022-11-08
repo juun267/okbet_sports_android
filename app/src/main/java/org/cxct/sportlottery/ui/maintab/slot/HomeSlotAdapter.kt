c7a1a85c653d46cdb0c82189d0592c38ec66c430
anderson <devanderson1108@gmail.com>
2022-11-08 09:43:00 +0800

    老虎机列表样式调整

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotAdapter.kt
@@ -31,15 +31,15 @@ class HomeSlotAdapter(data: MutableList<QueryGameEntryData>) :
 //            LanguageManager.Language.ZH -> item.chineseName
 //            else -> item.englishName
 //        })
-        if (item.status == 1) {
-            helper.setText(R.id.tv_status, R.string.new_games_beta)
-            helper.setGone(R.id.tv_game_name, true)
-            helper.setGone(R.id.iv_repair, false)
-        } else {
-            helper.setText(R.id.tv_status, R.string.comingsoon)
-            helper.setGone(R.id.tv_game_name, false)
-            helper.setGone(R.id.iv_repair, true)
-        }
+//        if (item.status == 1) {
+        helper.setText(R.id.tv_status, R.string.new_games_beta)
+        helper.setGone(R.id.tv_game_name, true)
+//            helper.setGone(R.id.iv_repair, false)
+//        } else {
+//            helper.setText(R.id.tv_status, R.string.comingsoon)
+//            helper.setGone(R.id.tv_game_name, false)
+//            helper.setGone(R.id.iv_repair, true)
+//        }
     }
 
 }
