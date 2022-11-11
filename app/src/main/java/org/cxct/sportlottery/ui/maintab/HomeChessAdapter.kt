f08bd99145502808d039a3b108f5da43bcebd18a
anderson <devanderson1108@gmail.com>
2022-11-11 22:42:02 +0800

    点击直播列表，棋牌列表调整宽度

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/HomeChessAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/HomeChessAdapter.kt
@@ -5,22 +5,27 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
-import kotlinx.android.synthetic.main.home_recommend_item.view.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.network.third_game.third_games.QueryGameEntryData
+import org.cxct.sportlottery.util.DisplayUtil.dp
+import org.cxct.sportlottery.util.ScreenUtil
 
 class HomeChessAdapter(data: MutableList<QueryGameEntryData>):
     BaseQuickAdapter<QueryGameEntryData, BaseViewHolder>(R.layout.item_poker_game,data) {
 
     override fun convert(helper: BaseViewHolder, item: QueryGameEntryData) {
-
+        var itemWith = (ScreenUtil.getScreenWidth(mContext) - 30.dp - 10.dp) / 2
+        helper.itemView.layoutParams.apply {
+            width = itemWith
+            helper.itemView.layoutParams = this
+        }
         Glide.with(mContext)
             .load(item.entryImage)
             .apply(
                 RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .dontTransform()
-                .placeholder(R.drawable.icon_chess_and_card))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .dontTransform()
+                    .placeholder(R.drawable.icon_chess_and_card))
             .into(helper.getView(R.id.iv_poker_bg))
 
     }
