d6822b8850a38b6ba1445088f1b326f381ea72d4
duck <android_duck@okbet.com>
2022-12-07 10:40:08 +0800

    OKBETOK-3703

--- app/src/main/java/org/cxct/sportlottery/ui/login/foget/ForgetViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/login/foget/ForgetViewModel.kt
@@ -139,8 +139,6 @@ class ForgetViewModel(
     fun checkPassword(password: String, confirmPassword: String? = null): String? {
         val msg = when {
             password.isNullOrEmpty() -> LocalUtils.getString(R.string.error_input_empty)
-            !VerifyConstUtil.verifyPwdFormat(password) -> LocalUtils.getString(R.string.error_new_password)
-            password.length !in 6..20 -> LocalUtils.getString(R.string.error_new_password)
             !VerifyConstUtil.verifyPwd(password) -> LocalUtils.getString(R.string.error_new_password)
             else -> null
         }
