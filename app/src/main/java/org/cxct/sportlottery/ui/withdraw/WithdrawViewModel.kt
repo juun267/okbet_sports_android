02328a7202b4d6cf314d5cefc7eff1ee184230fd
duck <android_duck@okbet.com>
2022-12-12 12:06:01 +0800

    paymaya本地代码注释掉

--- app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/withdraw/WithdrawViewModel.kt
@@ -496,11 +496,11 @@ class WithdrawViewModel(
                                     TransferType.STATION.type
                                 )
                             }
-                            UWType.PAY_MAYA.type -> {
-                                if (withdrawConfig.find { it.type == TransferType.PAYMAYA.type }?.open.toString() == FLAG_OPEN) tabList.add(
-                                    TransferType.PAYMAYA.type
-                                )
-                            }
+//                            UWType.PAY_MAYA.type -> {
+//                                if (withdrawConfig.find { it.type == TransferType.PAYMAYA.type }?.open.toString() == FLAG_OPEN) tabList.add(
+//                                    TransferType.PAYMAYA.type
+//                                )
+//                            }
                         }
                     }
                     _withdrawTabIsShow.postValue(tabList)
@@ -770,12 +770,12 @@ class WithdrawViewModel(
                 0,
                 RoundingMode.FLOOR
             )
-            TransferType.PAYMAYA -> ArithUtil.div(
-                (userMoney.value ?: 0.0),
-                ((cardConfig?.feeRate?.plus(1) ?: 1.0)),
-                0,
-                RoundingMode.FLOOR
-            )
+//            TransferType.PAYMAYA -> ArithUtil.div(
+//                (userMoney.value ?: 0.0),
+//                ((cardConfig?.feeRate?.plus(1) ?: 1.0)),
+//                0,
+//                RoundingMode.FLOOR
+//            )
         }
     }
 
@@ -877,9 +877,9 @@ class WithdrawViewModel(
             TransferType.STATION -> {
                 rechargeConfigs.value?.uwTypes?.find { config -> config.type == TransferType.STATION.type }?.detailList?.first()
             }
-            TransferType.PAYMAYA -> {
-                rechargeConfigs.value?.uwTypes?.find { config -> config.type == TransferType.PAYMAYA.type }?.detailList?.first()
-            }
+//            TransferType.PAYMAYA -> {
+//                rechargeConfigs.value?.uwTypes?.find { config -> config.type == TransferType.PAYMAYA.type }?.detailList?.first()
+//            }
         }
     }
 
