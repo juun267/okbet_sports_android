1dc71e4299e15638968d72629b310f1a6d9554fc
clyde <android_clyde@okbet.com>
2023-01-03 17:31:18 +0800

    修复'注单'页面可赢金额显示误差

--- app/src/main/java/org/cxct/sportlottery/util/ArithUtil.kt
+++ app/src/main/java/org/cxct/sportlottery/util/ArithUtil.kt
@@ -53,9 +53,9 @@ object ArithUtil : DecimalFormatUtil() {
     /**
      * 20201015賠率、手續費率等，保留小數點後三位
      */
-    fun toOddFormat(value: Double?): String {
+    fun toOddFormat(value: Double?, scale: Int= 3): String {
         val rounded = round(value ?: 0.0, 4, RoundingMode.HALF_UP)
-        return round(rounded.toDouble(), 3, RoundingMode.HALF_UP)
+        return round(rounded.toDouble(), scale, RoundingMode.HALF_UP)
     }
 
     /**
