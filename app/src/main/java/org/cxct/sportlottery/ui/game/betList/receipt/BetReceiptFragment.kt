66659a162b703dfd2152860dd793b21790000d9b
Ray <android_ray@okbet.com>
2023-02-13 18:05:24 +0800

    注单购物车 viewModel修改

--- app/src/main/java/org/cxct/sportlottery/ui/game/betList/receipt/BetReceiptFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/game/betList/receipt/BetReceiptFragment.kt
@@ -21,6 +21,7 @@ import org.cxct.sportlottery.repository.sConfigData
 import org.cxct.sportlottery.repository.showCurrencySign
 import org.cxct.sportlottery.ui.base.BaseBottomNavViewModel
 import org.cxct.sportlottery.ui.base.BaseSocketFragment
+import org.cxct.sportlottery.ui.game.betList.BetListViewModel
 import org.cxct.sportlottery.ui.maintab.MainTabActivity
 import org.cxct.sportlottery.util.BetsFailedReasonUtil
 import org.cxct.sportlottery.util.LocalUtils
@@ -34,7 +35,7 @@ import timber.log.Timber
  * @app_destination 注單收據
  */
 class BetReceiptFragment :
-    BaseSocketFragment<BaseBottomNavViewModel>(BaseBottomNavViewModel::class) {
+    BaseSocketFragment<BetListViewModel>(BetListViewModel::class) {
 
     private var betResultData: Receipt? = null
 
