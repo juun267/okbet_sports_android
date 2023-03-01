902d88ebd3f5d2ce654659520fa2129a26b2a1da
anderson <devanderson1108@gmail.com>
2023-03-01 14:14:09 +0800

    像素单位调整

--- app/src/main/java/org/cxct/sportlottery/extentions/ContextExt.kt
+++ app/src/main/java/org/cxct/sportlottery/extentions/ContextExt.kt
@@ -16,15 +16,6 @@ inline val Context.screenHeight: Int
 inline val Context.density: Float
     get() = resources.displayMetrics.density
 
-//dp 转为 px
-inline fun Context.dp2px(value: Int): Int = (density * value).toInt()
-
-//dp 转为 px
-inline fun Context.dp2px(value: Float): Int = (density * value).toInt()
-
-//px 转为 dp
-inline fun Context.px2dp(value: Int): Float = value.toFloat() / density
-
 //使用内联函数的泛型参数 reified 特性来实现
 inline fun <reified T : Activity> Context.startActivity() {
     val intent = Intent(this, T::class.java)
