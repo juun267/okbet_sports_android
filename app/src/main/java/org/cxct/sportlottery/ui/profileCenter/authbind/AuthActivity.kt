ff28af152f5d50f7092b263d43adbd51941c0f57
anderson <devanderson1108@gmail.com>
2023-03-02 16:38:25 +0800

    谷歌登陆注册/授权显示

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/authbind/AuthActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/authbind/AuthActivity.kt
@@ -6,9 +6,11 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.view_base_tool_bar_no_drawer.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.repository.sConfigData
 import org.cxct.sportlottery.ui.base.BaseSocketActivity
+import org.cxct.sportlottery.ui.game.ServiceDialog
 import org.cxct.sportlottery.util.AuthManager
-import org.cxct.sportlottery.util.setServiceClick
+import org.cxct.sportlottery.util.JumpUtil
 import org.cxct.sportlottery.util.setStartDrawable
 
 /**
@@ -93,7 +95,21 @@ class AuthActivity : BaseSocketActivity<AuthViewModel>(AuthViewModel::class) {
     }
 
     private fun setupServiceButton() {
-        tv_customer_service.setServiceClick(supportFragmentManager)
+        tv_customer_service.setOnClickListener {
+            val serviceUrl = sConfigData?.customerServiceUrl
+            val serviceUrl2 = sConfigData?.customerServiceUrl2
+            when {
+                !serviceUrl.isNullOrBlank() && !serviceUrl2.isNullOrBlank() -> {
+                    ServiceDialog().show(supportFragmentManager, null)
+                }
+                serviceUrl.isNullOrBlank() && !serviceUrl2.isNullOrBlank() -> {
+                    JumpUtil.toExternalWeb(this@AuthActivity, serviceUrl2)
+                }
+                !serviceUrl.isNullOrBlank() && serviceUrl2.isNullOrBlank() -> {
+                    JumpUtil.toExternalWeb(this@AuthActivity, serviceUrl)
+                }
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
