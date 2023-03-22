41d63222d20a495a7fa562ac10cc1bce705e1705
anderson <devanderson1108@gmail.com>
2023-03-22 14:52:00 +0800

    赛事显示tab显示优化

--- app/src/main/java/org/cxct/sportlottery/ui/sport/detail/SportDetailActivity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/sport/detail/SportDetailActivity.kt
@@ -540,30 +540,28 @@ class SportDetailActivity : BaseBottomNavActivity<SportViewModel>(SportViewModel
         }
         viewModel.oddsDetailResult.observe(this) {
             it?.getContentIfNotHandled()?.let { result ->
-                if (!result.success) {
-                    showErrorPromptDialog(getString(R.string.prompt), result.msg) {}
-                    return@observe
-                }
-                if (tabCateAdapter.dataList.size <= tabCateAdapter.selectedPosition) {
-                    return@observe
-                }
-                result.setupPlayCateTab()
-                oddsDetailListAdapter?.notifyDataSetChangedByCode(tabCateAdapter.dataList[tabCateAdapter.selectedPosition].code)
-                matchOdd = result.oddsDetailData?.matchOdd
+                if (result.success) {
+                    result.setupPlayCateTab()
+                    oddsDetailListAdapter?.notifyDataSetChangedByCode(tabCateAdapter.dataList[tabCateAdapter.selectedPosition].code)
+                    matchOdd = result.oddsDetailData?.matchOdd
 
-                result.oddsDetailData?.matchOdd?.matchInfo?.let { matchInfo ->
-                    this.matchInfo = matchInfo
-                    //region 配置主客隊名稱給內部Item使用
-                    matchInfo.homeName?.let { home ->
-                        oddsDetailListAdapter?.homeName = home
+                    result.oddsDetailData?.matchOdd?.matchInfo?.let { matchInfo ->
+                        this.matchInfo = matchInfo
+                        //region 配置主客隊名稱給內部Item使用
+                        matchInfo.homeName?.let { home ->
+                            oddsDetailListAdapter?.homeName = home
+                        }
+                        matchInfo.awayName.let { away ->
+                            oddsDetailListAdapter?.awayName = away
+                        }
+                        //endregion
+
+                        setupMatchInfo(matchInfo)
                     }
-                    matchInfo.awayName.let { away ->
-                        oddsDetailListAdapter?.awayName = away
-                    }
-                    //endregion
-                    setupMatchInfo(matchInfo)
+                    setupLiveView(result.oddsDetailData?.matchOdd?.matchInfo?.liveVideo)
+                } else {
+                    showErrorPromptDialog(getString(R.string.prompt), result.msg) {}
                 }
-                setupLiveView(result.oddsDetailData?.matchOdd?.matchInfo?.liveVideo)
             }
         }
 
