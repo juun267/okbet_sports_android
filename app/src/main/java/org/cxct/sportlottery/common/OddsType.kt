27c15befed39e7ce8b302318023d42974f5177da
anderson <devanderson1108@gmail.com>
2023-03-23 14:25:43 +0800

    解决投注奔溃

--- app/src/main/java/org/cxct/sportlottery/common/OddsType.kt
+++ app/src/main/java/org/cxct/sportlottery/common/OddsType.kt
@@ -1,7 +1,9 @@
 package org.cxct.sportlottery.common
 
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.proguard.KeepMembers
 
+@KeepMembers
 enum class OddsType(val code: String, val res: Int) {
     HK("HK", R.string.odd_type_hk),
     EU("EU", R.string.odd_type_eu),
