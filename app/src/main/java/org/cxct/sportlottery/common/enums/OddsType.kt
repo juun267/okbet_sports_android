16ac888e23b3b4011aa3cae3113cc3d55c53b0e1
Ray <android_ray@okbet.com>
2023-03-30 16:48:16 +0800

    1.优化混淆注解 2.优化启动页加载逻辑

--- app/src/main/java/org/cxct/sportlottery/common/enums/OddsType.kt
+++ app/src/main/java/org/cxct/sportlottery/common/enums/OddsType.kt
@@ -1,9 +1,9 @@
 package org.cxct.sportlottery.common.enums
 
 import org.cxct.sportlottery.R
-import org.cxct.sportlottery.common.proguards.KeepClasses
+import org.cxct.sportlottery.common.proguards.KeepMembers
 
-@KeepClasses
+@KeepMembers
 enum class OddsType(val code: String, val res: Int) {
     HK("HK", R.string.odd_type_hk),
     EU("EU", R.string.odd_type_eu),
--- app/src/main/java/org/cxct/sportlottery/common/proguards/KeepMembers.kt
+++ app/src/main/java/org/cxct/sportlottery/common/proguards/KeepMembers.kt
@@ -1,7 +1,12 @@
 package org.cxct.sportlottery.common.proguards
 
+import android.annotation.SuppressLint
+import java.lang.annotation.ElementType
+
+
 // 可配注解与类、方法、属性，注解后将不会被混淆.(如果注解到类上面，则：方法和属性都不会被混淆)
-@Retention(AnnotationRetention.SOURCE)
+
+@Retention(AnnotationRetention.BINARY)
 @Target(
     AnnotationTarget.ANNOTATION_CLASS,
     AnnotationTarget.CLASS,
@@ -11,4 +16,13 @@ package org.cxct.sportlottery.common.proguards
     AnnotationTarget.CONSTRUCTOR,
     AnnotationTarget.FIELD
 )
-annotation class KeepMembers 
+
+//@SuppressLint("SupportAnnotationUsage")
+//@java.lang.annotation.Target(
+//    ElementType.TYPE,
+//    ElementType.ANNOTATION_TYPE,
+//    ElementType.CONSTRUCTOR,
+//    ElementType.METHOD,
+//    ElementType.FIELD
+//)
+annotation class KeepMembers
