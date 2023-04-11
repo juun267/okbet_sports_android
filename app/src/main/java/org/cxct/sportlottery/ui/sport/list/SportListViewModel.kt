5a14fec7f7b31cf313162903b7cb8a65a2dbfed6
android_clyde <android_clyde@okbet.com>
2023-04-11 16:03:51 +0800

    修复玩法赔率不显示问题

--- app/src/main/java/org/cxct/sportlottery/ui/sport/list/SportListViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/sport/list/SportListViewModel.kt
@@ -381,11 +381,10 @@ class SportListViewModel(
 
                     matchOdd.setupOddDiscount()
                     matchOdd.updateOddStatus()
-                    matchOdd.oddsSort =
-                        PlayCateMenuFilterUtils.filterOddsSort(
-                            matchOdd.matchInfo?.gameType,
-                            MenuCode.MAIN.code
-                        )
+                    matchOdd.oddsSort = PlayCateMenuFilterUtils.filterOddsSort(matchOdd.matchInfo?.gameType,
+                            if (playCateMenuCode == MenuCode.CS.code) { MenuCode.CS.code } else { MenuCode.MAIN.code } )
+
+
                     matchOdd.filterQuickPlayCate(matchType)
                     //波胆的数据获取方式
                     if (matchType == MatchType.CS.postValue) {
