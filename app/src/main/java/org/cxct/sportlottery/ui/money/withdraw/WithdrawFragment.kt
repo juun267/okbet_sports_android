c7e869d98c08cbcaee4a1f90e6b9bb0af190034d
anderson <devanderson1108@gmail.com>
2023-04-12 10:30:07 +0800

    翻译修改

--- app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/WithdrawFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/WithdrawFragment.kt
@@ -200,7 +200,7 @@ class WithdrawFragment : BaseSocketFragment<WithdrawViewModel>(WithdrawViewModel
                                 if (paymataTransfer) View.VISIBLE else View.GONE
                         }
                         tv_add_bank.text = context?.getString(R.string.bank_list_add,
-                            context?.getString(R.string.pay_maya))
+                            context?.getString(R.string.online_maya))
                         clearEvent()
                     }
                     getString(R.string.Outlets_Reserve) -> {
