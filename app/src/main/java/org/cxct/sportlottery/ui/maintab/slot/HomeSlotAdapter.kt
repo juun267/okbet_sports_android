b84c564b903546d5b151c01c26192e741cce581b
Ray <android_ray@okbet.com>
2023-04-17 10:37:01 +0800

    FunkyGame去掉Beta

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/slot/HomeSlotAdapter.kt
@@ -6,6 +6,7 @@ import org.cxct.sportlottery.common.extentions.load
 import org.cxct.sportlottery.databinding.ItemHomeSlotBinding
 import org.cxct.sportlottery.network.third_game.third_games.QueryGameEntryData
 import org.cxct.sportlottery.ui.common.adapter.BindingAdapter
+import timber.log.Timber
 
 class HomeSlotAdapter: BindingAdapter<QueryGameEntryData, ItemHomeSlotBinding>() {
 
@@ -15,9 +16,10 @@ class HomeSlotAdapter: BindingAdapter<QueryGameEntryData, ItemHomeSlotBinding>()
 
         ivPeople.load(item.entryImage)
         tvFirmName.text = item.firmName
+        Timber.d("gameCode:${item.gameCode}")
         tvStatus.setText(if (
             item.gameCode == "TPG"  ||
-                    item.gameCode == "FunkyGame"
+                    item.gameCode == "FKG"
         ) R.string.new_games else R.string.new_games_beta)
         tvGameName.gone()
     }
