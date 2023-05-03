0be5476dca9624a60ceff2c1d98ce8e080077fd0
dachang <android_dachang@okbet.com>
2023-05-03 16:38:16 +0800

    bingo UI

---
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/RecyclerBingoExtraAdapter.kt
@@ -0,0 +1,10 @@
+package org.cxct.sportlottery.ui.maintab.home.view
+
+import org.cxct.sportlottery.databinding.ItemBingoExtraBinding
+import org.cxct.sportlottery.ui.common.adapter.BindingAdapter
+
+class RecyclerBingoExtraAdapter : BindingAdapter<String, ItemBingoExtraBinding>() {
+
+    override fun onBinding(position: Int, binding: ItemBingoExtraBinding, item: String) {
+    }
+}
