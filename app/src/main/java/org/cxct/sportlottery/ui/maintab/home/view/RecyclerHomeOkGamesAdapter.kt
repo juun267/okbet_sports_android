a0b553a1c00ba69d45e925c8de45287c1548b96a
android_clyde <android_clyde@okbet.com>
2023-05-08 16:57:37 +0800

    调节UI

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/RecyclerHomeOkGamesAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/RecyclerHomeOkGamesAdapter.kt
@@ -1,30 +1,40 @@
 package org.cxct.sportlottery.ui.maintab.home.view
 
 import android.view.View
+import android.view.ViewGroup
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.common.extentions.load
 import org.cxct.sportlottery.databinding.ItemHomeOkgameBinding
 import org.cxct.sportlottery.net.games.data.OKGameBean
 import org.cxct.sportlottery.ui.common.adapter.BindingAdapter
+import org.cxct.sportlottery.ui.common.adapter.BindingVH
 
 class RecyclerHomeOkGamesAdapter : BindingAdapter<OKGameBean, ItemHomeOkgameBinding>() {
     private var imageSize:Int=0
 
+    override fun onCreateDefViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BindingVH<ItemHomeOkgameBinding> {
+        val vh = super.onCreateDefViewHolder(parent, viewType)
+        setImageSize(vh.vb.cardCover)
+        return vh
+    }
+
     override fun onBinding(position: Int, binding: ItemHomeOkgameBinding, item: OKGameBean) {
-        binding.tvGameName.text=item.gameName
-        binding.tvGameType.text=item.firmName
-        setImageSize(binding.cardCover)
+        binding.tvGameName.text = item.gameName
+        binding.tvGameType.text = item.firmName
         binding.ivCover.load(item.imgGame, R.drawable.img_banner01)
     }
 
     fun setScreenWidth(screenWidth:Int){
-        imageSize=screenWidth/3
+        imageSize = screenWidth/3
     }
 
     private fun setImageSize(imageView: View){
-        val layoutParams=imageView.layoutParams
-        layoutParams.width=imageSize
-        layoutParams.height=imageSize
-        imageView.layoutParams=layoutParams
+        val layoutParams = imageView.layoutParams
+        layoutParams.width = imageSize
+        layoutParams.height = imageSize
+        imageView.layoutParams = layoutParams
     }
 }
