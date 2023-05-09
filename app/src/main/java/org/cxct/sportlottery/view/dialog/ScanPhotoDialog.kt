8bfceaf5d114610219d3f569e8c9c7fc5f61a8bc
Ray <android_ray@okbet.com>
2023-05-09 11:53:00 +0800

    1.使用封装的baseBindingDialog重写ScanPhotoDialog

--- app/src/main/java/org/cxct/sportlottery/view/dialog/ScanPhotoDialog.kt
+++ app/src/main/java/org/cxct/sportlottery/view/dialog/ScanPhotoDialog.kt
@@ -7,51 +7,46 @@ import android.view.WindowManager
 import android.widget.TextView
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.databinding.DialogScanCameraPhotoBinding
+import org.cxct.sportlottery.ui.base.BaseBindingDialog
 import org.cxct.sportlottery.util.DisplayUtil.dp
 import org.cxct.sportlottery.util.drawable.DrawableCreatorUtils
 
-class ScanPhotoDialog(mContext: Context) : Dialog(mContext) {
+class ScanPhotoDialog(mContext: Context) : BaseBindingDialog<DialogScanCameraPhotoBinding>(mContext,
+DialogScanCameraPhotoBinding::inflate) {
 
-    init {
-        initDialog()
-    }
 
     var tvAlbumClickListener: (() -> Unit)? = null
     var tvCameraScanClickListener: (() -> Unit)? = null
 
 
-    private fun initDialog() {
-        val binding = DialogScanCameraPhotoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        window?.setBackgroundDrawableResource(android.R.color.transparent)
-        val tvAlbum = findViewById<TextView>(R.id.tvAlbum)
-        val tvCameraScan = findViewById<TextView>(R.id.tvCameraScan)
 
-        tvAlbum.background = DrawableCreatorUtils.getCommonBackgroundStyle(
-            20,
-            R.color.color_025BE8,
-        )
+    override fun initHeightParams(): Int = 115.dp
 
-        tvCameraScan.background = DrawableCreatorUtils.getCommonBackgroundStyle(
-            20,
-            R.color.color_ff8A00,
-        )
+    override fun initWidthParams(): Int = 245.dp
 
-        tvAlbum.setOnClickListener {
-            tvAlbumClickListener?.invoke()
-            dismiss()
+    override fun initView() {
+        binding.apply {
+            tvAlbum.background = DrawableCreatorUtils.getCommonBackgroundStyle(
+                20,
+                R.color.color_025BE8,
+            )
+
+            tvCameraScan.background = DrawableCreatorUtils.getCommonBackgroundStyle(
+                20,
+                R.color.color_ff8A00,
+            )
+
+            tvAlbum.setOnClickListener {
+                tvAlbumClickListener?.invoke()
+                dismiss()
+            }
+
+            tvCameraScan.setOnClickListener {
+                tvCameraScanClickListener?.invoke()
+                dismiss()
+            }
         }
 
-        tvCameraScan.setOnClickListener {
-            tvCameraScanClickListener?.invoke()
-            dismiss()
-        }
-
-        val lp = window?.attributes
-        lp?.width = WindowManager.LayoutParams.WRAP_CONTENT
-        lp?.height = WindowManager.LayoutParams.WRAP_CONTENT
-        lp?.gravity = Gravity.CENTER
-        window?.attributes = lp
     }
 
 
