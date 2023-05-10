6f2f8e3bd3a736610a636d408a2f2f5cfbc64bd4
android_clyde <android_clyde@okbet.com>
2023-05-10 14:15:29 +0800

    撤销调试代码

--- app/src/main/java/org/cxct/sportlottery/network/manager/RequestManager.kt
+++ app/src/main/java/org/cxct/sportlottery/network/manager/RequestManager.kt
@@ -65,10 +65,10 @@ class RequestManager private constructor(context: Context) {
 
         .apply {
             //debug版本才打印api內容
-//            if (BuildConfig.DEBUG) {
-//                addInterceptor(logging)
+            if (BuildConfig.DEBUG) {
+                addInterceptor(logging)
                 addInterceptor(HttpLogInterceptor())
-//            }
+            }
         }
 
     private val mMoshi: Moshi = Moshi.Builder()
