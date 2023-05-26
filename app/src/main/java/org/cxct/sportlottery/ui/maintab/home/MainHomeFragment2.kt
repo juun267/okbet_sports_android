828c164db277e0516a47630546bca53cd2cd5eca
kevin <an_kevin@okbet.com>
2023-05-26 16:27:21 +0800

    添加跑马灯数据为空时隐藏ui布局的逻辑

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/MainHomeFragment2.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/MainHomeFragment2.kt
@@ -59,16 +59,31 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
 
     override fun onBindViewStatus(view: View) = binding.run {
         homeTopView.setup(this@MainHomeFragment2)
-        hotMatchView.onCreate(viewModel.publicityRecommend,this@MainHomeFragment2)
+        hotMatchView.onCreate(viewModel.publicityRecommend, this@MainHomeFragment2)
         okGamesView.setOkGamesData(this@MainHomeFragment2)
         initBetWinsRecodeLayout()
         initObservable()
     }
 
     private fun initBetWinsRecodeLayout() {
-        binding.winsRankView.setUp(viewLifecycleOwner, { viewModel.getRecordNew() }, { viewModel.getRecordResult() })
-        receiver.recordBetNew.observe(viewLifecycleOwner) { it?.let { binding.winsRankView.onNewWSBetData(it) } }
-        receiver.recordWinsResult.observe(viewLifecycleOwner) { it?.let { binding.winsRankView.onNewWSWinsData(it) } }//最新大奖
+        binding.winsRankView.setUp(
+            viewLifecycleOwner,
+            { viewModel.getRecordNew() },
+            { viewModel.getRecordResult() })
+        receiver.recordBetNew.observe(viewLifecycleOwner) {
+            it?.let {
+                binding.winsRankView.onNewWSBetData(
+                    it
+                )
+            }
+        }
+        receiver.recordWinsResult.observe(viewLifecycleOwner) {
+            it?.let {
+                binding.winsRankView.onNewWSWinsData(
+                    it
+                )
+            }
+        }//最新大奖
         viewModel.recordBetNewHttp.observe(viewLifecycleOwner) {
             if (!it.isNullOrEmpty()) {
                 binding.winsRankView.onNewHttpBetData(it.reversed())
@@ -96,6 +111,7 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
             refreshHotMatch()
         }
     }
+
     override fun onHiddenChanged(hidden: Boolean) {
         if (hidden) {
             //隐藏时取消赛事监听
@@ -132,28 +148,31 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
 
             val titleList: MutableList<String> = mutableListOf()
             messageListResult.rows?.forEach { data ->
-                if (data.type.toInt() == 1)  {
+                if (data.type.toInt() == 1) {
                     titleList.add(data.title + " - " + data.message)
                 }
             }
             setupAnnouncement(titleList)
         }
     }
+
     //hot match
-    private fun refreshHotMatch(){
+    private fun refreshHotMatch() {
         //重新设置赔率监听
         binding.hotMatchView.postDelayed({
             binding.hotMatchView.onResume(this@MainHomeFragment2)
             viewModel.getRecommend()
-        },500)
+        }, 500)
 
     }
+
     //hot match end
     private fun initNews() {
         binding.includeNews.apply {
             tabNews.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab?) {
-                    val categoryId = if (tab?.position == 0) NewsRepository.NEWS_OKBET_ID else NewsRepository.NEWS_SPORT_ID
+                    val categoryId =
+                        if (tab?.position == 0) NewsRepository.NEWS_OKBET_ID else NewsRepository.NEWS_SPORT_ID
                     viewModel.getHomeNews(1, 5, listOf(categoryId))
                 }
 
@@ -176,7 +195,10 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
                 rvNews.adapter = HomeNewsAdapter().apply {
                     setList(newsList)
                     setOnItemClickListener(listener = OnItemClickListener { adapter, view, position ->
-                        NewsDetailActivity.start(requireContext(),(adapter.data[position] as NewsItem))
+                        NewsDetailActivity.start(
+                            requireContext(),
+                            (adapter.data[position] as NewsItem)
+                        )
                     })
                 }
             } else {
@@ -190,8 +212,12 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
             if (rvBettingStation.adapter == null) {
                 rvBettingStation.layoutManager =
                     LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-                rvBettingStation.addItemDecoration(SpaceItemDecoration(requireContext(),
-                    R.dimen.margin_10))
+                rvBettingStation.addItemDecoration(
+                    SpaceItemDecoration(
+                        requireContext(),
+                        R.dimen.margin_10
+                    )
+                )
                 PagerSnapHelper().attachToRecyclerView(rvBettingStation)
                 rvBettingStation.adapter = HomeBettingStationAdapter().apply {
                     setList(newsList)
@@ -210,22 +236,28 @@ class MainHomeFragment2 : BindingSocketFragment<MainHomeViewModel, FragmentMainH
     }
 
     private fun setupAnnouncement(titleList: List<String>) {
-        var marqueeAdapter = MarqueeAdapter()
-        lin_announcement.setOnClickListener {
-            startActivity(Intent(requireActivity(), NewsActivity::class.java))
-        }
-        rv_marquee.apply {
-            layoutManager =
-                LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            adapter = marqueeAdapter
+        if (titleList.isEmpty()) {
+            lin_announcement.visibility = View.GONE
+        } else {
+            lin_announcement.visibility = View.VISIBLE
+            var marqueeAdapter = MarqueeAdapter()
+            lin_announcement.setOnClickListener {
+                startActivity(Intent(requireActivity(), NewsActivity::class.java))
+            }
+            rv_marquee.apply {
+                layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                adapter = marqueeAdapter
+            }
+
+            marqueeAdapter.setData(titleList.toMutableList())
+            if (titleList.isNotEmpty()) {
+                rv_marquee.startAuto(false) //啟動跑馬燈
+            } else {
+                rv_marquee.stopAuto(true) //停止跑馬燈
+            }
         }
 
-        marqueeAdapter.setData(titleList.toMutableList())
-        if (titleList.isNotEmpty()) {
-            rv_marquee.startAuto(false) //啟動跑馬燈
-        } else {
-            rv_marquee.stopAuto(true) //停止跑馬燈
-        }
     }
 
 }
