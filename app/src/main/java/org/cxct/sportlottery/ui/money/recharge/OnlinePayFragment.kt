205c22c7c3b07798ead7b96c6730bf1e9b84c9cf
anderson <devanderson1108@gmail.com>
2023-06-16 15:47:06 +0800

    增加aub圆图

--- app/src/main/java/org/cxct/sportlottery/ui/money/recharge/OnlinePayFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/recharge/OnlinePayFragment.kt
@@ -173,7 +173,7 @@ class OnlinePayFragment : BaseFragment<MoneyRechViewModel>(MoneyRechViewModel::c
             OnlineType.PAYPAL.type -> R.drawable.ic_paypal_type
             OnlineType.DRAGON_PAY.type -> R.drawable.ic_gragon_pay_type
             OnlineType.FORTUNE_PAY.type -> R.drawable.icon_fortunepay
-            OnlineType.AUB.type -> R.drawable.ic_aub
+            OnlineType.AUB.type -> R.drawable.ic_aub_round
             else -> R.drawable.ic_online_pay_type
         }
         return this
