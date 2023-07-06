8fc7e4c5250dedb950fe021c1c5b7f81399ad021
anderson <devanderson1108@gmail.com>
2023-07-06 14:14:53 +0800

    倒计时结束保持等待状态

--- app/src/main/java/org/cxct/sportlottery/ui/betList/receipt/BetReceiptFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/betList/receipt/BetReceiptFragment.kt
@@ -275,8 +275,9 @@ class BetReceiptFragment :
             }
 
             betReceiptDiffAdapter?.refreshBetStatusFinishFunction = {
-                lin_result_status_processing?.gone()
-                lin_result_status?.visible()
+                //倒计时结束后，不处理
+//                lin_result_status_processing?.gone()
+//                lin_result_status?.visible()
             }
 
             adapter = betReceiptDiffAdapter
