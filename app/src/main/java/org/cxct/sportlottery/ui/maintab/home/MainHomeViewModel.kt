32d9734d1259c2ea5aeaf3359b971244f26d8fe7
clyde <android_clyde@okbet.com>
2023-07-07 12:26:35 +0800

    fix bug

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/MainHomeViewModel.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/MainHomeViewModel.kt
@@ -1,7 +1,6 @@
 package org.cxct.sportlottery.ui.maintab.home
 
 import android.app.Application
-import android.text.TextUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -24,25 +23,16 @@ import org.cxct.sportlottery.network.common.FavoriteType
 import org.cxct.sportlottery.network.common.MatchType
 import org.cxct.sportlottery.network.match.MatchRound
 import org.cxct.sportlottery.network.message.MessageListResult
-import org.cxct.sportlottery.network.odds.list.MatchLiveData
 import org.cxct.sportlottery.network.service.record.RecordNewEvent
-import org.cxct.sportlottery.network.sport.SportMenuData
 import org.cxct.sportlottery.network.sport.SportMenuFilter
 import org.cxct.sportlottery.network.sport.publicityRecommend.PublicityRecommendRequest
 import org.cxct.sportlottery.network.sport.publicityRecommend.Recommend
-import org.cxct.sportlottery.network.third_game.third_games.QueryGameEntryConfigRequest
 import org.cxct.sportlottery.network.third_game.third_games.QueryGameEntryData
-import org.cxct.sportlottery.network.third_game.third_games.TotalRewardAmountData
-import org.cxct.sportlottery.network.third_game.third_games.hot.HandicapData
-import org.cxct.sportlottery.network.third_game.third_games.hot.HotMatchLiveData
 import org.cxct.sportlottery.repository.*
 import org.cxct.sportlottery.ui.base.BaseBottomNavViewModel
 import org.cxct.sportlottery.ui.base.BaseFragment
 import org.cxct.sportlottery.ui.maintab.entity.EnterThirdGameResult
-import org.cxct.sportlottery.ui.maintab.entity.GameCateData
-import org.cxct.sportlottery.ui.maintab.publicity.PromotionItemData
 import org.cxct.sportlottery.util.*
-import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -62,6 +52,7 @@ open class MainHomeViewModel(
     infoCenterRepository,
     favoriteRepository,
 ) {
+
     private val _publicityRecommend = MutableLiveData<Event<List<Recommend>>>()
     val publicityRecommend: LiveData<Event<List<Recommend>>>
         get() = _publicityRecommend
@@ -75,60 +66,21 @@ open class MainHomeViewModel(
     val messageListResult: LiveData<Event<MessageListResult?>>
         get() = _messageListResult
 
-    //優惠活動圖文公告
-    private val _publicityPromotionList = MutableLiveData<List<PromotionItemData>>()
-    val publicityPromotionList: LiveData<List<PromotionItemData>>
-        get() = _publicityPromotionList
     val _enterThirdGameResult = MutableLiveData<Pair<String, EnterThirdGameResult>>()
     val enterThirdGameResult: LiveData<Pair<String, EnterThirdGameResult>>
         get() = _enterThirdGameResult
 
     //试玩线路
-    private val _enterTrialPlayGameResult = MutableLiveData<Pair<String, EnterThirdGameResult>?>()
+    private val _enterTrialPlayGameResult = SingleLiveEvent<Pair<String, EnterThirdGameResult>?>()
     val enterTrialPlayGameResult: LiveData<Pair<String, EnterThirdGameResult>?>
         get() = _enterTrialPlayGameResult
-
-    private val _errorPromptMessage = MutableLiveData<Event<String>>()
-    val token = loginRepository.token
-
-    val homeGameData: LiveData<List<QueryGameEntryData>?>
-        get() = _homeGameData
-    private val _homeGameData = MutableLiveData<List<QueryGameEntryData>?>()
-    val elecGameData: LiveData<List<QueryGameEntryData>?>
-        get() = _elecGameData
-    private val _elecGameData = MutableLiveData<List<QueryGameEntryData>?>()
-    val slotGameData: LiveData<List<QueryGameEntryData>?>
-        get() = _slotGameData
-    private val _slotGameData = MutableLiveData<List<QueryGameEntryData>?>()
-
-    private val _totalRewardAmount = MutableLiveData<List<TotalRewardAmountData>>()
-    val totalRewardAmount: LiveData<List<TotalRewardAmountData>>
-        get() = _totalRewardAmount
-
-    private val _liveRoundHall = MutableLiveData<List<MatchLiveData>>()
-    val liveRoundHall: LiveData<List<MatchLiveData>>
-        get() = _liveRoundHall
-    val hotLiveData: LiveData<List<HotMatchLiveData>?>
-        get() = _hotLiveData
-    private val _hotLiveData = MutableLiveData<List<HotMatchLiveData>?>()
-
-    val hotHandicap: LiveData<Event<List<HandicapData>?>>
-        get() = _hotHandicap
-    private val _hotHandicap = MutableLiveData<Event<List<HandicapData>?>>()
 
     //賽事列表直播網址
     private val _matchLiveInfo = MutableLiveData<Event<MatchRound>?>()
     val matchLiveInfo: LiveData<Event<MatchRound>?>
         get() = _matchLiveInfo
 
-    //首页的直播地址获取
-    private val _homeMatchLiveInfo = MutableLiveData<Event<MatchRound>?>()
-    val homeMatchLiveInfo: LiveData<Event<MatchRound>?>
-        get() = _homeMatchLiveInfo
 
-    private val _liveRoundCount = MutableLiveData<String>()
-    val liveRoundCount: LiveData<String>
-        get() = _liveRoundCount
     val gameBalanceResult: LiveData<Event<Triple<String, EnterThirdGameResult, Double>>>
         get() = _gameBalanceResult
     private var _gameBalanceResult =
@@ -377,56 +329,6 @@ open class MainHomeViewModel(
         }
     }
 
-    fun getThirdGame() {
-        viewModelScope.launch {
-            doNetwork(androidContext) {
-                ThirdGameRepository.getThirdGame()
-            }
-        }
-    }
-
-    private suspend fun getThirdGameList(): MutableList<GameCateData>? {
-        doNetwork(androidContext) {
-            ThirdGameRepository.getThirdGameResponse()
-        }?.let { result ->
-            return if (result.success) {
-                ThirdGameRepository.createHomeGameList(result.t)
-            } else {
-                Timber.e("獲取第三方遊戲配置失敗")
-                null
-            }
-        }
-        return null
-    }
-
-    private fun SportMenuData.sortSport(): SportMenuData {
-        this.menu.inPlay.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.today.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.early.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.cs.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.parlay.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.outright.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.atStart.items.sortedBy { sport ->
-            sport.sortNum
-        }
-        this.menu.eps?.items?.sortedBy { sport ->
-            sport.sortNum
-        }
-
-        return this
-    }
 
     //獲取系統公告
     fun getAnnouncement() {
@@ -547,14 +449,6 @@ open class MainHomeViewModel(
         }
     }
 
-    /**
-     * 未登录试玩
-     */
-    private suspend fun thirdGameNoLogin(firmType: String, gameCode: String): NetResult? {
-        return doNetwork(androidContext) {
-            OneBoSportApi.thirdGameService.thirdNoLogin(firmType, gameCode)
-        }
-    }
 
     private suspend fun autoTransfer(firmType: String): Boolean {
         if (isThirdTransferOpen()) {
@@ -567,49 +461,6 @@ open class MainHomeViewModel(
         }
 
         return false
-    }
-
-    fun setSportClosePromptMessage(sport: String) {
-        _errorPromptMessage.postValue(
-            Event(
-                String.format(
-                    androidContext.getString(R.string.message_no_sport_game),
-                    sport
-                )
-            )
-        )
-    }
-
-    //region 宣傳頁 優惠活動文字跑馬燈、圖片公告
-    fun getPublicityPromotion() {
-        sConfigData?.imageList?.filter { it.imageType == ImageType.PROMOTION.code && !(getMarketSwitch() && it.isHidden) }
-            ?.let { promotionList ->
-                promotionList.filter {
-                    (it.viewType == 1) && TextUtils.equals(LanguageManager.getSelectLanguage(
-                        androidContext).key, it.lang)
-                }
-                    .mapNotNull { it.imageText1 }
-                    .let {
-                        //優惠活動圖片公告清單
-                        _publicityPromotionList.postValue(promotionList.map {
-                            PromotionItemData.createData(it)
-                        })
-                    }
-            }
-    }
-
-    /**
-     * 設置大廳獲取的玩法排序、玩法名稱
-     */
-    private fun Recommend.setupOddsSort() {
-        val nowGameType = gameType
-        val playCateMenuCode = menuList.firstOrNull()?.code
-        val oddsSortFilter = PlayCateMenuFilterUtils.filterOddsSort(nowGameType, playCateMenuCode)
-        val playCateNameMapFilter =
-            PlayCateMenuFilterUtils.filterPlayCateNameMap(nowGameType, playCateMenuCode)
-
-        oddsSort = oddsSortFilter
-        playCateNameMap = playCateNameMapFilter
     }
 
     private fun Recommend.sortOddsByMenu() {
@@ -664,121 +515,7 @@ open class MainHomeViewModel(
             matchInfo?.socketMatchStatus = status
         }
     }
-    //二次改版region
-    /**
-     * 电子和棋牌
-     * @position position 1: 首页； 2: 主页
-     * @gameType gameType 1: 棋牌； 2: 电子
-     */
-    fun getGameEntryConfig(position: Int, gameType: Int?) {
-        viewModelScope.launch {
-            val result = doNetwork(androidContext) {
-                OneBoSportApi.thirdGameService.queryGameEntryConfig(
-                    QueryGameEntryConfigRequest(position, gameType)
-                )
-            }
-            result?.rows.let {
-                if (position==2) {
-                    if (gameType == 1) {
-                        _slotGameData.postValue(it)
-                    } else {
-                        _elecGameData.postValue(it)
-                    }
-                }else{
-                    _homeGameData.postValue(it)
-                }
-            }
-        }
-    }
 
-    /**
-     * 电子金额排名
-     */
-    fun getTotalRewardAmount() {
-        viewModelScope.launch {
-            val result = doNetwork(androidContext) {
-                OneBoSportApi.thirdGameService.queryTotalRewardAmount()
-            }
-            result?.rows?.let {
-                _totalRewardAmount.postValue(it)
-            }
-        }
-    }
-
-    /**
-     * 热门盘口
-     * @handicapType 盘口类型, 1:独赢 2：让球 3:大小
-     */
-
-    fun getHandicapConfig(handicapType: Int) {
-        viewModelScope.launch {
-            val result = doNetwork(androidContext) {
-                OneBoSportApi.thirdGameService.getHotHandicapList(handicapType)
-            }
-            result?.rows.let { handicapList ->
-                handicapList?.forEach { hotHandicap ->
-                    hotHandicap.matchInfos.forEach {
-                        it.leagueName =
-                            if (TextUtils.isEmpty(it.leagueName)) hotHandicap.league.name else it.leagueName
-                    }
-                }
-                _hotHandicap.postValue(Event(handicapList))
-            }
-        }
-    }
-    /**
-     * 热门直播
-     */
-
-    fun getHotLiveList(){
-        viewModelScope.launch {
-          doNetwork(androidContext) {
-                OneBoSportApi.thirdGameService.getLiveList()
-            }?.let { result->
-                _hotLiveData.postValue(result.MatchLiveList)
-            }
-        }
-    }
-    fun getLiveRoundHall() {
-        viewModelScope.launch {
-            var result = doNetwork(androidContext) {
-                OneBoSportApi.matchService.getLiveRoundHall()
-            }?.let {
-                if (it.success) {
-                    _liveRoundHall.postValue(it.MatchLiveList)
-                }
-            }
-        }
-    }
-
-    fun getLiveInfo(roundNo: String, position: Int) {
-        viewModelScope.launch {
-            var result = doNetwork(androidContext) {
-                OneBoSportApi.matchService.getMatchLiveRound(roundNo)
-            }
-            result?.matchRound?.let {
-                it.roundNo = roundNo
-                if (position == 0) {
-                    _homeMatchLiveInfo.postValue(Event(it))
-                } else {
-                    _matchLiveInfo.postValue(Event(it))
-                }
-
-            }
-        }
-    }
-    //直播数量
-    fun getLiveRoundCount() {
-        viewModelScope.launch {
-            var result = doNetwork(androidContext) {
-                OneBoSportApi.matchService.getLiveRoundCount()
-            }?.let {
-                if (it.success) {
-                    _liveRoundCount.postValue(it.t.toString())
-                }
-            }
-        }
-    }
 
     private fun getGameBalance(
         firmType: String,
