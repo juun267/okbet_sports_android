76e8062f957bd70e785cc8592323f6c0f5a8c2a1
anderson <devanderson1108@gmail.com>
2023-07-10 11:52:39 +0800

    获取热门赛事数据

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/games/OKGamesFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/games/OKGamesFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_all_okgames.*
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.common.event.MenuEvent
 import org.cxct.sportlottery.common.extentions.isEmptyStr
@@ -22,6 +24,7 @@ import org.cxct.sportlottery.util.EventBusUtil
 import org.cxct.sportlottery.util.FragmentHelper
 import org.cxct.sportlottery.util.enterThirdGame
 import org.cxct.sportlottery.util.loginedRun
+import org.cxct.sportlottery.view.ObservableScrollView
 import org.cxct.sportlottery.view.dialog.PopImageDialog
 import org.cxct.sportlottery.view.transform.TransformInDialog
 
@@ -65,6 +68,7 @@ class OKGamesFragment : BaseBottomNavigationFragment<OKGamesViewModel>(OKGamesVi
     override fun onBindView(view: View) {
         initToolBar()
         initTopView()
+        initScrollView()
         showGameAll()
         initObservable()
         viewModel.getOKGamesHall()
@@ -125,7 +129,18 @@ class OKGamesFragment : BaseBottomNavigationFragment<OKGamesViewModel>(OKGamesVi
             }
         }
     }
+    private fun initScrollView(){
+        binding.scrollView.setOnScrollStatusListener(object :
+            ObservableScrollView.OnScrollStatusListener{
+            override fun onScrollStop() {
+                if (isAllTba())
+                    hot_match_view.firstVisibleRange((fragmentHelper.getFragment(0) as AllGamesFragment))
+            }
 
+            override fun onScrolling() {
+            }
+        })
+    }
     private fun onTabChange(tab: OKGameTab): Boolean {
         when {
 
