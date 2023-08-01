8a19a11d3b229a215f02d46ef490e92fac945f4c
clyde <android_clyde@okbet.com>
2023-08-01 10:37:17 +0800

    最新大奖优化

--- app/src/main/java/org/cxct/sportlottery/network/service/record/RecordNewEvent.kt
+++ app/src/main/java/org/cxct/sportlottery/network/service/record/RecordNewEvent.kt
@@ -1,21 +1,17 @@
 package org.cxct.sportlottery.network.service.record
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 import org.cxct.sportlottery.common.proguards.KeepMembers
 import org.cxct.sportlottery.network.service.ServiceEventType
 
-@JsonClass(generateAdapter = true)
 @KeepMembers
 data class RecordNewEvent(
-    @Json(name = "eventType")
     override val eventType: String,
-    @Json(name = "player")
     val player: String,
-    @Json(name = "games")
     val games: String,
-    @Json(name = "betAmount")
     val betAmount: String,
-    @Json(name = "profitAmount")
     var profitAmount: String,
+    val iconUrl: String?= null,
+    val betTime: Long = 0,
+    val firmType: String? = null,
+    val gameCode: String? = null
     ) : ServiceEventType
---
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/WinsDialog.kt
@@ -0,0 +1,73 @@
+package org.cxct.sportlottery.ui.maintab.home.view
+
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.common.extentions.load
+import org.cxct.sportlottery.common.extentions.toIntS
+import org.cxct.sportlottery.network.service.record.RecordNewEvent
+import org.cxct.sportlottery.repository.showCurrencySign
+import org.cxct.sportlottery.ui.base.BaseAlertDialog
+import org.cxct.sportlottery.util.TimeUtil
+import org.cxct.sportlottery.util.TimeUtil.NEWS_TIME_FORMAT2
+import org.cxct.sportlottery.util.drawable.DrawableCreatorUtils
+
+class WinsDialog(private val betRecode: RecordNewEvent,
+                 act: AppCompatActivity,
+                 private val onEnterGame: (firmType: String?, gameCode: String?) -> Unit)
+    : BaseAlertDialog(act), OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.setBackgroundDrawable(null)
+        setContentView(R.layout.dialog_wins_betdetails)
+        initStyle()
+        bindInfo()
+    }
+
+    private fun bindInfo() {
+        findViewById<ImageView>(R.id.ivGamesIcon)!!.load("", R.drawable.ic_okgames_nodata)
+
+        setText(R.id.tvGameName, betRecode.games)
+        setText(R.id.tvPlayer, "${context.getString(R.string.N710)}: ${betRecode.player}")
+
+
+        if (betRecode.betTime != 0L) {
+            setText(R.id.tvBetTime, "${context.getString(R.string.I039)}: ${TimeUtil.timeFormat(betRecode.betTime, NEWS_TIME_FORMAT2)}")
+        }
+        showCurrencySign.let {
+            setText(R.id.tvBet, "$it ${betRecode.betAmount}")
+            val color = if (betRecode.profitAmount.toIntS() < 0) {
+                context.getColor(R.color.color_FF2E00)
+            } else {
+                context.getColor(R.color.color_1CD219)
+            }
+            setText(R.id.tvPayout, "$it ${betRecode.profitAmount}").setTextColor(color)
+        }
+    }
+
+    private inline fun setText(viewId: Int, text: String): TextView {
+        return findViewById<TextView>(viewId)!!.apply { setText(text) }
+    }
+
+
+    private fun initStyle() {
+        findViewById<View>(R.id.ivClose)!!.setOnClickListener { dismiss() }
+        val bgDrawable = DrawableCreatorUtils.getCommonBackgroundStyle(8, R.color.white, R.color.color_ebf1fc)
+        findViewById<View>(R.id.llBet)!!.background = bgDrawable
+        findViewById<View>(R.id.llPayout)!!.background = bgDrawable
+        val btnPlay = findViewById<View>(R.id.btnPlay)!!
+        btnPlay.background = DrawableCreatorUtils.getCommonBackgroundStyle(8, R.color.color_025BE8)
+        btnPlay.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        dismiss()
+        onEnterGame.invoke(betRecode.firmType, betRecode.gameCode)
+    }
+
+}
