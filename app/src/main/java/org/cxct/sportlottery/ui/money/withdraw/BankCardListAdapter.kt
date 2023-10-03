9325adc9bc14549e2cdd1cf2eb86651048a4271f
clyde <android_clyde@okbet.com>
2023-10-03 11:26:49 +0800

    编辑绑卡前需要短信验证

--- app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankCardListAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankCardListAdapter.kt
@@ -1,133 +1,74 @@
 package org.cxct.sportlottery.ui.money.withdraw
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.content_rv_bank_card_list.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.common.adapter.BindingAdapter
+import org.cxct.sportlottery.databinding.ContentRvBankCardListBinding
 import org.cxct.sportlottery.network.bank.my.BankCardList
 import org.cxct.sportlottery.network.money.config.MoneyRechCfg
-import org.cxct.sportlottery.network.money.config.MoneyRechCfgData
 import org.cxct.sportlottery.network.money.config.TransferType
+import org.cxct.sportlottery.network.money.config.UwType
 import org.cxct.sportlottery.repository.sConfigData
-import org.cxct.sportlottery.util.LocalUtils
 import org.cxct.sportlottery.util.MoneyManager
 
-class BankCardListAdapter(private val mBankCardListClickListener: BankCardListClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BankCardListAdapter(private val onCryptoEdit: (BankCardList) -> Unit,
+                          private val onBankEdit: (BankCardList) -> Unit,
+                          private val onDelete: (BankCardList) -> Unit
+): BindingAdapter<BankCardList, ContentRvBankCardListBinding>() {
 
-    enum class CardType { EDIT, CRYPTO_EDIT }
-
-    var bankList = listOf<BankCardList>()
+    var uwTypes: List<UwType>? = null
         set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-
-
-    var moneyConfig: MoneyRechCfgData? = null
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return BankItemViewHolder.from(parent)
-
-    }
-
-    override fun getItemCount(): Int {
-        return bankList.size
-
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return when (bankList[position].transferType) {
-                     TransferType.BANK -> CardType.EDIT.ordinal
-                    TransferType.CRYPTO -> CardType.CRYPTO_EDIT.ordinal
-                    TransferType.E_WALLET -> CardType.EDIT.ordinal
-            TransferType.PAYMAYA -> CardType.EDIT.ordinal
-            TransferType.STATION -> CardType.EDIT.ordinal
-        }
-    }
-
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is BankItemViewHolder -> {
-                holder.bind(bankList[position], moneyConfig, mBankCardListClickListener)
-            }
-            else ->{
-                //do nothing
-            }
-        }
-    }
-
-    class BankItemViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(data: BankCardList, moneyConfig: MoneyRechCfgData?, mBankCardListClickListener: BankCardListClickListener) {
-            val bankOpen = moneyConfig?.uwTypes?.find { it.type == TransferType.BANK.type }?.open == MoneyRechCfg.Switch.OPEN.code
-            val cryptoOpen = moneyConfig?.uwTypes?.find { it.type == TransferType.CRYPTO.type }?.open == MoneyRechCfg.Switch.OPEN.code
-            itemView.apply {
-                if (data.transferType == TransferType.CRYPTO) {
-                    iv_bank_icon.setImageResource(MoneyManager.getCryptoIconByCryptoName(data.bankName))//虚拟币图标
-                }else{
-                    iv_bank_icon.setImageResource(MoneyManager.getBankIconByBankName(data.bankName)) //银行卡图标
-                }
-
-                tv_bank_name.text = data.bankName //银行名字
-                tv_tail_number.text =
-                    if (data.cardNo.length > 4) data.cardNo.substring(data.cardNo.length - 4) else  data.cardNo //尾號四碼
-                bank_card_type.text =  when(data.transferType.type){
-                    TransferType.BANK.type -> LocalUtils.getString(R.string.bank_card)
-                    TransferType.CRYPTO.type -> LocalUtils.getString(R.string.crypto)
-                    TransferType.E_WALLET.type -> LocalUtils.getString(R.string.ewallet)
-                    TransferType.PAYMAYA.type -> context.getString(R.string.ewallet)
-                    else -> LocalUtils.getString(R.string.bank_card)
-                }
-                when(data.transferType){
-                    TransferType.CRYPTO ->
-
-                        if (sConfigData?.enableModifyBank == "1" && cryptoOpen) {
-                            rl_item_content.apply {
-                                setOnClickListener {
-                                    mBankCardListClickListener.onCryptoEdit(data)
-                                }
-                            }
-                        }
-                    else -> {
-                        if (sConfigData?.enableModifyBank == "1" && bankOpen) {
-                            rl_item_content.apply {
-                                setOnClickListener {
-                                    mBankCardListClickListener.onBankEdit(data)
-                                }
-                            }
-                        }
-                    }
-                }
-
+            if (value != field) {
+                field = value
+                notifyDataSetChanged()
             }
         }
 
-        companion object {
-            fun from(parent: ViewGroup): RecyclerView.ViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.content_rv_bank_card_list, parent, false)
-                return BankItemViewHolder(view)
+    override fun onBinding(position: Int, binding: ContentRvBankCardListBinding, data: BankCardList) = binding.run {
+        val bankOpen = uwTypes?.find { it.type == TransferType.BANK.type }?.open == MoneyRechCfg.Switch.OPEN.code
+        val cryptoOpen = uwTypes?.find { it.type == TransferType.CRYPTO.type }?.open == MoneyRechCfg.Switch.OPEN.code
+
+        if (data.transferType == TransferType.CRYPTO) {
+            ivBankIcon.setImageResource(MoneyManager.getCryptoIconByCryptoName(data.bankName))//虚拟币图标
+        } else {
+            ivBankIcon.setImageResource(MoneyManager.getBankIconByBankName(data.bankName)) //银行卡图标
+        }
+
+        tvBankName.text = data.bankName //银行名字
+        tvTailNumber.text = if (data.cardNo.length > 4) data.cardNo.substring(data.cardNo.length - 4) else data.cardNo //尾號四碼
+        bankCardType.text = when (data.transferType.type) {
+            TransferType.BANK.type -> context.getString(R.string.bank_card)
+            TransferType.CRYPTO.type -> context.getString(R.string.crypto)
+            TransferType.E_WALLET.type -> context.getString(R.string.ewallet)
+            TransferType.PAYMAYA.type -> context.getString(R.string.ewallet)
+            else -> context.getString(R.string.bank_card)
+        }
+
+        if (data.transferType == TransferType.CRYPTO) {
+            if (sConfigData?.enableModifyBank == "1" && cryptoOpen) {
+                rlItemContent.setOnClickListener { onCryptoEdit(data) }
+                ivDelete.setOnClickListener { onDelete(data) }
+            } else {
+                rlItemContent.setOnClickListener(null)
+                ivDelete.setOnClickListener(null)
+            }
+        } else {
+            if (sConfigData?.enableModifyBank == "1" && bankOpen) {
+                rlItemContent.setOnClickListener { onBankEdit(data) }
+                ivDelete.setOnClickListener { onDelete(data) }
+            } else {
+                rlItemContent.setOnClickListener(null)
+                ivDelete.setOnClickListener(null)
             }
         }
     }
 
-
-}
-
-class BankCardListClickListener(
-    private val editBankListener: (bankCard: BankCardList) -> Unit,
-    private val editCryptoListener: (cryptoCard: BankCardList) -> Unit,
-
-) {
-    fun onBankEdit(bankCard: BankCardList) = editBankListener(bankCard)
-    fun onCryptoEdit(cryptoCard: BankCardList) = editCryptoListener(cryptoCard)
+    fun removeCard(cardNo: String) {
+        data.forEachIndexed { index, bankCardList ->
+            if (bankCardList.id.toString() == cardNo) {
+                removeAt(index)
+                return
+            }
+        }
+    }
 
 }
--- app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankListFragment.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankListFragment.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_bank_list.*
 import kotlinx.android.synthetic.main.fragment_bank_list.view.*
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.common.extentions.isEmptyStr
+import org.cxct.sportlottery.common.extentions.setLinearLayoutManager
+import org.cxct.sportlottery.common.extentions.toLongS
 import org.cxct.sportlottery.network.money.config.PAYMAYA
 import org.cxct.sportlottery.network.money.config.TransferType
+import org.cxct.sportlottery.repository.UserInfoRepository
 import org.cxct.sportlottery.ui.base.BaseFragment
+import org.cxct.sportlottery.util.ToastUtil
 
 /**
  * @app_destination 提款设置
  */
 class BankListFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::class) {
+
     private val mNavController by lazy {
         findNavController()
     }
@@ -26,26 +32,36 @@ class BankListFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::clas
 
     private val mBankListAdapter by lazy {
         BankCardListAdapter(
-            BankCardListClickListener(
-                editBankListener = {
+                {
                     val transferType =
                         if (it.transferType == TransferType.E_WALLET && it.bankCode == PAYMAYA) TransferType.PAYMAYA else it.transferType
-                    val action =
-                        BankListFragmentDirections.actionBankListFragmentToBankCardFragment(it,
+                    val action = BankListFragmentDirections.actionBankListFragmentToBankCardFragment(it,
                             transferType,
                             null)
                     mNavController.navigate(action)
                 },
-                editCryptoListener = {
+                {
                     val transferType =
                         if (it.transferType == TransferType.E_WALLET && it.bankCode == PAYMAYA) TransferType.PAYMAYA else it.transferType
-                    val action =
-                        BankListFragmentDirections.actionBankListFragmentToBankCardFragment(it,
+                    val action = BankListFragmentDirections.actionBankListFragmentToBankCardFragment(it,
                             transferType,
                             null)
                     mNavController.navigate(action)
+                }, {
+
+                    val phoneNo = UserInfoRepository.userInfo?.value?.phone
+                    if (phoneNo.isEmptyStr()) {
+                        ToastUtil.showToast(context, R.string.set_phone_no)
+                        return@BankCardListAdapter
+                    }
+
+                    DeleteBankCardDialog(phoneNo!!) { pwd, code ->
+                        loading()
+                        viewModel.deleteBankCard(it.id.toString(), pwd, code)
+                    }.show(childFragmentManager, null)
                 }
-            ))
+
+            )
 
     }
 
@@ -74,41 +90,35 @@ class BankListFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::clas
     private fun setupObserve() {
         viewModel.loading.observe(this.viewLifecycleOwner, Observer {
             if (it)
-               // loading()
+//                loading()
             else
                 hideLoading()
         })
 
         viewModel.rechargeConfigs.observe(this.viewLifecycleOwner, Observer {
-            mBankListAdapter.moneyConfig = it
+            mBankListAdapter.uwTypes = it.uwTypes
         })
         viewModel.addMoneyCardSwitch.observe(this.viewLifecycleOwner, Observer {
          //   mBankListAdapter.transferAddSwitch = it
-           it.run {
-               val stringList = arrayListOf<String>()
-               if ((!bankTransfer) && (!cryptoTransfer) && (!walletTransfer) && (!paymataTransfer)) {
-                   return@Observer
-               }
-               if (bankTransfer) stringList.add(getString(R.string.bank_list_bank))
-               if (cryptoTransfer) stringList.add(getString(R.string.bank_list_crypto))
-               if (walletTransfer) stringList.add(getString(R.string.bank_list_e_wallet))
-               if (paymataTransfer) stringList.add(getString(R.string.online_maya))
-               cardTypeTitle = stringList.joinToString("/")
-               tv_unbind_bank_card.text = getString(R.string.bank_list_not_bink, cardTypeTitle)
-               tv_add_money_card_type.text =
-                   getString(R.string.add_credit_or_virtual, cardTypeTitle)
-               tv_money_card_type.text =
-                   getString(R.string.my_bank_card, cardTypeTitle, viewModel.numberOfBankCard.value)
-            }
+           val stringList = arrayListOf<String>()
+           if ((!it.bankTransfer) && (!it.cryptoTransfer) && (!it.walletTransfer) && (!it.paymataTransfer)) {
+               return@Observer
+           }
+           if (it.bankTransfer) stringList.add(getString(R.string.bank_list_bank))
+           if (it.cryptoTransfer) stringList.add(getString(R.string.bank_list_crypto))
+           if (it.walletTransfer) stringList.add(getString(R.string.bank_list_e_wallet))
+           if (it.paymataTransfer) stringList.add(getString(R.string.online_maya))
+           cardTypeTitle = stringList.joinToString("/")
+           tv_unbind_bank_card.text = getString(R.string.bank_list_not_bink, cardTypeTitle)
+           tv_add_money_card_type.text =
+               getString(R.string.add_credit_or_virtual, cardTypeTitle)
+           tv_money_card_type.text =
+               getString(R.string.my_bank_card, cardTypeTitle, viewModel.numberOfBankCard.value)
         })
         viewModel.bankCardList.observe(this.viewLifecycleOwner, Observer { bankCardList ->
-            bankCardList.let { data ->
-                mBankListAdapter.bankList = data ?: listOf()
-                if (!data.isNullOrEmpty()) {
-                    tv_no_bank_card.visibility = View.GONE
-                }
-                viewModel.checkBankCardCount()
-            }
+            mBankListAdapter.setNewInstance(bankCardList?.toMutableList())
+            tv_no_bank_card.isVisible = bankCardList.isNullOrEmpty()
+            viewModel.checkBankCardCount()
             if (bankCardList.isNullOrEmpty()){
                 tv_unbind_bank_card.visibility = View.VISIBLE
             }else{
@@ -133,14 +143,18 @@ class BankListFragment : BaseFragment<WithdrawViewModel>(WithdrawViewModel::clas
                     transferType)
             mNavController.navigate(action)
         }
+
+        viewModel.bankDeleteResult.observe(this.viewLifecycleOwner) {
+            val result = it.second
+            ToastUtil.showToast(context, result.msg)
+            if (result.success) {
+                mBankListAdapter.removeCard(it.first.toString())
+            }
+        }
     }
 
     private fun setupRecyclerView(view: View) {
-        view.rv_bank_list.apply {
-            layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false).apply {
-            }
-
-            adapter = mBankListAdapter
-        }
+        view.rv_bank_list.setLinearLayoutManager()
+        view.rv_bank_list.adapter = mBankListAdapter
     }
 }
