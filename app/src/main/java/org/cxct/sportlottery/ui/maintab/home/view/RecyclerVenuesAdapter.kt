01e29f57dafa749a13e421a0a05b99a2551bdb45
clyde <android_clyde@okbet.com>
2023-10-06 17:25:41 +0800

    fix bug

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/RecyclerVenuesAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/RecyclerVenuesAdapter.kt
@@ -1,5 +1,6 @@
 package org.cxct.sportlottery.ui.maintab.home.view
 
+import androidx.core.view.isGone
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.common.adapter.BindingAdapter
 import org.cxct.sportlottery.common.extentions.gone
@@ -44,8 +45,10 @@ class RecyclerVenuesAdapter : BindingAdapter<HomeGameBean, ItemHomeVenuesBinding
                 //bingo
                 HomeTopView.OkBingo->{
                     ivSportCover.setImageResource(R.drawable.img_esport)
-                    //判断体育维护是否开启
-                    if(getSportEnterIsClose()){
+                    if (!item.isOpen()) {
+                        tvSportClose.visible()
+                        tvSportClose.text=context.getString(R.string.N700)
+                    } else if(getSportEnterIsClose()){ //判断体育维护是否开启
                         //展示维护中
                         tvSportClose.visible()
                         tvSportClose.text=context.getString(R.string.N257)
@@ -68,7 +71,14 @@ class RecyclerVenuesAdapter : BindingAdapter<HomeGameBean, ItemHomeVenuesBinding
                         tvSportClose.text=context.getString(R.string.N700)
                     }
                 }
+
+                else -> {
+                    tvSportClose.visible()
+                    tvSportClose.text=context.getString(R.string.N700)
+                }
             }
         }
+
+        binding.root.isEnabled = binding.tvSportClose.isGone
     }
 }
