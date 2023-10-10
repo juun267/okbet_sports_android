7cf23d91d21451e7cbf922f68eec73f1fddee5fa
clyde <android_clyde@okbet.com>
2023-10-10 17:21:53 +0800

    代码回退

--- app/src/main/java/org/cxct/sportlottery/util/ResourceWrapper.kt
+++ app/src/main/java/org/cxct/sportlottery/util/ResourceWrapper.kt
@@ -5,22 +5,28 @@ import android.content.res.Configuration
 import android.content.res.Resources
 import android.os.Build
 import android.os.LocaleList
+import org.cxct.sportlottery.util.LanguageManager
 import java.util.*
 
 class ResourceWrapper(private val context: Context, origin: Resources): Resources(origin.assets, origin.displayMetrics, origin.configuration) {
 
     private var currentLocale: Locale = LanguageManager.getSetLanguageLocale(context)
+    private var localeContext: Context = getLocalizedContext(currentLocale)
 
     override fun getString(id: Int): String {
         checkLocal()
-        return super.getString(id)
+        return try {
+            localeContext.getString(id)
+        } catch (e: Exception) {
+            ""
+        }
     }
 
     private fun checkLocal() {
         val selectedLocal = LanguageManager.getSetLanguageLocale(context)
         if (currentLocale != selectedLocal) {
             currentLocale = selectedLocal
-            getLocalizedContext(currentLocale)
+            localeContext = getLocalizedContext(currentLocale)
         }
     }
 
