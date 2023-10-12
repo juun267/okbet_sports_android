e5af17fb97b437b2df8472f30354645e22772bc3
clyde <android_clyde@okbet.com>
2023-10-12 15:09:48 +0800

    优化MarqueeAdapter

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/publicity/MarqueeAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/publicity/MarqueeAdapter.kt
@@ -1,15 +1,15 @@
 package org.cxct.sportlottery.ui.maintab.publicity
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import org.cxct.sportlottery.R
 
-
-open class MarqueeAdapter : RecyclerView.Adapter<MarqueeAdapter.DetailViewHolder>() {
+open class MarqueeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
         const val TYPE_ITEM = 100
@@ -18,14 +18,18 @@ open class MarqueeAdapter : RecyclerView.Adapter<MarqueeAdapter.DetailViewHolder
 
     private var mDataList: MutableList<String> = mutableListOf()
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): DetailViewHolder {
-        val layoutView = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.content_marquee, viewGroup, false)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
+        val textView = AppCompatTextView(viewGroup.context)
+        textView.layoutParams = ViewGroup.LayoutParams(-2, -1)
+        textView.gravity = Gravity.CENTER_VERTICAL
+        textView.textSize = 12.0f
+        textView.maxLines = 1
+        textView.setTextColor(viewGroup.context.getColor(R.color.color_6D7693))
         //開頭結尾的空白過場，寬度設置跟 父層 Layout 一樣
-        if (viewType == TYPE_BLANK) layoutView.minimumWidth = viewGroup.measuredWidth
+        if (viewType == TYPE_BLANK) textView.minimumWidth = viewGroup.measuredWidth
 
-        return DetailViewHolder(layoutView)
+        return MarqueeVH(textView, textView)
     }
 
     override fun getItemCount(): Int {
@@ -40,11 +44,11 @@ open class MarqueeAdapter : RecyclerView.Adapter<MarqueeAdapter.DetailViewHolder
     }
 
     @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(viewHolder: DetailViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
         try {
             if (getItemViewType(position) == TYPE_ITEM && mDataList.size != 0) {
                 val dataPosition = (position - 1) % mDataList.size
-                viewHolder.detail.text = mDataList[dataPosition] + "　"
+                (viewHolder.itemView as TextView).text = mDataList[dataPosition] + "　"
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -60,9 +64,7 @@ open class MarqueeAdapter : RecyclerView.Adapter<MarqueeAdapter.DetailViewHolder
         notifyDataSetChanged()//更新資料
     }
 
+    class MarqueeVH(private val textView: TextView, itemView: View): RecyclerView.ViewHolder(itemView)
 
-    open class DetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        open var detail: TextView = itemView.findViewById(R.id.tv_marquee)
-    }
 
 }
