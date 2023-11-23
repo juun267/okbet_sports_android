e1c7234e54d4e4fa22989ccb9c8d8ef8b20b8916
clyde <android_clyde@okbet.com>
2023-11-23 17:21:29 +0800

    修复UI问题

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/game/slot/ElectTabAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/game/slot/ElectTabAdapter.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.listener.OnItemClickListener
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import org.cxct.sportlottery.R
+import org.cxct.sportlottery.application.MultiLanguagesApplication
 import org.cxct.sportlottery.net.games.data.OKGamesCategory
 import org.cxct.sportlottery.util.AppFont
 import org.cxct.sportlottery.util.DisplayUtil.dp
@@ -22,9 +24,11 @@ class ElectTabAdapter(private val onSelected:(OKGamesCategory) -> Unit)
     private val iconId = View.generateViewId()
     private val nameId = View.generateViewId()
     private val dividerId = View.generateViewId()
+    private val selColor = MultiLanguagesApplication.appContext.getColor(R.color.color_025BE8)
+    private val norColor = MultiLanguagesApplication.appContext.getColor(R.color.color_6D7693)
     private val lp = LinearLayout.LayoutParams(-1, -1)
     private val iconLp = 32.dp.let { LinearLayout.LayoutParams(it, it)}
-    private val nameLp = LinearLayout.LayoutParams(-2, -2).apply { topMargin = 5.dp }
+    private val nameLp = LinearLayout.LayoutParams(-2, -2).apply { topMargin = 4.dp }
     private var currentPosition = 0
     private var currentItem: OKGamesCategory? = null
 
@@ -60,7 +64,7 @@ class ElectTabAdapter(private val onSelected:(OKGamesCategory) -> Unit)
         dividerLp.rightMargin = dividerLp.leftMargin
 
         val root = FrameLayout(context)
-        root.layoutParams = LinearLayout.LayoutParams(-1, 68.dp)
+        root.layoutParams = LinearLayout.LayoutParams(-1, 73.dp)
         root.addView(divider, dividerLp)
         root.addView(lin)
 
@@ -69,14 +73,17 @@ class ElectTabAdapter(private val onSelected:(OKGamesCategory) -> Unit)
 
     override fun convert(holder: BaseViewHolder, item: OKGamesCategory) {
         item.bindLabelIcon(holder.getView(iconId))
-        item.bindNameText(holder.getView(nameId))
+        val nameText = holder.getView<TextView>(nameId)
+        nameText.text = item.categoryName
         val position = holder.bindingAdapterPosition
         val isSelected = currentPosition == position
         holder.setGone(dividerId, isSelected || position + 1 == currentPosition || position == itemCount - 1)
         if (isSelected) {
             holder.itemView.setBackgroundResource(R.drawable.bg_sportvenue_type)
+            nameText.setTextColor(selColor)
         } else {
             holder.itemView.background = null
+            nameText.setTextColor(norColor)
         }
     }
 
