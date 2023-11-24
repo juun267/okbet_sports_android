58c03eb2a88c224c9e2f38f5dcb0ac07e16ba6d9
anderson <devanderson1108@gmail.com>
2023-11-24 16:41:22 +0800

    补上 @KeepMembers

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/UserInfoDetailsEntity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/UserInfoDetailsEntity.kt
@@ -1,11 +1,14 @@
 package org.cxct.sportlottery.ui.profileCenter.profile
 
+import org.cxct.sportlottery.common.proguards.KeepMembers
 import org.cxct.sportlottery.network.common.BaseResult
 
+@KeepMembers
 data class UserInfoDetailsEntity(
     val t: Uide, override val code: Int, override val msg: String, override val success: Boolean
 ) : BaseResult()
 
+@KeepMembers
 data class Uide(
     var address: String? = "",
     val birthday: String? = "",
@@ -21,7 +24,7 @@ data class Uide(
     val salarySource: SalarySource? = null,
     var zipCode: String? = ""
 )
-
+@KeepMembers
 data class SalarySource(
     val id: Int? = -1,
     val name: String? = ""
