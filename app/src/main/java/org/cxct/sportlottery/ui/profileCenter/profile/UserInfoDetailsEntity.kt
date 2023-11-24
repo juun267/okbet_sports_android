38d625778d51e3578a9f39b49d203186cd8884e0
LouisWu <louis_android@cxgi.org>
2023-11-24 16:49:15 +0800

    [Fix] 加上@KeepMembers 避免個人中心閃退

--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/UserInfoDetailsEntity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/UserInfoDetailsEntity.kt
@@ -1,11 +1,14 @@
 package org.cxct.sportlottery.ui.profileCenter.profile
 
+import org.cxct.sportlottery.common.proguards.KeepMembers
 import org.cxct.sportlottery.network.common.BaseResult
 
+@KeepMembers
 data class UserInfoDetailsEntity(
     val t: Uide, override val code: Int, override val msg: String, override val success: Boolean
 ) : BaseResult()
 
+@KeepMembers
 data class Uide(
     var address: String? = "",
     val birthday: String? = "",
@@ -22,6 +25,7 @@ data class Uide(
     var zipCode: String? = ""
 )
 
+@KeepMembers
 data class SalarySource(
     val id: Int? = -1,
     val name: String? = ""
--- app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/WorkNameEntity.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/profileCenter/profile/WorkNameEntity.kt
@@ -1,7 +1,9 @@
 package org.cxct.sportlottery.ui.profileCenter.profile
 
+import org.cxct.sportlottery.common.proguards.KeepMembers
 import org.cxct.sportlottery.network.common.BaseResult
 
+@KeepMembers
 data class WorkNameEntity(
     var rows: MutableList<String>,
     override val code: Int,
