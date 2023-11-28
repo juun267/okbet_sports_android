e00f80b0e6ca51e6ae0489ba1d7603b4af1764e9
anderson <devanderson1108@gmail.com>
2023-11-28 18:28:31 +0800

    Revert "首页OKGames和OKLive的Tab显示优化"
    This reverts commit 56727e4dc03993fd314733407e0bd15dc163f0f5.

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/HomeOkGamesAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/HomeOkGamesAdapter.kt
@@ -1,8 +1,12 @@
 package org.cxct.sportlottery.ui.maintab.home.view
 
+import android.graphics.Color
+import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import androidx.core.view.isVisible
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import eightbitlab.com.blurview.RenderScriptBlur
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.common.adapter.BindingAdapter
 import org.cxct.sportlottery.common.extentions.collectWith
@@ -10,6 +14,8 @@ import org.cxct.sportlottery.common.extentions.load
 import org.cxct.sportlottery.databinding.ItemHomeOkgameBinding
 import org.cxct.sportlottery.net.games.data.OKGameBean
 import org.cxct.sportlottery.service.ServiceBroadcastReceiver
+import org.cxct.sportlottery.util.AppManager
+import splitties.resources.drawable
 
 class HomeOkGamesAdapter : BindingAdapter<OKGameBean, ItemHomeOkgameBinding>() {
 
@@ -24,8 +30,13 @@ class HomeOkGamesAdapter : BindingAdapter<OKGameBean, ItemHomeOkgameBinding>() {
         }
     }
     override fun onBinding(position: Int, binding: ItemHomeOkgameBinding, item: OKGameBean) {
-
+        binding.tvGameName.text = item.gameName
+        binding.tvGameType.text = "- ${item.firmName} -"
         binding.ivCover.load(item.imgGame, R.drawable.ic_okgames_nodata)
+        binding.blurView
+            .setupWith(binding.root)
+            .setFrameClearDrawable(binding.root.background)
+            .setBlurRadius(4f)
         binding.linMaintenance.isVisible = item.isMaintain()
     }
 
