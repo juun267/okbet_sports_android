229be91aa434d0b419e34b2a92bda72425d6dc46
anderson <devanderson1108@gmail.com>
2023-11-29 09:59:31 +0800

    okgame item优化

--- app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/HomeOkGamesAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/maintab/home/view/HomeOkGamesAdapter.kt
@@ -1,22 +1,15 @@
 package org.cxct.sportlottery.ui.maintab.home.view
 
-import android.graphics.Color
-import android.view.ViewGroup
-import android.view.ViewOutlineProvider
 import androidx.core.view.isVisible
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import eightbitlab.com.blurview.RenderScriptBlur
 import org.cxct.sportlottery.R
 import org.cxct.sportlottery.common.adapter.BindingAdapter
 import org.cxct.sportlottery.common.extentions.collectWith
 import org.cxct.sportlottery.common.extentions.load
-import org.cxct.sportlottery.databinding.ItemHomeOkgameBinding
 import org.cxct.sportlottery.databinding.ItemHomeOkgameChrisBinding
 import org.cxct.sportlottery.net.games.data.OKGameBean
 import org.cxct.sportlottery.service.ServiceBroadcastReceiver
-import org.cxct.sportlottery.util.AppManager
-import splitties.resources.drawable
 
 class HomeOkGamesAdapter : BindingAdapter<OKGameBean, ItemHomeOkgameChrisBinding>() {
 
@@ -31,7 +24,13 @@ class HomeOkGamesAdapter : BindingAdapter<OKGameBean, ItemHomeOkgameChrisBinding
         }
     }
     override fun onBinding(position: Int, binding: ItemHomeOkgameChrisBinding, item: OKGameBean) {
+        binding.tvGameName.text = item.gameName
+        binding.tvGameType.text = "- ${item.firmName} -"
         binding.ivCover.load(item.imgGame, R.drawable.ic_okgames_nodata)
+        binding.blurView
+            .setupWith(binding.root)
+            .setFrameClearDrawable(binding.root.background)
+            .setBlurRadius(4f)
         binding.linMaintenance.isVisible = item.isMaintain()
     }
 
