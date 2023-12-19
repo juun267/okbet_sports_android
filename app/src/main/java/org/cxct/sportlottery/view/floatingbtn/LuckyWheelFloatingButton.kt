43cdf33badf6b4f60e5a101528cf80caaea442eb
clyde <android_clyde@okbet.com>
2023-12-19 15:14:43 +0800

    优化代码

--- app/src/main/java/org/cxct/sportlottery/view/floatingbtn/LuckyWheelFloatingButton.kt
+++ app/src/main/java/org/cxct/sportlottery/view/floatingbtn/LuckyWheelFloatingButton.kt
@@ -2,33 +2,46 @@ package org.cxct.sportlottery.view.floatingbtn
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import androidx.constraintlayout.motion.widget.MotionLayout
-import kotlinx.android.synthetic.main.motion_lottery_floating.view.*
-import org.cxct.sportlottery.databinding.MotionViewLuckyWheelFloatingBinding
-import org.cxct.sportlottery.util.LotteryManager
+import android.view.Gravity
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageView
+import org.cxct.sportlottery.R
+import org.cxct.sportlottery.util.DisplayUtil.dp
 import org.cxct.sportlottery.util.LuckyWheelManager
 
 class LuckyWheelFloatingButton @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyle: Int = 0
-) :
-    MotionLayout(context, attributeSet, defStyle) {
-    private val binding: MotionViewLuckyWheelFloatingBinding
+) : FrameLayout(context, attributeSet, defStyle) {
+
 
     init {
-        binding = MotionViewLuckyWheelFloatingBinding.inflate(LayoutInflater.from(context),this,false)
-        addView(binding.root)
-        initClickEvent()
+
+        val lp = LayoutParams(-2, -2)
+        lp.gravity = Gravity.RIGHT or Gravity.BOTTOM
+        lp.rightMargin = 10.dp
+        lp.bottomMargin = 300.dp
+        layoutParams = lp
+
+        val imageView = AppCompatImageView(context)
+        imageView.setImageResource(R.drawable.ic_lucky_wheel)
+        imageView.adjustViewBounds = true
+        val imgLP = LayoutParams(96.dp, -2)
+        imgLP.topMargin = 6.dp
+        addView(imageView, imgLP)
+
+        val ivClose = AppCompatImageView(context)
+        val dp15 = 15.dp
+        val closeLP = LayoutParams(dp15, dp15)
+        closeLP.gravity = Gravity.RIGHT
+        closeLP.rightMargin = 9.dp
+        ivClose.setImageResource(R.drawable.ic_close_float)
+        addView(ivClose, closeLP)
+
+        setOnTouchListener(SuckEdgeTouch())
+        setOnClickListener { LuckyWheelManager.instance.clickContent() }
+        ivClose.setOnClickListener { LuckyWheelManager.instance.clickCloseFloatBtn() }
     }
 
-    private fun initClickEvent() {
-        binding.movableLayout.setOnClickListener {
-            LuckyWheelManager.instance.clickContent()
-        }
-        binding.ivClose.setOnClickListener {
-            LuckyWheelManager.instance.clickCloseFloatBtn()
-        }
-    }
 }
