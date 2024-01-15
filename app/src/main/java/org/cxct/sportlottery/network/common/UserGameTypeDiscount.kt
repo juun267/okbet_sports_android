7c14e5997e67c398092ca020fe30e38594d368fb
anderson <devanderson1108@gmail.com>
2024-01-15 16:25:50 +0800

    修复登录报错

--- app/src/main/java/org/cxct/sportlottery/network/common/UserGameTypeDiscount.kt
+++ app/src/main/java/org/cxct/sportlottery/network/common/UserGameTypeDiscount.kt
@@ -1,14 +1,12 @@
 package org.cxct.sportlottery.network.common
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.cxct.sportlottery.common.proguards.KeepMembers
 
+@Parcelize
 @KeepMembers
-@JsonClass(generateAdapter = true)
 data class UserGameTypeDiscount(
-    @Json(name = "discount")
     var discount: String,
-    @Json(name = "gameType")
     val gameType: String
-)
+):Parcelable
--- app/src/main/java/org/cxct/sportlottery/network/index/login/LoginResult.kt
+++ app/src/main/java/org/cxct/sportlottery/network/index/login/LoginResult.kt
@@ -1,10 +1,13 @@
 package org.cxct.sportlottery.network.index.login
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
 import org.cxct.sportlottery.common.proguards.KeepMembers
 import org.cxct.sportlottery.network.common.BaseResult
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 @KeepMembers
 data class LoginResult(
@@ -18,4 +21,4 @@ data class LoginResult(
     val t: LoginData? = null,  // google登陆会返回这个字段
     @Json(name = "rows")
     val rows: List<LoginData>? = null
-) : BaseResult(),java.io.Serializable
+) : BaseResult(),Parcelable
