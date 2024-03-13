bcf44496b27d9806b046eb173b18795200204b45
anderson <devanderson1108@gmail.com>
2024-03-13 11:27:53 +0800

    优化消息列表弹窗

--- app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterDetailDialog.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/infoCenter/InfoCenterDetailDialog.kt
@@ -1,16 +1,15 @@
 package org.cxct.sportlottery.ui.infoCenter
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import org.cxct.sportlottery.databinding.DialogInfoCenterDetailListBinding
 import org.cxct.sportlottery.network.infoCenter.InfoCenterData
 import org.cxct.sportlottery.ui.base.BaseDialog
+import org.cxct.sportlottery.util.DisplayUtil.dp
 
 class InfoCenterDetailDialog: BaseDialog<InfoCenterViewModel,DialogInfoCenterDetailListBinding>() {
 
-
+    init {
+        marginHorizontal = 40.dp
+    }
     private var data: InfoCenterData? = null
         get() {
             if (field == null) {
@@ -20,11 +19,6 @@ class InfoCenterDetailDialog: BaseDialog<InfoCenterViewModel,DialogInfoCenterDet
         }
 
     override fun onInitView() {
-        binding.apply {
-            infoCenterViewModel = this@InfoCenterDetailDialog.viewModel
-            lifecycleOwner = this@InfoCenterDetailDialog
-            infoCenterData = infoCenterData
-        }
         binding.infoCenterDetailConfirm.setOnClickListener {
             dismiss()
         }
