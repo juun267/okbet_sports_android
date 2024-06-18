2103ca193b9fecd471e42b73c50187d1887b3185
anderson <devanderson1108@gmail.com>
2024-06-18 15:36:58 +0800

    增加测试邀请码页面

--- app/src/main/java/org/cxct/sportlottery/network/Constants.kt
+++ app/src/main/java/org/cxct/sportlottery/network/Constants.kt
@@ -248,7 +248,7 @@ object Constants {
 
     val copyRightString = "Copyright © ${Calendar.getInstance().get(Calendar.YEAR)} OKBET ALL RIGHTS RESERVED"
 
-    fun getInviteUrl() = "${getH5BaseUrl()}mobile/InviteFriends"
+    fun getInviteUrl() = "https://okbet-v3.cxsport.net/mobile/InviteFriends"
 
     /**
      * 给h5地址加上统一参数
