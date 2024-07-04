f50fde27c4760e6c4af8a7f85e02f61e961df50f
anderson <devanderson1108@gmail.com>
2024-07-04 11:36:43 +0800

    gcash钱包列表删除优化

--- app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankCardListAdapter.kt
+++ app/src/main/java/org/cxct/sportlottery/ui/money/withdraw/BankCardListAdapter.kt
@@ -9,6 +9,7 @@ import org.cxct.sportlottery.network.money.config.MoneyRechCfg
 import org.cxct.sportlottery.network.money.config.TransferType
 import org.cxct.sportlottery.network.money.config.UwType
 import org.cxct.sportlottery.repository.sConfigData
+import org.cxct.sportlottery.util.LogUtil
 import org.cxct.sportlottery.util.MoneyManager
 
 class BankCardListAdapter(private val onCryptoEdit: (BankCardList) -> Unit,
@@ -25,15 +26,11 @@ class BankCardListAdapter(private val onCryptoEdit: (BankCardList) -> Unit,
         }
 
     override fun onBinding(position: Int, binding: ContentRvBankCardListBinding, data: BankCardList) = binding.run {
-        val bankOpen = uwTypes?.find { it.type == TransferType.BANK.type }?.open == MoneyRechCfg.Switch.OPEN.code
-        val cryptoOpen = uwTypes?.find { it.type == TransferType.CRYPTO.type }?.open == MoneyRechCfg.Switch.OPEN.code
-
         if (data.transferType == TransferType.CRYPTO) {
             ivBankIcon.setImageResource(MoneyManager.getCryptoIconByCryptoName(data.bankName))//虚拟币图标
         } else {
             ivBankIcon.setImageResource(MoneyManager.getBankIconByBankName(data.bankName)) //银行卡图标
         }
-
         tvBankName.text = data.bankName //银行名字
         tvTailNumber.text = if (data.cardNo.length > 4) data.cardNo.substring(data.cardNo.length - 4) else data.cardNo //尾號四碼
         bankCardType.text = when (data.transferType.type) {
@@ -43,23 +40,13 @@ class BankCardListAdapter(private val onCryptoEdit: (BankCardList) -> Unit,
             TransferType.PAYMAYA.type -> context.getString(R.string.ewallet)
             else -> context.getString(R.string.bank_card)
         }
-        //
-        if (data.transferType == TransferType.CRYPTO) {
-            if (sConfigData?.enableModifyBank == "1" && cryptoOpen) {
-                rlItemContent.clickDelay { onCryptoEdit(data) }
-                ivDelete.clickDelay { onDelete(data) }
-            } else {
-                rlItemContent.setOnClickListener(null)
-                ivDelete.setOnClickListener(null)
-            }
+        val typeOpen = uwTypes?.find { it.type == data.transferType.type }?.open == MoneyRechCfg.Switch.OPEN.code
+        if (sConfigData?.enableModifyBank == "1" && typeOpen) {
+            rlItemContent.clickDelay { onCryptoEdit(data) }
+            ivDelete.clickDelay { onDelete(data) }
         } else {
-            if (sConfigData?.enableModifyBank == "1" && bankOpen) {
-                rlItemContent.clickDelay { onBankEdit(data) }
-                ivDelete.clickDelay { onDelete(data) }
-            } else {
-                rlItemContent.setOnClickListener(null)
-                ivDelete.setOnClickListener(null)
-            }
+            rlItemContent.setOnClickListener(null)
+            ivDelete.setOnClickListener(null)
         }
     }
 
