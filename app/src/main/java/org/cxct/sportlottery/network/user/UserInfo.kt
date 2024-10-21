2cba91a72fd49371530e17675d9cd73e5adc27f5
anderson <devanderson1108@gmail.com>
2024-10-21 16:36:07 +0800

    解决冲突

--- app/src/main/java/org/cxct/sportlottery/network/user/UserInfo.kt
+++ app/src/main/java/org/cxct/sportlottery/network/user/UserInfo.kt
@@ -56,7 +56,7 @@ data class UserInfo(
     var rejectRemark: String? = null,
     val levelCode: String?=null,
     val regSource: Int? = null,
-    val safeQuestionType: Int? = null //是否设置密保问题
+    val safeQuestionType: Int? = null, //是否设置密保问题
     val fullVerified: Int? = null ////是否完全KYC认证，0为未完全认证，1为完全认证
 ){
     fun isGlifeAccount():Boolean = vipType==1
